4d73106eaefc5f8e03f76855b0e41844446a0a65
alckasoc <tuvincent0106@gmail.com>
2023-10-18 18:21:08 -0700

    removed chain.py in utils

--- discussion_agents/utils/chain.py
+++
@@ -1,8 +0,0 @@
-"""Utility methods for creating basic LangChain chains."""
-from typing import Any, Dict, Optional
-
-from langchain.chains import LLMChain
-
-
-def chain(llm, llm_kwargs: Optional[Dict[str, Any]] = {}, memory=None):
-    pass
