aaf2a7449dc982e9965931ab1f1c5c2634824844
alckasoc <tuvincent0106@gmail.com>
2023-10-21 18:33:38 -0700

    updated docstring for memory core

--- discussion_agents/core/memory.py
+++ discussion_agents/core/memory.py
@@ -7,6 +7,23 @@ from discussion_agents.core.base import BaseCore
 
 
 class BaseCoreWithMemory(BaseCore):
+    """
+    Agent core class with memory support and a retriever, extending the BaseCore.
+
+    This class extends BaseCore, enforcing a retriever and memory component
+    as arguments. Additionally, it uses the memory in the LLMChain.
+
+    Attributes:
+        llm (BaseLanguageModel): The language model used by the agent's core.
+        llm_kwargs (Dict[str, Any], optional): Additional keyword arguments for configuring the language model.
+        retriever (BaseRetriever): A retriever component for accessing external data.
+        memory (BaseMemory): A memory management component for storing and retrieving agent memories.
+
+    Methods:
+        chain(prompt: str) -> LLMChain:
+            Create an LLMChain instance for generating responses based on the provided prompt.
+    """
+
     retriever: BaseRetriever
     memory: BaseMemory
 
