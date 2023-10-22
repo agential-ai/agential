0a76ea1b2fbf865405a2931d9990c01457079285
alckasoc <tuvincent0106@gmail.com>
2023-10-21 17:53:09 -0700

    completed test cases + docstrings for BaseCore

--- discussion_agents/core/base.py
+++ discussion_agents/core/base.py
@@ -6,20 +6,59 @@ from langchain.chains import LLMChain
 from langchain.schema.language_model import BaseLanguageModel
 from langchain.schema.memory import BaseMemory
 from langchain.schema.retriever import BaseRetriever
+from langchain.schema.prompt_template import BasePromptTemplate
 from pydantic.v1 import BaseModel, Field
 
 
 class BaseCoreInterface(BaseModel, ABC):
     @abstractmethod
-    def chain(self, prompt: str) -> LLMChain:
+    def chain(self, prompt: BasePromptTemplate) -> LLMChain:
+        """Abstract method for creating an LLMChain based on a given prompt.
+
+        Args:
+            prompt (BasePromptTemplate): The prompt used to initialize the LLMChain.
+
+        Returns:
+            LLMChain: An instance of the LLMChain with the specified prompt.
+        """
         pass
 
 
 class BaseCore(BaseCoreInterface):
+    """
+    Base class for an agent core that interfaces with language models, retrievers, and memory.
+
+    This base class defines the structure of an agent's core component, which serves as a bridge
+    between various components such as language models, retrievers, and memory management. It
+    provides methods for creating and managing LLMChain instances for generating responses
+    and insights.
+
+    Note:
+        Subclasses of this base core class should provide specific implementations for
+        their respective use cases, customizing the interaction between language models,
+        retrievers, and memory management.
+
+    Attributes:
+        llm (BaseLanguageModel): The language model used by the agent's core.
+        llm_kwargs (Dict[str, Any], optional): Additional keyword arguments for configuring the language model.
+        retriever (BaseRetriever, optional): A retriever component for accessing external data.
+        memory (BaseMemory, optional): A memory management component for storing and retrieving agent memories.
+    """
+
     llm: BaseLanguageModel
     llm_kwargs: Dict[str, Any] = Field(default_factory=dict)
     retriever: BaseRetriever = Field(default=None)
     memory: BaseMemory = Field(default=None)
 
-    def chain(self, prompt: str) -> LLMChain:
+    def chain(self, prompt: BasePromptTemplate) -> LLMChain:
+        """Create an LLMChain based on a given prompt template.
+
+        BaseCore only returns stateless LLMChains. 
+        
+        Args:
+            prompt (BasePromptTemplate): The prompt template used to initialize the LLMChain.
+
+        Returns:
+            LLMChain: An instance of the LLMChain with the specified prompt template.
+        """
         return LLMChain(llm=self.llm, llm_kwargs=self.llm_kwargs, prompt=prompt)
---
+++ tests/core/test_base_core.py
@@ -0,0 +1,62 @@
+"""Unit tests for base core."""
+
+import os
+
+from datetime import datetime
+
+import dotenv
+import faiss
+
+from langchain.prompts import PromptTemplate
+from langchain.docstore import InMemoryDocstore
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms.huggingface_hub import HuggingFaceHub
+from langchain.retrievers import TimeWeightedVectorStoreRetriever
+from langchain.vectorstores import FAISS
+
+from discussion_agents.core.base import BaseCore
+
+dotenv.load_dotenv(".env")
+huggingface_hub_api_key = os.getenv("HUGGINGFACE_HUB_API_KEY")
+
+llm = HuggingFaceHub(
+    repo_id="gpt2",
+    huggingfacehub_api_token=huggingface_hub_api_key,
+)
+
+embedding_size = (
+    768  # Embedding dimension for all-mpnet-base-v2. FAISS needs the same count.
+)
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": False}
+
+test_date = datetime(year=2022, month=11, day=14, hour=3, minute=14)
+
+def create_memory_retriever():
+    """Creates a TimeWeightedVectorStoreRetriever."""
+    embeddings_model = HuggingFaceEmbeddings(
+        model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
+    )
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+    retriever = TimeWeightedVectorStoreRetriever(
+        vectorstore=vectorstore, otherScoreKeys=["importance"], k=5
+    )
+    return retriever
+
+def test_base_core():
+    """Test BaseCore & chain method."""
+    core = BaseCore(
+        llm=llm,
+        llm_kwargs={},
+        retriever=create_memory_retriever()
+    )
+
+    prompt = PromptTemplate.from_template(
+        "Explain the importance of eating a proper meal."
+    )
+
+    chain = core.chain(prompt=prompt)
+    out = chain.run({})
+    assert type(out) is str
