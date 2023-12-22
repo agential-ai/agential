66f2c74c7bb3342235c7083f4d32510eef2c8e3b
alckasoc <tuvincent0106@gmail.com>
2023-12-22 15:44:04 -0800

    added base scoring class

--- discussion_agents/cog/modules/scoring/base.py
+++ discussion_agents/cog/modules/scoring/base.py
@@ -1,1 +1,21 @@
 """Base scoring module."""
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Union
+
+from langchain_core.language_models import LLM
+
+
+class BaseScoring(ABC):
+    """Base scoring class."""
+
+    llm: LLM
+    llm_kwargs: Dict[str, Any] = {}
+
+    @abstractmethod
+    def score(
+        self,
+        memory_contents: Union[str, List[str]],
+        relevant_memories: Union[str, List[str]],
+    ) -> List[str]:
+        """Score memory_contents with respect to relevant memories."""
+        pass
