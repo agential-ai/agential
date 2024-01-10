e05f10ba9df96c38417b52db918905868fb71275
alckasoc <tuvincent0106@gmail.com>
2024-01-10 02:05:13 -0800

    added tst cases for memory

---
+++ tests/cog/eval/test_reflexion_eval.py
@@ -0,0 +1,15 @@
+"""Unit tests for Reflexion eval."""
+
+from discussion_agents.cog.eval.reflexion import EM
+
+def test_em() -> None:
+    """Test EM function."""
+    sample_answer = "A fox jumped over the fence. An apple was on the table. The quick brown fox."
+    sample_key = "A fox jumped over the fence. An apple was on the table. The quick brown fox."
+    result = EM(sample_answer, sample_key)
+    assert result
+
+    sample_answer = "1A fox jumped over the fence. An apple was on the table. The quick brown fox."
+    sample_key = "A fox jumped over the fence. An apple was on the table. The quick brown fox."
+    result = EM(sample_answer, sample_key)
+    assert not result 
