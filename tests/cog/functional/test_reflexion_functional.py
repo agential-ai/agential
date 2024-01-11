630b79f66ca56708a7811324358c011405c8631a
alckasoc <tuvincent0106@gmail.com>
2024-01-10 19:04:29 -0800

    added test case method signatures

---
+++ tests/cog/functional/test_reflexion_functional.py
@@ -0,0 +1,45 @@
+"""Unit tests for Reflexion functional methods."""
+
+def test__truncate_scratchpad() -> None:
+    """Test _truncate_scratchpad function."""
+    pass
+
+def test__format_reflections() -> None:
+    """Test _format_reflections function."""
+    pass
+
+def test__format_last_attempt() -> None:
+    """Test _format_last_attempt function."""
+    pass
+
+def test__format_step() -> None:
+    """Test _format_step function."""
+    pass
+
+def test__parse_action() -> None:
+    """Test _parse_action function."""
+    pass
+
+def test__prompt_cot_agent() -> None:
+    """Test _prompt_cot_agent function."""
+    pass
+
+def test__prompt_cot_reflection() -> None:
+    """Test _prompt_cot_reflection function."""
+    pass
+
+def test_reflect_last_attempt() -> None:
+    """Test reflect_last_attempt function."""
+    pass
+
+def test_reflect_reflexion() -> None:
+    """Test reflect_reflexion function."""
+    pass
+
+def test_reflect_last_attempt_and_reflexion() -> None:
+    """Test reflect_last_attempt_and_reflexion function."""
+    pass
+
+def test_reflect() -> None:
+    """Test reflection function."""
+    pass
