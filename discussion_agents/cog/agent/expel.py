72f1c042135bdc4ea97f713ec82225d22f6e0272
alckasoc <tuvincent0106@gmail.com>
2024-02-07 17:02:48 -0800

    extra files; working on experience gathering section

---
+++ discussion_agents/cog/agent/expel.py
@@ -0,0 +1,13 @@
+
+
+from discussion_agents.cog.agent.base import BaseAgent
+
+
+class ExpeLAgent(BaseAgent):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def generate(self) -> str:
+        if self.perform_reflection and not self.is_success():
+            self.reflect()
+        ReactAgent.run(self, reset)
