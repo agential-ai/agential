93554ca0a48c2e6c48559a842361a7216461a139
alckasoc <tuvincent0106@gmail.com>
2024-03-04 03:50:01 -0800

    added general util

---
+++ discussion_agents/utils/general.py
@@ -0,0 +1,25 @@
+"""General utility functions."""
+
+from typing import List, Any
+import math
+import random
+
+def random_divide_list(lst: List[Any], k: int) -> List[List[Any]]:
+    """Divides the list into chunks, each with maximum length k.
+
+    Args:
+        lst: The list to be divided.
+        k: The maximum length of each chunk.
+
+    Returns:
+        A list of chunks.
+
+    Ref: https://github.com/LeapLabTHU/ExpeL.
+    """
+    random.shuffle(lst)
+    if len(lst) <= k:
+        return [lst]
+    else:
+        num_chunks = math.ceil(len(lst) / k)
+        chunk_size = math.ceil(len(lst) / num_chunks)
+        return [lst[i*chunk_size:(i+1)*chunk_size] for i in range(num_chunks)]
