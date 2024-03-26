4bf3168048f3e5b6db016af90cd1a81963e7095a
alckasoc <tuvincent0106@gmail.com>
2024-03-25 22:22:00 -0700

    added unit test skeletons

--- tests/cog/agent/test_expel.py
+++ tests/cog/agent/test_expel.py
@@ -1,3 +1,25 @@
 """Unit tests for ExpeL."""
 
 from discussion_agents.cog.agent.expel import ExpeLAgent
+
+def test_init() -> None:
+    """Test initialization."""
+    
+def test_generate() -> None:
+    """Test generate."""
+
+def test_reset() -> None:
+    """Test reset."""
+
+def test_retrieve() -> None:
+    """Test retrieve."""
+
+def test_gather_experience() -> None:
+    """Test gather_experience."""
+
+def test_extract_insights() -> None:
+    """Test extract_insights."""
+
+def test_update_insights() -> None:
+    """"Test update_insights."""
+    
