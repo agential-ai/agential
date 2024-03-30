1933d574b22e4e75ca2139c4eb7679f8028c20dd
alckasoc <tuvincent0106@gmail.com>
2024-03-30 00:34:31 -0700

    added gsm8k fewshot examples

--- discussion_agents/cog/agent/self_refine.py
+++ discussion_agents/cog/agent/self_refine.py
@@ -3,3 +3,22 @@
 Original Webpage: https://selfrefine.info/
 Paper Repository: https://github.com/madaan/self-refine
 """
+
+from typing import Any
+from discussion_agents.cog.agent.base import BaseAgent
+
+class SelfRefineAgent(BaseAgent):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def generate(self, *args: Any, **kwargs: Any) -> Any:
+        return super().generate(*args, **kwargs)
+    
+    def reset(self, *args: Any, **kwargs: Any) -> Any:
+        return super().reset(*args, **kwargs)
+    
+    def reflect(self, *args: Any, **kwargs: Any) -> Any:
+        return super().reflect(*args, **kwargs)
+    
+    def retrieve(self, *args: Any, **kwargs: Any) -> Any:
+        return super().retrieve(*args, **kwargs)
