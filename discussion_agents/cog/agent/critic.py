511ac6358f7ab1cc6bc1390e7e81d2c273845656
alckasoc <tuvincent0106@gmail.com>
2024-04-04 03:58:11 -0700

    added tmp nb; crtique prompts

--- discussion_agents/cog/agent/critic.py
+++ discussion_agents/cog/agent/critic.py
@@ -7,8 +7,14 @@ Original Paper: http://arxiv.org/abs/2305.11738
 from typing import Any
 from discussion_agents.cog.agent.base import BaseAgent
 from langchain_core.language_models.chat_models import BaseChatModel
-from discussion_agents.cog.functional.critic import _prompt_agent
-from discussion_agents.cog.prompts.critic import HOTPOTQA_FEWSHOT_EXAMPLES_COT, CRITIC_INSTRUCTION_HOTPOTQA
+from discussion_agents.cog.functional.critic import _prompt_agent, _prompt_critique, _build_critique_format_prompt
+from discussion_agents.cog.prompts.critic import (
+    HOTPOTQA_FEWSHOT_EXAMPLES_COT, 
+    CRITIC_INSTRUCTION_HOTPOTQA,
+    HOTPOTQA_FEWSHOT_EXAMPLES_CRITIC,
+    CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA,
+    CRITIC_CRITIQUE_FORMAT_HOTPOTQA
+)
 
 class CriticAgent(BaseAgent):
     def __init__(
@@ -23,12 +29,47 @@ class CriticAgent(BaseAgent):
         self, 
         question: str,
         examples: str = HOTPOTQA_FEWSHOT_EXAMPLES_COT,
-        prompt: str = CRITIC_INSTRUCTION_HOTPOTQA
+        prompt: str = CRITIC_INSTRUCTION_HOTPOTQA,
+        critique_examples: str = HOTPOTQA_FEWSHOT_EXAMPLES_CRITIC,
+        critique_prompt: str = CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA,
+        critique_format_prompt: str = CRITIC_CRITIQUE_FORMAT_HOTPOTQA,
+        max_interactions: int = 7,
+        use_tool: bool = True
     ) -> Any:
-        out = _prompt_agent(
+        answer = _prompt_agent(
             llm=self.llm,
             question=question,
             examples=examples,
             prompt=prompt
         )
+
+        exist_query = []
+        exist_evidence = set()
+        for idx in range(max_interactions):
+            critique = _prompt_critique(
+                llm=self.llm,
+                question=question,
+                examples=critique_examples,
+                answer=answer,
+                prompt=critique_prompt
+            )
+
+            if "> Search Query: " in critique:
+                _, search_query = critique.split("> Search Query:")[:2]
+                search_query = search_query.split("\n")[0].strip()
+                a = _build_critique_format_prompt(
+                    question=question,
+                    examples=critique_examples,
+                    answer=answer,
+                    critique=critique,
+                    prompt=critique_format_prompt
+                )
+
+                if use_tool:
+                    exist_query.append(search_query)
+                    
+                else:
+                    pass
+
+
 
