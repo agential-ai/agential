d5e04a81243cca26034fd3770b017043a54cc7d0
alckasoc <tuvincent0106@gmail.com>
2024-04-04 03:58:11 -0700

    added tmp nb; crtique prompts

--- discussion_agents/cog/functional/critic.py
+++ discussion_agents/cog/functional/critic.py
@@ -3,7 +3,12 @@
 from langchain.prompts import PromptTemplate
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.messages.human import HumanMessage
-from discussion_agents.cog.prompts.critic import CRITIC_INSTRUCTION_HOTPOTQA
+from discussion_agents.cog.prompts.critic import (
+    CRITIC_INSTRUCTION_HOTPOTQA,
+    CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA,
+    CRITIC_CRITIQUE_FORMAT_HOTPOTQA
+)
+
 
 def _build_agent_prompt(
     question: str,
@@ -15,6 +20,7 @@ def _build_agent_prompt(
         examples=examples
     )
     return prompt
+
 
 def _prompt_agent(
     llm: BaseChatModel,
@@ -36,3 +42,57 @@ def _prompt_agent(
     ).content
 
     return out
+
+
+def _build_critique_prompt(
+    question: str,
+    examples: str, 
+    answer: str,
+    prompt: str = CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA
+) -> str:
+    prompt = PromptTemplate.from_template(prompt).format(
+        question=question,
+        examples=examples,
+        answer=answer
+    )
+    return prompt
+
+
+def _prompt_critique(
+    llm: BaseChatModel,
+    question: str,
+    examples: str,
+    answer: str,
+    prompt: str = CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA
+) -> str:
+    prompt = _build_critique_prompt(
+        question=question,
+        examples=examples,
+        answer=answer,
+        prompt=prompt
+    )
+    out = llm(
+        [
+            HumanMessage(
+                content=prompt,
+            )
+        ]
+    ).content
+
+    return out
+
+
+def _build_critique_format_prompt(
+    question: str,
+    examples: str, 
+    answer: str,
+    critique: str,
+    prompt: str = CRITIC_CRITIQUE_FORMAT_HOTPOTQA
+) -> str:
+    prompt = PromptTemplate.from_template(prompt).format(
+        question=question,
+        examples=examples,
+        answer=answer,
+        critique=critique
+    )
+    return prompt
--- discussion_agents/cog/prompts/critic.py
+++ discussion_agents/cog/prompts/critic.py
@@ -4,7 +4,8 @@
 CRITIC_INSTRUCTION_HOTPOTQA = """{examples}
 (END OF EXAMPLES)
 
-Q: {question}\nA: """
+Q: {question}
+A: """
 
 
 HOTPOTQA_FEWSHOT_EXAMPLES_COT = """Q: What is the elevation range for the area that the eastern sector of the Colorado orogeny extends into?
@@ -125,6 +126,19 @@ Thought 3: The evidence suggests that Nikita Katsalapov won the 2016 Russian nat
 Action 3: Finish[Nikita Katsalapov]
 
 ---"""
+
+
+CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA = """{examples}
+(END OF EXAMPLES)
+
+Question: {question}
+Proposed Answer: {answer}
+
+What's the problem with the above answer?
+
+1. Plausibility:
+
+"""
 
 
 HOTPOTQA_FEWSHOT_EXAMPLES_CRITIC = """Question: Serianna is a band of what genre that combines elements of heavy metal and hardcore punk?
@@ -341,3 +355,16 @@ Question: Who won the 2016 Russian national silver medal with another Russian ic
 Here's the most possible answer: Let's think step by step. The 2016 Russian national silver medal in ice dancing was won by Victoria Sinitsina and Nikita Katsalapov. Victoria Sinitsina was born on April 29, 1995. So the answer is: Nikita Katsalapov.
 
 ---"""
+
+
+CRITIC_CRITIQUE_FORMAT_HOTPOTQA = """{examples}
+(END OF EXAMPLES)
+
+Question: {question}
+Proposed Answer: {answer}
+
+What's the problem with the above answer?
+
+1. Plausibility:
+
+{critique}"""
