153d91bb7589231a334c246d91e8202ee106baa2
alckasoc <tuvincent0106@gmail.com>
2024-04-03 23:04:02 -0700

    added fewshot examples

--- discussion_agents/cog/agent/critic.py
+++ discussion_agents/cog/agent/critic.py
@@ -7,6 +7,8 @@ Original Paper: http://arxiv.org/abs/2305.11738
 from typing import Any
 from discussion_agents.cog.agent.base import BaseAgent
 from langchain_core.language_models.chat_models import BaseChatModel
+from discussion_agents.cog.functional.critic import _prompt_agent
+from discussion_agents.cog.prompts.critic import HOTPOTQA_FEWSHOT_EXAMPLES_COT, CRITIC_INSTRUCTION_HOTPOTQA
 
 class CriticAgent(BaseAgent):
     def __init__(
@@ -17,6 +19,16 @@ class CriticAgent(BaseAgent):
 
         self.llm = llm
 
-    def generate(self, *args: Any, **kwargs: Any) -> Any:
-        pass
-    
+    def generate(
+        self, 
+        question: str,
+        examples: str = HOTPOTQA_FEWSHOT_EXAMPLES_COT,
+        prompt: str = CRITIC_INSTRUCTION_HOTPOTQA
+    ) -> Any:
+        out = _prompt_agent(
+            llm=self.llm,
+            question=question,
+            examples=examples,
+            prompt=prompt
+        )
+
--- discussion_agents/cog/functional/critic.py
+++ discussion_agents/cog/functional/critic.py
@@ -1,5 +1,8 @@
 """Functional module for CRITIC."""
 
+from langchain.prompts import PromptTemplate
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.messages.human import HumanMessage
 from discussion_agents.cog.prompts.critic import CRITIC_INSTRUCTION_HOTPOTQA
 
 def _build_agent_prompt(
@@ -7,11 +10,29 @@ def _build_agent_prompt(
     examples: str, 
     prompt: str = CRITIC_INSTRUCTION_HOTPOTQA
 ) -> str:
-    pass
+    prompt = PromptTemplate.from_template(prompt).format(
+        question=question,
+        examples=examples
+    )
+    return prompt
 
 def _prompt_agent(
+    llm: BaseChatModel,
     question: str,
     examples: str,
     prompt: str = CRITIC_INSTRUCTION_HOTPOTQA
 ) -> str:
-    pass
+    prompt = _build_agent_prompt(
+        question=question,
+        examples=examples,
+        prompt=prompt
+    )
+    out = llm(
+        [
+            HumanMessage(
+                content=prompt,
+            )
+        ]
+    ).content
+
+    return out
