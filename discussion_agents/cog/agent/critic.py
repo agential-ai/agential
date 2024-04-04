c362032387df2e133c2a0e5edaf78434baedc27b
alckasoc <tuvincent0106@gmail.com>
2024-04-03 22:16:59 -0700

    added skeleton

--- discussion_agents/cog/agent/critic.py
+++ discussion_agents/cog/agent/critic.py
@@ -6,12 +6,17 @@ Original Paper: http://arxiv.org/abs/2305.11738
 
 from typing import Any
 from discussion_agents.cog.agent.base import BaseAgent
+from langchain_core.language_models.chat_models import BaseChatModel
 
 class CriticAgent(BaseAgent):
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        llm: BaseChatModel
+    ) -> None:
         super().__init__()
 
+        self.llm = llm
+
     def generate(self, *args: Any, **kwargs: Any) -> Any:
-        return super().generate(*args, **kwargs)
-    
+        pass
     
