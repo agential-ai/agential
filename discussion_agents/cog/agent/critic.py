8f5a74efc51b692924184acfb163bb48fbfdfa02
alckasoc <tuvincent0106@gmail.com>
2024-04-05 02:37:02 -0700

    fixed small issue with selfrefine; added googleapi client lib

--- discussion_agents/cog/agent/critic.py
+++ discussion_agents/cog/agent/critic.py
@@ -7,6 +7,7 @@ Original Paper: http://arxiv.org/abs/2305.11738
 from typing import Any
 from discussion_agents.cog.agent.base import BaseAgent
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_community.utilities.google_search import GoogleSearchAPIWrapper
 from discussion_agents.cog.functional.critic import _prompt_agent, _prompt_critique, _build_critique_format_prompt
 from discussion_agents.cog.prompts.critic import (
     HOTPOTQA_FEWSHOT_EXAMPLES_COT, 
@@ -19,11 +20,13 @@ from discussion_agents.cog.prompts.critic import (
 class CriticAgent(BaseAgent):
     def __init__(
         self,
-        llm: BaseChatModel
+        llm: BaseChatModel,
+        search: GoogleSearchAPIWrapper
     ) -> None:
         super().__init__()
 
         self.llm = llm
+        self.search = search
 
     def generate(
         self, 
@@ -34,7 +37,8 @@ class CriticAgent(BaseAgent):
         critique_prompt: str = CRITIC_CRITIQUE_INSTRUCTION_HOTPOTQA,
         critique_format_prompt: str = CRITIC_CRITIQUE_FORMAT_HOTPOTQA,
         max_interactions: int = 7,
-        use_tool: bool = True
+        use_tool: bool = True,
+        evidence_length: int = 400
     ) -> Any:
         answer = _prompt_agent(
             llm=self.llm,
@@ -67,9 +71,17 @@ class CriticAgent(BaseAgent):
 
                 if use_tool:
                     exist_query.append(search_query)
-                    
+                    for k in range(exist_query.count(search_query), 8):
+                        search_result = self.search.results(search_query, k=k)
+                        if search_result['snippet'] not in exist_evidence:
+                            exist_evidence.add(search_result['snippet'])
+                            break
+
+                    context = f"""> Evidence: [{search_result['title']}] {search_result['snippet'][:evidence_length]}\n\n"""
+                    if idx == max_interactions - 2:
+                        context += f"Let's give the most possible answer.\n\nQuestion: {question}\nHere's "
                 else:
-                    pass
+                    context = """> Evidence: """
 
 
 
