96ecc7105d0de71012296dcbd1b864663993ac63
alckasoc <tuvincent0106@gmail.com>
2024-04-07 03:46:15 -0700

    added unit tests; deleted tmp

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,8 @@
 from tests.fixtures.agent import *
 from tests.fixtures.data import *
 from tests.fixtures.retriever import *
+from tests.fixtures.api import *
+
+from dotenv import load_dotenv
+
+load_dotenv()
---
+++ tests/fixtures/api.py
@@ -0,0 +1,22 @@
+"""Fixtures for API keys."""
+
+import os
+import pytest
+
+
+@pytest.fixture
+def google_api_key() -> str:
+    """Fixture to retrieve the google_api_key from environment variables."""
+    key = os.getenv("GOOGLE_API_KEY")
+    if key is None:
+        pytest.fail("GOOGLE_API_KEY not set in the environment variables", pytrace=False)
+    return key
+
+
+@pytest.fixture
+def google_cse_id() -> str:
+    """Fixture to retrieve the google_cse_id from environment variables."""
+    key = os.getenv("GOOGLE_CSE_ID")
+    if key is None:
+        pytest.fail("GOOGLE_CSE_ID not set in the environment variables", pytrace=False)
+    return key
