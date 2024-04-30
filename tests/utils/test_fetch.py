6593a97cd2d498cf4e6f856d7dcfbcbfcd323bef
alckasoc <tuvincent0106@gmail.com>
2024-04-30 16:37:56 -0700

    .

--- tests/utils/test_fetch.py
+++ tests/utils/test_fetch.py
@@ -9,9 +9,7 @@ from agential.utils.fetch import fetch_memories
 test_date = datetime(year=2022, month=11, day=14, hour=3, minute=14)
 
 
-def test_fetch_memories(
-    time_weighted_retriever: TimeWeightedVectorStoreRetriever,
-) -> None:
+def test_fetch_memories(time_weighted_retriever: TimeWeightedVectorStoreRetriever) -> None:
     """Test fetch_memories."""
     observation = "Some observation."
 
