beae19f2a8ccd53c85bc200c0ab6ac15d33ed497
alckasoc <tuvincent0106@gmail.com>
2024-05-18 19:47:02 -0700

    added strategy factory; strategies module; needs testing

---
+++ agential/cog/strategies/critic/base.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod
+from typing import Dict
+from agential.cog.strategies.base import BaseStrategy
+
+class CriticBaseStrategy(BaseStrategy):
+    @abstractmethod
+    def generate_critique(self, llm, question: str, examples: str, answer: str, prompt: str, additional_keys: Dict[str, str], critique_additional_keys: Dict[str, str], tests: str, use_interpreter_tool: bool):
+        pass
+
+    @abstractmethod
+    def create_output_dict(self, answer: str, critique: str, additional_keys_update: Dict[str, str]) -> Dict[str, str]:
+        pass
+
+    @abstractmethod
+    def update_answer_based_on_critique(self, llm, question: str, answer: str, critique: str) -> str:
+        pass
