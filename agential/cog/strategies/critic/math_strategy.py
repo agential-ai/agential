a0ec7825d5cf8dabee5d38f38d9758bd9b27a45b
alckasoc <tuvincent0106@gmail.com>
2024-05-20 18:27:32 -0700

    update math strat

--- agential/cog/strategies/critic/math_strategy.py
+++ agential/cog/strategies/critic/math_strategy.py
@@ -1,12 +1,21 @@
-import re
 from typing import Dict
 from agential.cog.functional.critic import _prompt_agent, _prompt_critique, safe_execute
 from agential.cog.strategies.critic.base import CriticBaseStrategy
+import re
 
 class MathStrategy(CriticBaseStrategy):
-    def generate(self, llm, question: str, examples: str, prompt: str, additional_keys: Dict[str, str]) -> str:
+    def __init__(self, llm):
+        self.llm = llm
+
+    def generate(
+        self, 
+        question: str, 
+        examples: str, 
+        prompt: str, 
+        additional_keys: Dict[str, str]
+    ) -> str:
         code = _prompt_agent(
-            llm=llm,
+            llm=self.llm,
             question=question,
             examples=examples,
             additional_keys=additional_keys,
@@ -19,7 +28,20 @@ class MathStrategy(CriticBaseStrategy):
             pass
         return code
 
-    def generate_critique(self, llm, question: str, examples: str, answer: str, prompt: str, additional_keys: Dict[str, str], use_interpreter_tool: bool, use_search_tool: bool):
+    def generate_critique(
+        self, 
+        idx: int, 
+        question: str, 
+        examples: str, 
+        answer: str, 
+        critique: str,
+        prompt: str, 
+        additional_keys: Dict[str, str], 
+        use_interpreter_tool: bool, 
+        use_search_tool: bool,
+        max_interactions: int,
+        **kwargs
+    ):
         critique_additional_keys = additional_keys.copy()
         if use_interpreter_tool:
             code_answer, execution_status = safe_execute(answer)
@@ -29,7 +51,7 @@ class MathStrategy(CriticBaseStrategy):
             })
 
         critique = _prompt_critique(
-            llm=llm,
+            llm=self.llm,
             question=question,
             examples=examples,
             answer=answer,
@@ -48,9 +70,18 @@ class MathStrategy(CriticBaseStrategy):
             output_dict["code_answer"] = external_tool_info["code_answer"]
         return output_dict
 
-    def update_answer_based_on_critique(self, llm, question: str, examples: str, answer: str, critique: str, prompt: str, additional_keys: Dict[str, str]) -> str:
+    def update_answer_based_on_critique(
+        self, 
+        question: str, 
+        examples: str, 
+        answer: str, 
+        critique: str, 
+        prompt: str, 
+        additional_keys: Dict[str, str],
+        **kwargs
+    ) -> str:
         return _prompt_critique(
-            llm=llm,
+            llm=self.llm,
             question=question,
             examples=examples,
             answer=answer,
@@ -61,3 +92,6 @@ class MathStrategy(CriticBaseStrategy):
 
     def halting_condition(self, critique: str) -> bool:
         return "is correct." in critique.lower()
+
+    def reset(self) -> bool:
+        pass
