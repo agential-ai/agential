40f240a62d4579de1fda4b26e70ce3c7af5cd4fa
alckasoc <tuvincent0106@gmail.com>
2024-05-22 20:47:13 -0700

    fixed up some logic

--- agential/cog/strategies/critic/code_strategy.py
+++ agential/cog/strategies/critic/code_strategy.py
@@ -2,6 +2,11 @@ from typing import Dict
 from agential.cog.functional.critic import _prompt_agent, _prompt_critique, safe_execute
 from agential.cog.strategies.critic.base import CriticBaseStrategy
 
+def validate_overlapping_keys(dict_1: Dict[str, str], dict_2: Dict[str, str]) -> bool:
+    overlapping_keys = dict_1.keys() & dict_2.keys()
+    if overlapping_keys:
+        raise ValueError(f"Overlapping keys detected: {overlapping_keys}")
+
 class CodeStrategy(CriticBaseStrategy):
     def __init__(self, llm):
         self.llm = llm
@@ -38,24 +43,28 @@ class CodeStrategy(CriticBaseStrategy):
         max_interactions: int,
         **kwargs
     ):
-        if "tests" not in kwargs:
-            raise ValueError("The 'tests' parameter must be specified in the kwargs.")
-        tests = kwargs["tests"]
-
         external_tool_info = {}
         if use_interpreter_tool:
+            if "tests" not in additional_keys:
+                raise ValueError("The 'tests' parameter must be specified in the `additional_keys`.")
+            tests = additional_keys["tests"]
+
             _, execution_status = safe_execute(f"{answer}\n\n{tests}")
             external_tool_info = {
                 "execution_status": execution_status,
             }
 
+            validate_overlapping_keys(additional_keys, external_tool_info)
+            
+        additional_keys.update(external_tool_info)
+
         new_critique = _prompt_critique(
             llm=self.llm,
-            question=answer,
+            question=question,
             examples=examples,
-            answer=tests,
+            answer=answer,
             critique="",
-            additional_keys=external_tool_info if external_tool_info else additional_keys,
+            additional_keys=additional_keys,
             prompt=prompt,
         ).split("Here's")[0]
 
@@ -76,17 +85,16 @@ class CodeStrategy(CriticBaseStrategy):
         external_tool_info: Dict[str, str],
         **kwargs
     ) -> str:
-        if "tests" not in kwargs:
-            raise ValueError("The 'tests' parameter must be specified in the kwargs.")
-        tests = kwargs["tests"]
-
+        validate_overlapping_keys(additional_keys, external_tool_info)
+        additional_keys.update(external_tool_info)
+        
         new_answer = _prompt_critique(
             llm=self.llm,
-            question=answer,
+            question=question,
             examples=examples,
-            answer=tests,
+            answer=answer,
             critique=f"{critique}\n\nHere's a better solution:\n```python\n",
-            additional_keys=external_tool_info if external_tool_info else additional_keys,
+            additional_keys=additional_keys,
             prompt=prompt,
         )
         new_answer = new_answer.split("```python")[-1].split("```")[0].strip()
