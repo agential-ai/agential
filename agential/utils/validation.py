21089e80509d7bfe368de46d185fdcc6ef62be76
alckasoc <tuvincent0106@gmail.com>
2024-05-22 22:26:38 -0700

    .

---
+++ agential/utils/validation.py
@@ -0,0 +1,6 @@
+from typing import Dict
+
+def validate_overlapping_keys(dict_1: Dict[str, str], dict_2: Dict[str, str]) -> bool:
+    overlapping_keys = dict_1.keys() & dict_2.keys()
+    if overlapping_keys:
+        raise ValueError(f"Overlapping keys detected: {overlapping_keys}")
