30ab44b2ce17a4c5379ec9a65bf5c53e63632a42
alckasoc <tuvincent0106@gmail.com>
2024-05-25 04:05:55 -0700

    finished test_generate_critique

--- agential/cog/functional/critic.py
+++ agential/cog/functional/critic.py
@@ -126,6 +126,8 @@ def _prompt_agent(
             )
         ]
     ).content
+    print("ANSWER")
+    print(repr(out))
     assert isinstance(out, str)
     return out
 
@@ -199,5 +201,6 @@ def _prompt_critique(
             )
         ]
     ).content
+    print(repr(out))
     assert isinstance(out, str)
     return out
