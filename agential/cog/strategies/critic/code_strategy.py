f1345b73912ad8accd34c2ecb52ac09822f0647a
alckasoc <tuvincent0106@gmail.com>
2024-05-25 23:35:34 -0700

    add docstrings

--- agential/cog/strategies/critic/code_strategy.py
+++ agential/cog/strategies/critic/code_strategy.py
@@ -1,4 +1,6 @@
-from typing import Dict
+from typing import Dict, Tuple, Any
+
+from langchain_core.language_models.chat_models import BaseChatModel
 
 from agential.cog.functional.critic import _prompt_agent, _prompt_critique, safe_execute
 from agential.cog.strategies.critic.base import CriticBaseStrategy
@@ -6,7 +8,13 @@ from agential.utils.validation import validate_overlapping_keys
 
 
 class CriticCodeStrategy(CriticBaseStrategy):
-    def __init__(self, llm):
+    """A strategy class for Code benchmarks using the CRITIC agent.
+
+    Attributes:
+        llm (BaseChatModel): The language model used for generating answers and critiques.
+    """
+    def __init__(self, llm: BaseChatModel) -> None:
+        """Initialization."""
         self.llm = llm
         self._halt = False
 
@@ -17,6 +25,17 @@ class CriticCodeStrategy(CriticBaseStrategy):
         prompt: str,
         additional_keys: Dict[str, str],
     ) -> str:
+        """Generates an answer for the given question using the provided prompt and examples.
+
+        Args:
+            question (str): The math question to generate an answer for.
+            examples (str): Few-shot examples to guide the language model.
+            prompt (str): The prompt to generate an answer.
+            additional_keys (Dict[str, str]): Additional keys for the prompt.
+
+        Returns:
+            str: The generated answer.
+        """
         answer = _prompt_agent(
             llm=self.llm,
             question=question,
@@ -40,7 +59,36 @@ class CriticCodeStrategy(CriticBaseStrategy):
         use_tool: bool,
         max_interactions: int,
         **kwargs,
-    ):
+    ) -> Tuple[str, Dict[str, Any]]:
+        """Generates a critique for the provided answer using the given prompt and examples.
+
+        This method does the following:
+            1. Initializes an empty dictionary for external tool information.
+            2. If `use_tool` is True:
+                a. Checks if "tests" is in `additional_keys` and raises a ValueError if not.
+                b. Executes the answer as code along with the provided tests.
+                c. If the execution status is "Done", sets the `_halt` flag to True.
+                d. Updates the external tool information with the execution status.
+                e. Validates and merges additional keys with external tool information.
+            3. Copies the additional keys and updates them with external tool information.
+            4. Generates a new critique using the updated answer and keys.
+            5. Returns the new critique and external tool information.
+            
+        Args:
+            idx (int): The index of the current interaction.
+            question (str): The math question that was answered.
+            examples (str): Few-shot examples to guide the critique.
+            answer (str): The answer to critique.
+            critique (str): Existing critique to build upon.
+            prompt (str): The prompt to generate a critique.
+            additional_keys (Dict[str, str]): Additional keys for the prompt.
+            use_tool (bool): Whether to use an external tool during critique.
+            max_interactions (int): The maximum number of interactions allowed.
+            **kwargs: Additional arguments for specific implementations.
+
+        Returns:
+            Tuple[str, Dict[str, Any]]: The generated critique and external tool information.
+        """
         external_tool_info = {}
         if use_tool:
             if "tests" not in additional_keys:
@@ -76,6 +124,16 @@ class CriticCodeStrategy(CriticBaseStrategy):
     def create_output_dict(
         self, answer: str, critique: str, external_tool_info: Dict[str, str]
     ) -> Dict[str, str]:
+        """Creates an output dictionary containing the answer, critique, and external tool information.
+
+        Args:
+            answer (str): The generated answer.
+            critique (str): The generated critique.
+            external_tool_info (Dict[str, str]): Information from external tool execution.
+
+        Returns:
+            Dict[str, str]: The output dictionary.
+        """
         output_dict = {"code": answer, "critique": critique, **external_tool_info}
         return output_dict
 
@@ -90,6 +148,20 @@ class CriticCodeStrategy(CriticBaseStrategy):
         external_tool_info: Dict[str, str],
         **kwargs,
     ) -> str:
+        """Updates the answer based on the given critique.
+
+        Args:
+            question: The question that was answered by the language model.
+            examples: Few-shot examples to guide the language model.
+            answer: The answer provided by the language model.
+            critique: The critique of the answer.
+            prompt: The prompt to be used for generating the updated answer.
+            additional_keys: Additional context or parameters to include in the critique prompt.
+            external_tool_info: Information from any external tool used.
+
+        Returns:
+            str: The updated answer.
+        """
         validate_overlapping_keys(additional_keys, external_tool_info)
         additional_keys = additional_keys.copy()
         additional_keys.update(external_tool_info)
@@ -108,14 +180,33 @@ class CriticCodeStrategy(CriticBaseStrategy):
         return new_answer
 
     def halting_condition(self, critique: str) -> bool:
+        """Checks if the halting condition has been met.
+
+        Returns True if the CRITIC Agent's generated answer has an `execution_status="Done"`.
+
+        Args:
+            critique: The current critique of the answer.
+
+        Returns:
+            bool: True if the halting condition has been met, False otherwise.
+        """
         return self._halt
 
     def reset(self) -> bool:
+        """Resets the strategy to its initial state.
+
+        Resets internal variables keeping track of halting and answer history.
+
+        Returns:
+            bool: True if the reset was successful, False otherwise.
+        """
         self._answer_history = []
         self._halt = False
 
 
 class CritMBPPCodeStrategy(CriticCodeStrategy):
+    """A strategy class for the MBPP benchmark using the CRITIC agent."""
+
     pass
 
 
