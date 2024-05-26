3ec8d56d03fb7b86bd094ad74ee3d77d31135c32
alckasoc <tuvincent0106@gmail.com>
2024-05-25 20:28:02 -0700

    finished test_heval_update_answer_based_on_critique

--- tests/cog/strategies/critic/test_code_strategy.py
+++ tests/cog/strategies/critic/test_code_strategy.py
@@ -154,5 +154,31 @@ def test_heval_generate_critique() -> None:
 def test_heval_update_answer_based_on_critique() -> None:
     """Tests CritHEvalCodeStrategy update_answer_based_on_critique."""
 
+    inst = {"task_id": "HumanEval/0", "prompt": "from typing import List\n\n\ndef has_close_elements(numbers: List[float], threshold: float) -> bool:\n    \"\"\" Check if in given list of numbers, are any two numbers closer to each other than\n    given threshold.\n    >>> has_close_elements([1.0, 2.0, 3.0], 0.5)\n    False\n    >>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)\n    True\n    \"\"\"\n", "entry_point": "has_close_elements", "canonical_solution": "    for idx, elem in enumerate(numbers):\n        for idx2, elem2 in enumerate(numbers):\n            if idx != idx2:\n                distance = abs(elem - elem2)\n                if distance < threshold:\n                    return True\n\n    return False\n", "test": "\n\nMETADATA = {\n    'author': 'jt',\n    'dataset': 'test'\n}\n\n\ndef check(candidate):\n    assert candidate([1.0, 2.0, 3.9, 4.0, 5.0, 2.2], 0.3) == True\n    assert candidate([1.0, 2.0, 3.9, 4.0, 5.0, 2.2], 0.05) == False\n    assert candidate([1.0, 2.0, 5.9, 4.0, 5.0], 0.95) == True\n    assert candidate([1.0, 2.0, 5.9, 4.0, 5.0], 0.8) == False\n    assert candidate([1.0, 2.0, 3.0, 4.0, 5.0, 2.0], 0.1) == True\n    assert candidate([1.1, 2.2, 3.1, 4.1, 5.1], 1.0) == True\n    assert candidate([1.1, 2.2, 3.1, 4.1, 5.1], 0.5) == False\n\n"}
+    question = inst['prompt']
+    answer = '    for i in range(len(numbers)):\n        for j in range(i+1, len(numbers)):\n            if abs(numbers[i] - numbers[j]) < threshold:\n                return True\n    return False'
+    tests = f"{inst['test']}\ncheck({inst['entry_point']})"
+    
+    gt_critique = '    for i in range(len(numbers)):\n        for j in range(i+1, len(numbers)):\n            if abs(numbers[i] - numbers[j]) < threshold:\n                return True\n    return False'
+    gt_new_answer = '    for i in range(len(numbers)):\n        for j in range(i+1, len(numbers)):\n            if abs(numbers[i] - numbers[j]) < threshold:\n                return True\n    return False'
+    responses = [
+        '```python\n    for i in range(len(numbers)):\n        for j in range(i+1, len(numbers)):\n            if abs(numbers[i] - numbers[j]) < threshold:\n                return True\n    return False\n```'
+    ]
+    critique = 'There is no problem with the provided code. The `has_close_elements` function correctly checks if there are any two numbers in the list that are closer to each other than the given threshold. The function uses a nested loop to compare all pairs of numbers in the list and returns `True` if it finds any pair that meets the condition. The test cases provided in the `check` function also cover a variety of scenarios to verify the correctness of the implementation.'
+    llm = FakeListChatModel(responses=responses)
+    strategy = CritHEvalCodeStrategy(llm=llm)
+    new_answer = strategy.update_answer_based_on_critique(
+        question=question,
+        examples=HUMANEVAL_FEWSHOT_EXAMPLES_CRITIC,
+        answer=answer,
+        critique=critique,
+        prompt=CRITIC_CRITIQUE_INSTRUCTION_HUMANEVAL,
+        additional_keys={"tests": tests},
+        external_tool_info={"execution_status": "Done"}
+    )
+    assert new_answer == gt_new_answer
+    assert not strategy._halt
+
+
 def test_heval_reset() -> None:
     """Tests CritHEvalCodeStrategy reset."""
