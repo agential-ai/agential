344eccde220ca4ce55ab7abae6cb351666865b09
alckasoc <tuvincent0106@gmail.com>
2024-05-25 18:16:26 -0700

    test update ans base don crit

--- agential/cog/strategies/critic/math_strategy.py
+++ agential/cog/strategies/critic/math_strategy.py
@@ -111,6 +111,12 @@ class CriticMathStrategy(CriticBaseStrategy):
         external_tool_info: Dict[str, str],
         **kwargs,
     ) -> str:
+        # print("--------------------------------------")
+        # print(repr(answer))
+        # print(repr(critique))
+        # print(external_tool_info)
+        # print("--------------------------------------")
+
         validate_overlapping_keys(additional_keys, external_tool_info)
         additional_keys = additional_keys.copy()
         additional_keys.update(external_tool_info)
