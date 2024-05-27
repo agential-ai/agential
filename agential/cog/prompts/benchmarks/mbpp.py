ae2e23fa02df73447ce658912aed5af0fce735a0
alckasoc <tuvincent0106@gmail.com>
2024-05-27 02:25:01 -0700

    cleaned up fewshots

--- agential/cog/prompts/benchmarks/mbpp.py
+++ agential/cog/prompts/benchmarks/mbpp.py
@@ -39,6 +39,7 @@ def is_not_prime(n):
 
 You are an expert Python programmer, and here is your task: Write a function to find the n largest integers from a given list of numbers, returned in descending order.
 Your code should pass these tests:
+
 assert heap_queue_largest([25, 35, 22, 85, 14, 65, 75, 22, 58], 3) == [85, 75, 65]
 assert heap_queue_largest([25, 35, 22, 85, 14, 65, 75, 22, 58], 2) == [85, 75]
 assert heap_queue_largest([25, 35, 22, 85, 14, 65, 75, 22, 58], 5) == [85, 75, 65, 58, 35]
