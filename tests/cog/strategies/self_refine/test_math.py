8b15ae7ef290e1c508a741617b4b174b4bac07cb
alckasoc <tuvincent0106@gmail.com>
2024-05-26 23:15:21 -0700

    added skeleton unit test for strat

---
+++ tests/cog/strategies/self_refine/test_math.py
@@ -0,0 +1,31 @@
+"""Unit tests for Self-Refine math strategies."""
+
+from agential.cog.strategies.self_refine.math import (
+    SelfRefineGSM8KStrategy,
+    SelfRefineMathStrategy
+)
+
+
+def test_init() -> None:
+    """Test CriticQAStrategy initialization."""
+
+def test_generate() -> None:
+    """Tests SelfRefineMathStrategy generate."""
+
+def test_generate_critique() -> None:
+    """Tests SelfRefineMathStrategy generate_critique."""
+
+def test_create_output_dict() -> None:
+    """Tests SelfRefineMathStrategy create_output_dict."""
+
+def test_update_answer_based_on_critique() -> None:
+    """Tests SelfRefineMathStrategy update_answer_based_on_critique."""
+
+def test_halting_condition() -> None:
+    """Tests SelfRefineMathStrategy halting_condition."""
+
+def test_reset() -> None:
+    """Tests SelfRefineMathStrategy reset."""
+
+def test_instantiate_strategies() -> None:
+    """Test instantiate all Math strategies."""
