d57cd3a62e4f3405798a66afd9ab299875fdba19
alckasoc <tuvincent0106@gmail.com>
2024-05-26 17:18:04 -0700

    moved fever prompts

--- agential/cog/prompts/react.py
+++ agential/cog/prompts/react.py
@@ -30,33 +30,6 @@ Here are some examples:
 
 Question: {question}{scratchpad}"""
 
-FEVER_FEWSHOT_EXAMPLES = """Claim: Nikolaj Coster-Waldau worked with the Fox Broadcasting Company.
-Thought 1: I need to search Nikolaj Coster-Waldau and find if he has worked with the Fox Broadcasting Company.
-Action 1: Search[Nikolaj Coster-Waldau]
-Observation 1: Nikolaj William Coster-Waldau (born 27 July 1970) is a Danish actor and producer. He graduated from the Danish National School of Performing Arts in Copenhagen in 1993,[1] and had his breakthrough role in Denmark with the film Nightwatch (1994). He played Jaime Lannister in the HBO fantasy drama series Game of Thrones, for which he received two Primetime Emmy Award nominations for Outstanding Supporting Actor in a Drama Series.. Coster-Waldau has appeared in numerous films in his native Denmark and Scandinavia, including Headhunters (2011) and A Thousand Times Good Night (2013). In the U.S, his debut film role was in the war film Black Hawk Down (2001), playing Medal of Honor recipient Gary Gordon.[2] He then played a detective in the short-lived Fox television series New Amsterdam (2008), and appeared in the 2009 Fox television film Virtuality, originally intended as a pilot.
-Thought 2: Because he "appeared in the 2009 Fox television film Virtuality", he should have worked with the Fox Broadcasting Company.
-Action 2: Finish[SUPPORTS]
-
-Claim: Stranger Things is set in Bloomington, Indiana.
-Thought 1: I should search for Stranger Things, and see if it is set in Bloomington, Indiana.
-Action 1: Search[Stranger Things]
-Observation 1: Stranger Things is an American science fiction horror drama television series created by the Duffer Brothers. Set in the 1980s, primarily in the fictional town of Hawkins, Indiana, the series centers on a number of mysteries and supernatural events occurring around the town and their impact on an ensemble of child and adult characters. 
-Thought 2: The observation says that it is set in a "fictional town of Hawkins, Indiana", so it is not set in Bloomington.
-Action 2: Finish[REFUTES]
-
-Claim: Beautiful reached number two on the Billboard Hot 100 in 2003.?
-Thought 1: I need to search the song Beautiful and find if it reached number two on the Billboard Hot 100 in 2003.
-Action 1: Search[Beautiful]
-Observation 1: Could not find [Beautiful]. Similar: ['Beautiful', 'Beautiful, Beautiful', 'A Beautiful Mind (film)', 'Beautiful (Christina Aguilera song)', 'Life Is Beautiful'].
-Thought 2: From suggestions, I should search "Beautiful (Christina Aguilera song)" to find the song.
-Action 2: Search[Beautiful (Christina Aguilera song)]
-Observation 2: "Beautiful" is a song recorded by American singer Christina Aguilera for her fourth studio album, Stripped (2002).
-Thought 3: It does not mention Billboard, so I need to look up "Billboard Hot 100" to find if it reached number two on it in 2003.
-Action 3: Lookup[Billboard Hot 100]
-Observation 3: (Result 1 / 3) The song peaked at number two on the Billboard Hot 100 in the United States, where it was certified Gold for 500,000 units shipped.
-Thought 4: It only says the song peaked at number two on the Billboard Hot 100, but not if it was in 2003. I am not sure if this claim is true or not.
-Action 4: Finish[NOT ENOUGH INFO]"""
-
 
 # ======================================================================== AMBIGQA ======================================================================== #
 
