077ea48a05bbb8e72337106fca4628bec42033f5
alckasoc <tuvincent0106@gmail.com>
2024-05-27 00:35:40 -0700

    auto linted

--- tests/cog/agent/test_self_refine.py
+++ tests/cog/agent/test_self_refine.py
@@ -4,23 +4,20 @@ from langchain_community.chat_models.fake import FakeListChatModel
 from langchain_core.language_models.chat_models import BaseChatModel
 
 from agential.cog.agent.self_refine import SelfRefineAgent
-from agential.cog.strategies.self_refine.base import SelfRefineBaseStrategy
 from agential.cog.prompts.benchmarks.gsm8k import GSM8K_FEWSHOT_EXAMPLES_POT
 from agential.cog.prompts.self_refine import (
+    GSM8K_CRITIQUE_FEWSHOT_EXAMPLES,
+    GSM8K_REFINE_FEWSHOT_EXAMPLES,
     SELF_REFINE_CRITIQUE_INSTRUCTION_GSM8K,
     SELF_REFINE_INSTRUCTION_GSM8K,
     SELF_REFINE_REFINE_INSTRUCTION_GSM8K,
-    GSM8K_CRITIQUE_FEWSHOT_EXAMPLES,
-    GSM8K_REFINE_FEWSHOT_EXAMPLES
 )
+from agential.cog.strategies.self_refine.base import SelfRefineBaseStrategy
 
 
 def test_init() -> None:
     """Test initialization."""
-    agent = SelfRefineAgent(
-        llm=FakeListChatModel(responses=[]), 
-        mode={"math": "gsm8k"}
-    )
+    agent = SelfRefineAgent(llm=FakeListChatModel(responses=[]), mode={"math": "gsm8k"})
     assert isinstance(agent.llm, BaseChatModel)
     assert isinstance(agent.strategy, SelfRefineBaseStrategy)
     assert agent.mode == {"math": "gsm8k"}
@@ -28,10 +25,7 @@ def test_init() -> None:
 
 def test_reset() -> None:
     """Test reset."""
-    agent = SelfRefineAgent(
-        llm=FakeListChatModel(responses=[]), 
-        mode={"math": "gsm8k"}
-    )
+    agent = SelfRefineAgent(llm=FakeListChatModel(responses=[]), mode={"math": "gsm8k"})
     agent.strategy._halt = True
     agent.reset()
     assert not agent.strategy._halt
@@ -43,29 +37,28 @@ def test_generate() -> None:
 
     gt_out = [
         {
-            'code': 'blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts',
-            'critique': 'The error in the code is in the calculation of the white fiber needed for the robe. Since the robe takes half as much white fiber as blue fiber, the calculation for white fiber should be `white_fiber = blue_fiber / 2`, not `white_fiber = blue_fiber * 2`. This error affects the total number of bolts calculation as well. The correct calculation should be `total_bolts = blue_fiber + white_fiber`.'
+            "code": "blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts",
+            "critique": "The error in the code is in the calculation of the white fiber needed for the robe. Since the robe takes half as much white fiber as blue fiber, the calculation for white fiber should be `white_fiber = blue_fiber / 2`, not `white_fiber = blue_fiber * 2`. This error affects the total number of bolts calculation as well. The correct calculation should be `total_bolts = blue_fiber + white_fiber`.",
         },
         {
-            'code': 'blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts',
-            'critique': 'The error in the code is in the calculation of the white fiber needed for the robe. The white fiber needed is not half of the blue fiber, but rather half of the blue fiber bolts. Therefore, the calculation for white fiber should be white_fiber = blue_fiber / 2, not white_fiber = blue_fiber / 2.'
+            "code": "blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts",
+            "critique": "The error in the code is in the calculation of the white fiber needed for the robe. The white fiber needed is not half of the blue fiber, but rather half of the blue fiber bolts. Therefore, the calculation for white fiber should be white_fiber = blue_fiber / 2, not white_fiber = blue_fiber / 2.",
         },
         {
-            'code': 'blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts',
-            'critique': 'The error in the code is that it incorrectly calculates the amount of white fiber needed for the robe. The question states that the robe takes half as much white fiber as blue fiber, so the calculation for white fiber should be `white_fiber = blue_fiber / 2` instead of `white_fiber = blue_fiber * 2`.'
-        }
+            "code": "blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts",
+            "critique": "The error in the code is that it incorrectly calculates the amount of white fiber needed for the robe. The question states that the robe takes half as much white fiber as blue fiber, so the calculation for white fiber should be `white_fiber = blue_fiber / 2` instead of `white_fiber = blue_fiber * 2`.",
+        },
     ]
     responses = [
-        'blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts',
-        'The error in the code is in the calculation of the white fiber needed for the robe. Since the robe takes half as much white fiber as blue fiber, the calculation for white fiber should be `white_fiber = blue_fiber / 2`, not `white_fiber = blue_fiber * 2`. This error affects the total number of bolts calculation as well. The correct calculation should be `total_bolts = blue_fiber + white_fiber`.',
-        '```python\nblue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts\n```',
-        'The error in the code is in the calculation of the white fiber needed for the robe. The white fiber needed is not half of the blue fiber, but rather half of the blue fiber bolts. Therefore, the calculation for white fiber should be white_fiber = blue_fiber / 2, not white_fiber = blue_fiber / 2. ',
-        '```python\nblue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts\n```',
-        'The error in the code is that it incorrectly calculates the amount of white fiber needed for the robe. The question states that the robe takes half as much white fiber as blue fiber, so the calculation for white fiber should be `white_fiber = blue_fiber / 2` instead of `white_fiber = blue_fiber * 2`.'
+        "blue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts",
+        "The error in the code is in the calculation of the white fiber needed for the robe. Since the robe takes half as much white fiber as blue fiber, the calculation for white fiber should be `white_fiber = blue_fiber / 2`, not `white_fiber = blue_fiber * 2`. This error affects the total number of bolts calculation as well. The correct calculation should be `total_bolts = blue_fiber + white_fiber`.",
+        "```python\nblue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts\n```",
+        "The error in the code is in the calculation of the white fiber needed for the robe. The white fiber needed is not half of the blue fiber, but rather half of the blue fiber bolts. Therefore, the calculation for white fiber should be white_fiber = blue_fiber / 2, not white_fiber = blue_fiber / 2. ",
+        "```python\nblue_fiber = 2\nwhite_fiber = blue_fiber / 2\ntotal_bolts = blue_fiber + white_fiber\nanswer = total_bolts\n```",
+        "The error in the code is that it incorrectly calculates the amount of white fiber needed for the robe. The question states that the robe takes half as much white fiber as blue fiber, so the calculation for white fiber should be `white_fiber = blue_fiber / 2` instead of `white_fiber = blue_fiber * 2`.",
     ]
     agent = SelfRefineAgent(
-        llm=FakeListChatModel(responses=responses), 
-        mode={"math": "gsm8k"}
+        llm=FakeListChatModel(responses=responses), mode={"math": "gsm8k"}
     )
 
     out = agent.generate(
@@ -80,10 +73,10 @@ def test_generate() -> None:
         critique_additional_keys={},
         refine_additional_keys={},
         max_interactions=3,
-        reset=True
+        reset=True,
     )
 
     assert len(gt_out) == len(out)
     for gt_i, out_i in zip(gt_out, out):
-        assert gt_i['code'] == out_i['code']
-        assert gt_i['critique'] == out_i['critique']
+        assert gt_i["code"] == out_i["code"]
+        assert gt_i["critique"] == out_i["critique"]
