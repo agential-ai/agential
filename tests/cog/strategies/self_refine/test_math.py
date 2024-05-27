bfd49a9cc7ac9936fd82640206a223569f27a9cc
alckasoc <tuvincent0106@gmail.com>
2024-05-26 23:38:06 -0700

    half done with test gen crit

--- tests/cog/strategies/self_refine/test_math.py
+++ tests/cog/strategies/self_refine/test_math.py
@@ -6,7 +6,11 @@ from agential.cog.strategies.self_refine.math import (
     SelfRefineMathStrategy
 )
 from agential.cog.prompts.benchmarks.gsm8k import GSM8K_FEWSHOT_EXAMPLES_POT
-from agential.cog.prompts.self_refine import SELF_REFINE_INSTRUCTION_GSM8K
+from agential.cog.prompts.self_refine import (
+    SELF_REFINE_INSTRUCTION_GSM8K,
+    SELF_REFINE_CRITIQUE_INSTRUCTION_GSM8K,
+    GSM8K_CRITIQUE_FEWSHOT_EXAMPLES
+)
 
 
 def test_init() -> None:
@@ -37,6 +41,24 @@ def test_generate() -> None:
 
 def test_generate_critique() -> None:
     """Tests SelfRefineMathStrategy generate_critique."""
+    gt_critique = "The error in the code is that the result is hardcoded as 42 without actually calculating the total number of bolts needed for the robe. The code should calculate the total number of bolts required based on the information given in the question. Let's correct this:\n\n```python\nblue_bolts = 2\nwhite_bolts = blue_bolts / 2\ntotal_bolts = blue_bolts + white_bolts\nresult = total_bolts\n``` \n\nThis code snippet will correctly calculate the total number of bolts needed for the robe."
+    responses = [
+        "The error in the code is that the result is hardcoded as 42 without actually calculating the total number of bolts needed for the robe. The code should calculate the total number of bolts required based on the information given in the question. Let's correct this:\n\n```python\nblue_bolts = 2\nwhite_bolts = blue_bolts / 2\ntotal_bolts = blue_bolts + white_bolts\nresult = total_bolts\n``` \n\nThis code snippet will correctly calculate the total number of bolts needed for the robe."
+    ]
+    llm = FakeListChatModel(responses=responses)
+    strategy = SelfRefineMathStrategy(llm=llm)
+    question = "A robe takes 2 bolts of blue fiber and half that much white fiber.  How many bolts in total does it take?"
+    answer = "result = 42"
+    
+    critique = strategy.generate_critique(
+        question=question, 
+        examples=GSM8K_CRITIQUE_FEWSHOT_EXAMPLES, 
+        answer=answer, 
+        prompt=SELF_REFINE_CRITIQUE_INSTRUCTION_GSM8K, 
+        additional_keys={}
+    )
+    assert critique == gt_critique
+
 
 def test_create_output_dict() -> None:
     """Tests SelfRefineMathStrategy create_output_dict."""
