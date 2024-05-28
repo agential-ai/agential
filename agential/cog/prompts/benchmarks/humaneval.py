42c63adb4ac3f78fd265d0c2296fd0a34332f81a
alckasoc <tuvincent0106@gmail.com>
2024-05-27 17:52:51 -0700

    0 shot heval

--- agential/cog/prompts/benchmarks/humaneval.py
+++ agential/cog/prompts/benchmarks/humaneval.py
@@ -1,12 +1,3 @@
 """Few-shot examples for HumanEval."""
 
-HUMANEVAL_FEWSHOT_EXAMPLES_POT = """```python
-def has_duplicate_names(names_list: List[str]) -> bool:
-    \"\"\"Check if there is any name that appears more than once in the list.
-    >>> has_duplicate_names(['Alice', 'Bob', 'Charlie', 'Alice'])
-    True
-    >>> has_duplicate_names(['Alice', 'Bob', 'Charlie', 'Dave']) 
-    False
-    \"\"\"
-    return len(names_list) != len(set(names_list))
-```"""
+HUMANEVAL_FEWSHOT_EXAMPLES_POT = """"""
