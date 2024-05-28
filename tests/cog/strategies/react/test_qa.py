4ad118158d2d647449929def29bf918d1ccd6106
alckasoc <tuvincent0106@gmail.com>
2024-05-28 00:17:46 -0700

    scaffold test for qa strat react

--- tests/cog/strategies/react/test_qa.py
+++ tests/cog/strategies/react/test_qa.py
@@ -1,3 +1,29 @@
 """Unit tests for ReAct QA strategies."""
 
 
+from agential.cog.strategies.react.qa import ReActQAStrategy
+
+
+def test_init() -> None:
+    """Test ReActQAStrategy initialization."""
+
+def test_generate() -> None:
+    """Tests ReActQAStrategy generate."""
+
+def test_generate_action() -> None:
+    """Tests ReActQAStrategy generate_action."""
+
+def test_generate_observation() -> None:
+    """Tests ReActQAStrategy generate_observation."""
+
+def test_create_output_dict() -> None:
+    """Tests ReActQAStrategy create_output_dict."""
+
+def test_halting_condition() -> None:
+    """Tests ReActQAStrategy halting_condition."""
+
+def test_reset() -> None:
+    """Tests ReActQAStrategy reset."""
+
+def test_instantiate_strategies() -> None:
+    """Test instantiate all QA strategies."""
