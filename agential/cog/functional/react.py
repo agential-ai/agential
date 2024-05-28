09b58a6ad8b5cc160d33e880dcecb3e3d561e329
alckasoc <tuvincent0106@gmail.com>
2024-05-28 02:39:25 -0700

    delete tmp

--- agential/cog/functional/react.py
+++ agential/cog/functional/react.py
@@ -83,9 +83,6 @@ def _prompt_agent(
         additional_keys=additional_keys,
         prompt=prompt,
     )
-    print("<PROMPT AGENT=======================================================>")
-    print(prompt)
-    print("<PROMPT AGENT=======================================================>")
     out = llm(
         [
             HumanMessage(
@@ -93,9 +90,6 @@ def _prompt_agent(
             )
         ]
     ).content
-    print("<OUT AGENT=======================================================>")
-    print(repr(out))
-    print("<OUT AGENT=======================================================>")
     assert isinstance(out, str)
     return remove_newline(out)
 
