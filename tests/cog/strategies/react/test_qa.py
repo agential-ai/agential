a1234716a4e8a9f097ab387ca15a7166b7170a8b
alckasoc <tuvincent0106@gmail.com>
2024-05-28 00:19:24 -0700

    test init

--- tests/cog/strategies/react/test_qa.py
+++ tests/cog/strategies/react/test_qa.py
@@ -1,11 +1,27 @@
 """Unit tests for ReAct QA strategies."""
+from tiktoken import Encoding
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain.agents.react.base import DocstoreExplorer
 
+from langchain_community.chat_models.fake import FakeListChatModel
 
 from agential.cog.strategies.react.qa import ReActQAStrategy
 
 
 def test_init() -> None:
     """Test ReActQAStrategy initialization."""
+    llm = FakeListChatModel(responses=["1"])
+    strategy = ReActQAStrategy(llm=llm)
+    assert isinstance(strategy, ReActQAStrategy)
+    assert isinstance(strategy.llm, BaseChatModel)
+    assert strategy.max_steps == 6
+    assert strategy.max_tokens == 3896
+    assert isinstance(strategy.docstore, DocstoreExplorer)
+    assert isinstance(strategy.enc, Encoding)
+
+    assert strategy._step_n == 1
+    assert strategy._finished == False
+
 
 def test_generate() -> None:
     """Tests ReActQAStrategy generate."""
