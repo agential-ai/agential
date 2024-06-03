ac2c394e951c4b3479966685536c0ed059ae2038
alckasoc <tuvincent0106@gmail.com>
2024-06-03 04:12:19 -0700

    .

--- tests/cog/strategies/reflexion/test_qa.py
+++ tests/cog/strategies/reflexion/test_qa.py
@@ -7,6 +7,12 @@ from agential.cog.strategies.reflexion.qa import (
     parse_qa_action,
     ReflexionCoTQAStrategy,
 )
+from agential.cog.prompts.agent.reflexion import (
+    REFLEXION_COT_FEWSHOT_EXAMPLES,
+    REFLEXION_COT_INSTRUCTION,
+    REFLEXION_COT_REFLECT_FEWSHOT_EXAMPLES,
+    REFLEXION_COT_REFLECT_INSTRUCTION
+)
 
 
 def test_parse_qa_action() -> None:
@@ -31,7 +37,24 @@ def test_reflexion_cot_init() -> None:
 
 def test_reflexion_cot_generate() -> None:
     """Tests ReflexionCoTQAStrategy generate."""
+    question = "VIVA Media AG changed it's name in 2004. What does their new acronym stand for?"
+    key = "Gesellschaft mit beschränkter Haftung"
+    context = 'VIVA Media GmbH (until 2004 "VIVA Media AG") is a music television network originating from Germany. It was founded for broadcast of VIVA Germany as VIVA Media AG in 1993 and has been owned by their original concurrent Viacom, the parent company of MTV, since 2004. Viva channels exist in some European countries; the first spin-offs were launched in Poland and Switzerland in 2000.\n\nA Gesellschaft mit beschränkter Haftung (] , abbreviated GmbH ] and also GesmbH in Austria) is a type of legal entity very common in Germany, Austria, Switzerland (where it is equivalent to a S.à r.l.) and Liechtenstein. In the United States, the equivalent type of entity is the limited liability company (LLC). The name of the GmbH form emphasizes the fact that the owners ("Gesellschafter", also known as members) of the entity are not personally liable for the company\'s debts. "GmbH"s are considered legal persons under German and Austrian law. Other variations include mbH (used when the term "Gesellschaft" is part of the company name itself), and gGmbH ("gemeinnützige" GmbH) for non-profit companies.'
 
+    # Incorrect.
+    gt_out_scratchpad = '\nThought: The question is asking for the acronym that VIVA Media AG changed its name to in 2004. Based on the context, I know that VIVA Media AG is now known as VIVA Media GmbH. Therefore, the acronym "GmbH" stands for "Gesellschaft mit beschränkter Haftung" in German, which translates to "company with limited liability" in English.\nAction: Finish[Company with Limited Liability]\nObservation: Answer is INCORRECT'
+    responses=[
+        'The question is asking for the acronym that VIVA Media AG changed its name to in 2004. Based on the context, I know that VIVA Media AG is now known as VIVA Media GmbH. Therefore, the acronym "GmbH" stands for "Gesellschaft mit beschränkter Haftung" in German, which translates to "company with limited liability" in English.',
+        "Finish[Company with Limited Liability]",
+    ]
+    llm = FakeListChatModel(responses=responses)
+    strategy = ReflexionCoTQAStrategy(llm=llm)
+    out = strategy.generate(
+        question=question,
+        examples=, 
+        reflections=,
+        prompt=, 
+    )
 
 def test_reflexion_cot_generate_action() -> None:
     """Tests ReflexionCoTQAStrategy generate_action."""
