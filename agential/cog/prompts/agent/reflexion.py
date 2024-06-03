0772cd4c401b0cc5941fb2433bdcb73d834e8338
alckasoc <tuvincent0106@gmail.com>
2024-06-03 01:46:43 -0700

    removed context

--- agential/cog/prompts/agent/reflexion.py
+++ agential/cog/prompts/agent/reflexion.py
@@ -3,24 +3,7 @@
 # ======================================================================== HOTPOTQA ======================================================================== #
 
 
-# Fewshot CoT examples for prompting ReflexionCoT agent (think/act).
-REFLEXION_COT_FEWSHOT_EXAMPLES = """Relevant Context: The Nile River is the longest river in the world, spanning approximately 6,650 kilometers (4,132 miles) in length. It flows through eleven countries in northeastern Africa, including Egypt, Sudan, and Uganda.
-Question: What is the longest river in the world?
-Thought: The question asks for the longest river in the world, which I know is the Nile River based on the context provided.
-Action: Finish[Nile River]
-
-Relevant Context: Ludwig van Beethoven was a German composer and pianist who was a crucial figure in the transition between the Classical and Romantic eras in Western classical music. One of his most famous compositions is the Symphony No. 9, also known as the "Choral" symphony.
-Question: Which composer created Symphony No. 9?
-Thought: The question is asking for the composer of Symphony No. 9. Based on the context, I know that Ludwig van Beethoven composed this symphony.
-Action: Finish[Ludwig van Beethoven]
-
-Relevant Context: Photosynthesis is the process by which green plants and some other organisms convert light energy into chemical energy. During this process, plants absorb sunlight, carbon dioxide, and water to produce glucose and oxygen.
-Question: What do plants produce during photosynthesis?
-Thought: The question is asking about the products of photosynthesis. From the context, I know that plants produce glucose and oxygen during this process.
-Action: Finish[Glucose and Oxygen]"""
-
-# Fewshot CoT examples for prompting ReflexionCoT agent (think/act) with no context.
-REFLEXION_COT_FEWSHOT_EXAMPLES_NO_CONTEXT = """Question: What is the elevation range for the area that the eastern sector of the Colorado orogeny extends into?
+REFLEXION_COT_FEWSHOT_EXAMPLES = """Question: What is the elevation range for the area that the eastern sector of the Colorado orogeny extends into?
 Thought: Let's think step by step. The eastern sector of Colorado orogeny extends into the High Plains. High Plains rise in elevation from around 1,800 to 7,000 ft, so the answer is 1,800 to 7,000 ft.
 Action: Finish[1,800 to 7,000 ft]
 
@@ -44,23 +27,7 @@ Question: Were Pavel Urysohn and Leonid Levin known for the same type of work?
 Thought: Let's think step by step. Pavel Urysohn is a mathematician. Leonid Levin is a mathematician and computer scientist. So Pavel Urysohn and Leonid Levin have the same type of work.
 Action: Finish[Yes]"""
 
-# Fewshot CoT examples for ReflexionCoT reflection.
-REFLEXION_COT_REFLECT_FEWSHOT_EXAMPLES = """Relevant Context: Ernest Hemingway's novel "The Old Man and the Sea" tells the story of Santiago, an aging Cuban fisherman, who struggles to catch a giant marlin in the Gulf Stream. The book won the Pulitzer Prize for Fiction in 1953 and contributed to Hemingway's Nobel Prize for Literature in 1954.
-Question: Which literary award did "The Old Man and the Sea" contribute to Hemingway winning?
-Thought: The question is asking which award "The Old Man and the Sea" contributed to Hemingway winning. Based on the context, I know the novel won the Pulitzer Prize for Fiction and contributed to his Nobel Prize for Literature.
-Action: Finish[Pulitzer Prize for Fiction]
 
-Reflection: My answer was correct based on the context, but may not be the exact answer stored by the grading environment. Next time, I should try to provide a less verbose answer like "Pulitzer Prize" or "Nobel Prize."
-
-Context: On 14 October 1947, Chuck Yeager, a United States Air Force test pilot, became the first person to break the sound barrier by flying the Bell X-1 experimental aircraft at an altitude of 45,000 feet.
-Charles Elwood "Chuck" Yeager (13 February 1923 - 7 December 2020) was a United States Air Force officer, flying ace, and test pilot. He is best known for becoming the first person to break the sound barrier, which he achieved in the Bell X-1 aircraft named Glamorous Glennis. Yeager was also a distinguished fighter pilot during World War II and was credited with shooting down at least 12 enemy aircraft. In 1973, he was inducted into the National Aviation Hall of Fame for his significant contributions to aviation.
-Question: Who is the first person to break the sound barrier?
-Thought: The question is asking for the first person to break the sound barrier. From the context, I know that Chuck Yeager, a United States Air Force test pilot, was the first person to break the sound barrier.
-Action: Finish[Chuck Yeager]
-
-Reflection: Upon reflecting on the incorrect answer I provided, I realize that I may not have given the full name of the individual in question. In the context, both the given name and the nickname were mentioned, and I only used the nickname in my response. This could have been the reason my answer was deemed incorrect. Moving forward, when attempting this question again or similar questions, I will make sure to include the complete name of the person, which consists of their given name, any middle names, and their nickname (if applicable). This will help ensure that my answer is more accurate and comprehensive."""
-
-# Fewshot CoT examples for ReflexionCoT reflection with no context.
 REFLEXION_COT_REFLECT_FEWSHOT_EXAMPLES_NO_CONTEXT = """Question: What is the elevation range for the area that the eastern sector of the Colorado orogeny extends into?
 Thought: Let's think step by step. The eastern sector of Colorado orogeny extends into the Rocky Mountains. The Rocky Mountains rise in elevation from around 1,800 to 14,000 ft, so the answer is 1,800 to 14,000 ft.
 Action: Finish[1,800 to 14,000 ft]
@@ -73,20 +40,8 @@ Action: Finish[Mickey Mouse]
 
 Reflection: My reasoning for the naming of the character Milhouse in "The Simpsons" failed because I incorrectly assumed that the character was named after a prominent cartoon character, Mickey Mouse. In the future, when attempting this question, I should focus on better researching the background of "The Simpsons" and Matt Groening's influences in naming characters to avoid making incorrect assumptions. """
 
-# Prompt template for ReflexionCoT reflection.
-REFLEXION_COT_REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given access to relevant context and a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>] or there is a phrasing discrepancy with your provided answer and the answer key. In a few sentences, Diagnose a possible reason for failure or phrasing discrepancy and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.  
-Here are some examples:
-{examples}
-(END OF EXAMPLES)
 
-Previous trial:
-Relevant Context: {context}
-Question: {question}{scratchpad}
-
-Reflection:"""
-
-# Prompt template for ReflexionCoT reflection with no context.
-REFLEXION_COT_REFLECT_INSTRUCTION_NO_CONTEXT = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>] or there is a phrasing discrepancy with your provided answer and the answer key. In a few sentences, Diagnose a possible reason for failure or phrasing discrepancy and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.
+REFLEXION_COT_REFLECT_INSTRUCTION = """You are an advanced reasoning agent that can improve based on self refection. You will be given a previous reasoning trial in which you were given a question to answer. You were unsuccessful in answering the question either because you guessed the wrong answer with Finish[<answer>] or there is a phrasing discrepancy with your provided answer and the answer key. In a few sentences, Diagnose a possible reason for failure or phrasing discrepancy and devise a new, concise, high level plan that aims to mitigate the same failure. Use complete sentences.
 Here are some examples:
 {examples}
 (END OF EXAMPLES)
@@ -96,19 +51,8 @@ Question: {question}{scratchpad}
 
 Reflection:"""
 
-# Prompt template for ReflexionCoT agent (think/act).
-REFLEXION_COT_INSTRUCTION = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task. You will be given context that you should use to help you answer the question.
-Here are some examples:
-{examples}
-(END OF EXAMPLES)
 
-{reflections}
-
-Relevant Context: {context}
-Question: {question}{scratchpad}"""
-
-# Prompt template for ReflexionCoT agent (think/act) with no context.
-REFLEXION_COT_INSTRUCTION_NO_CONTEXT = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task.
+REFLEXION_COT_INSTRUCTION = """Solve a question answering task by having a Thought, then Finish with your answer. Thought can reason about the current situation. Finish[answer] returns the answer and finishes the task.
 Here are some examples:
 {examples}
 (END OF EXAMPLES)
