ed3cf1f5aeda9d2b98b24ad4d1e16ad8cee12ebe
alckasoc <tuvincent0106@gmail.com>
2024-06-02 17:45:58 -0700

    skeleton cot strat

---
+++ agential/cog/strategies/reflexion/qa.py
@@ -0,0 +1,146 @@
+"""Reflexion Agent strategies for QA."""
+
+from typing import Optional, Dict, Any, Tuple
+from agential.cog.modules.reflect.reflexion import (
+    ReflexionCoTReflector,
+)
+from agential.cog.strategies.reflexion.base import ReflexionCoTBaseStrategy
+from langchain_core.language_models.chat_models import BaseChatModel
+
+
+class ReflexioCoTQAStrategy(ReflexionCoTBaseStrategy):
+    """A strategy class for QA benchmarks using the ReflexionCoT agent.
+
+    Attributes:
+        llm (BaseChatModel): The language model used for generating answers and critiques.
+
+    """
+    def __init__(
+        self,
+        llm: BaseChatModel,
+        reflector: Optional[ReflexionCoTReflector] = None,
+        max_reflections: int = 3,
+        max_trials: int = 1,
+        patience: int = 1,
+    ) -> None:
+        """Initialization."""
+        super().__init__(llm)
+        self.llm = llm
+        self.max_reflections = max_reflections
+        self.max_trials = max_trials
+        self.patience = patience
+
+        if not reflector:
+            reflector = ReflexionCoTReflector(
+                llm=llm, max_reflections=max_reflections
+            )
+        self.reflector = reflector
+
+        self._trial_n = 0
+        self._scratchpad = ""
+        self._finished = False
+        self._answer = ""
+
+    def generate(
+        self,
+        question: str,
+        examples: str,
+        prompt: str,
+        additional_keys: Dict[str, str],
+        **kwargs: Dict[str, Any],
+    ) -> str:
+        """Generates a thought based on the question, examples, and prompt.
+
+        Args:
+            question (str): The question to be answered.
+            examples (str): Examples to guide the generation process.
+            prompt (str): The prompt used for generating the thought.
+            additional_keys (Dict[str, str]): Additional keys for the generation process.
+            **kwargs (Dict[str, Any]): Additional arguments.
+
+        Returns:
+            str: The generated thought.
+        """
+        pass
+
+    def generate_action(
+        self,
+        question: str,
+        examples: str,
+        prompt: str,
+        additional_keys: Dict[str, str],
+        **kwargs: Dict[str, Any],
+    ) -> Tuple[str, str]:
+        """Generates an action based on the question, examples, and prompt.
+
+        Args:
+            question (str): The question to be answered.
+            examples (str): Examples to guide the generation process.
+            prompt (str): The prompt used for generating the action.
+            additional_keys (Dict[str, str]): Additional keys for the generation process.
+            **kwargs (Dict[str, Any]): Additional arguments.
+
+        Returns:
+            Tuple[str, str]: The generated action type and query.
+        """
+        pass
+
+    def generate_observation(self, idx: int, action_type: str, query: str) -> str:
+        """Generates an observation based on the action type and query.
+
+        Args:
+            idx (int): The index of the observation.
+            action_type (str): The type of action to be performed.
+            query (str): The query for the action.
+
+        Returns:
+            str: The generated observation.
+        """
+        pass
+
+    def create_output_dict(
+        self, thought: str, action_type: str, query: str, obs: str
+    ) -> Dict[str, str]:
+        """Creates a dictionary of the output components.
+
+        Args:
+            thought (str): The generated thought.
+            action_type (str): The type of action performed.
+            query (str): The query for the action.
+            obs (str): The generated observation.
+
+        Returns:
+            Dict[str, str]: A dictionary containing the thought, action type, query, and observation.
+        """
+        pass
+
+    def halting_condition(
+        self,
+        idx: int,
+        question: str,
+        examples: str,
+        prompt: str,
+        additional_keys: Dict[str, str],
+        **kwargs: Dict[str, Any],
+    ) -> bool:
+        """Determines whether the halting condition has been met.
+
+        Args:
+            idx (int): The current step index.
+            question (str): The question being answered.
+            examples (str): Examples to guide the generation process.
+            prompt (str): The prompt used for generating the thought and action.
+            additional_keys (Dict[str, str]): Additional keys for the generation process.
+            **kwargs (Dict[str, Any]): Additional arguments.
+
+        Returns:
+            bool: True if the halting condition is met, False otherwise.
+        """
+        pass
+
+    def reset(self) -> None:
+        """Resets the internal state of the strategy.
+
+        Resets the scratchpad and the finished flag.
+        """
+        pass
