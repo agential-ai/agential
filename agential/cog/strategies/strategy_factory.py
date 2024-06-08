f7614803209d28f92246e4c063025694a51b614b
alckasoc <tuvincent0106@gmail.com>
2024-06-07 20:17:13 -0700

    add base class

--- agential/cog/strategies/strategy_factory.py
+++ agential/cog/strategies/strategy_factory.py
@@ -286,3 +286,66 @@ class ReflexionCoTStrategyFactory:
             raise ValueError(f"Unsupported mode: {mode}")
 
         return
+    
+
+class ReflexionReActStrategyFactory:
+    """A factory class for creating instances of different ReflexionCoT strategies based on the specified mode and benchmark.
+
+    Methods:
+        get_strategy(mode: Dict[str, str], **strategy_kwargs) -> ReflexionCoTBaseStrategy:
+            Returns an instance of the appropriate ReflexionCoT strategy based on the provided mode and benchmark.
+    """
+
+    @staticmethod
+    def get_strategy(
+        mode: Dict[str, str], **strategy_kwargs: Any
+    ) -> ReflexionCoTBaseStrategy:
+        """Returns an instance of the appropriate ReflexionCoT strategy based on the provided mode and benchmark.
+
+        Available modes:
+            - qa: "hotpotqa", "triviaqa", "ambignq", "fever"
+            - math: "gsm8k", "svamp", "tabmwp"
+            - code: "mbpp", "humaneval"
+
+        Args:
+            mode (Dict[str, str]): A dictionary specifying the mode and benchmark.
+                Example: {"qa": "hotpotqa"}, {"math": "gsm8k"}, {"code": "mbpp"}.
+            **strategy_kwargs (Dict[str, Any]): Additional keyword arguments to pass to the strategy's constructor.
+
+        Returns:
+            ReflexionCoTBaseStrategy: An instance of the appropriate ReflexionCoT strategy.
+
+        Raises:
+            ValueError: If the mode or benchmark is unsupported.
+        """
+        if "qa" in mode:
+            if mode["qa"] == "hotpotqa":
+                return ReflexionCoTHotQAStrategy(**strategy_kwargs)
+            elif mode["qa"] == "triviaqa":
+                return ReflexionCoTTriviaQAStrategy(**strategy_kwargs)
+            elif mode["qa"] == "ambignq":
+                return ReflexionCoTAmbigNQStrategy(**strategy_kwargs)
+            elif mode["qa"] == "fever":
+                return ReflexionCoTFEVERStrategy(**strategy_kwargs)
+            else:
+                raise ValueError(f"Unsupported QA benchmark: {mode['qa']}")
+        elif "math" in mode:
+            if mode["math"] == "gsm8k":
+                pass
+            elif mode["math"] == "svamp":
+                pass
+            elif mode["math"] == "tabmwp":
+                pass
+            else:
+                raise ValueError(f"Unsupported Math benchmark: {mode['math']}")
+        elif "code" in mode:
+            if mode["code"] == "mbpp":
+                pass
+            elif mode["code"] == "humaneval":
+                pass
+            else:
+                raise ValueError(f"Unsupported Code benchmark: {mode['code']}")
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+
+        return
