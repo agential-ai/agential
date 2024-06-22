7fa164ab6535b4fba3012b77aa644fb526cdd0e0
alckasoc <tuvincent0106@gmail.com>
2024-06-22 16:59:04 -0700

    bug fix; fever done

--- agential/cog/prompts/agent/react.py
+++ agential/cog/prompts/agent/react.py
@@ -21,6 +21,7 @@ Question: {question}{scratchpad}"""
 
 REACT_INSTRUCTION_FEVER = """Determine if there is Observation that SUPPORTS or REFUTES a Claim, or if there is NOT ENOUGH INFORMATION. Thought can reason about the current situation, and Action can be two types: 
 (1) Search[entity], which searches the exact entity on Wikipedia and returns the first paragraph if it exists. If not, it will return some similar entities to search.
+(2) Lookup[keyword], which returns the next sentence containing keyword in the last passage successfully found by Search.
 (3) Finish[answer], which returns the answer and finishes the task.
 You have a maximum of {max_steps} steps.
 
@@ -36,6 +37,7 @@ Claim: {question}{scratchpad}"""
 
 REACT_INSTRUCTION_AMBIGNQ = """Solve a question answering task with interleaving Thought, Action, Observation steps. Thought can reason about the current situation, and Action can be two types:
 (1) Search[entity], which searches the exact entity on Wikipedia and returns the first paragraph if it exists. If not, it will return some similar entities to search.
+(2) Lookup[keyword], which returns the next sentence containing keyword in the last passage successfully found by Search.
 (3) Finish[answer], which returns the answer and finishes the task.
 You have a maximum of {max_steps} steps.
 
