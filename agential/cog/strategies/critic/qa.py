8e5cb5ae7ce74b36c6a46776e124b96baedada87
emmalin-7 <73147873+emmalin-7@users.noreply.github.com>
2024-06-27 02:17:15 -0700

    Update qa.py

--- agential/cog/strategies/critic/qa.py
+++ agential/cog/strategies/critic/qa.py
@@ -203,7 +203,7 @@ class CriticQAStrategy(CriticBaseStrategy):
         
 
     def create_output_dict(
-        self, answer: str, critique: str, external_tool_info: Dict[str, str]
+        self, answer: str, critique: str, external_tool_info: Dict[str, Any]
     ) -> Dict[str, Any]:
         """Creates a dictionary containing the answer and critique, along with any additional key updates.
 
@@ -214,7 +214,7 @@ class CriticQAStrategy(CriticBaseStrategy):
         Args:
             answer (str): The original answer.
             critique (str): The generated critique.
-            external_tool_info (Dict[str, str]): Information from any external tools used during the critique.
+            external_tool_info (Dict[str, Any]): Information from any external tools used during the critique.
 
         Returns:
             Dict[str, Any]: A dictionary containing the answer, critique, and additional key updates.
@@ -222,7 +222,7 @@ class CriticQAStrategy(CriticBaseStrategy):
         output_dict = {
             "answer": answer if not self._halt else critique,
             "critique": critique,
-            **external_tool_info,
+            "external_tool_info": external_tool_info,
         }
         return output_dict
 
