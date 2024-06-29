a7f0d378fa89d660730e2a8a841e07e43c361d61
alckasoc <tuvincent0106@gmail.com>
2024-06-29 16:15:26 -0700

    func sig

--- agential/cog/strategies/reflexion/code.py
+++ agential/cog/strategies/reflexion/code.py
@@ -1,5 +1,11 @@
 """Reflexion Agent strategies for Code."""
+from typing import Any, Dict, List, Optional, Tuple
+from langchain_core.language_models.chat_models import BaseChatModel
 
+from agential.cog.modules.reflect.reflexion import (
+    ReflexionCoTReflector,
+    ReflexionReActReflector,
+)
 from agential.cog.strategies.reflexion.base import (
     ReflexionCoTBaseStrategy,
     ReflexionReActBaseStrategy,
@@ -7,8 +13,74 @@ from agential.cog.strategies.reflexion.base import (
 
 
 class ReflexionCoTCodeStrategy(ReflexionCoTBaseStrategy):
-    pass
+    def __init__(
+        self,
+        llm: BaseChatModel,
+        reflector: Optional[ReflexionCoTReflector] = None,
+        max_reflections: int = 3,
+        max_trials: int = 1,
+    ) -> None:
+        pass
 
+    def generate(
+        self,
+        question: str,
+        examples: str,
+        reflections: str,
+        prompt: str,
+        additional_keys: Dict[str, str],
+        **kwargs: Any,
+    ) -> str:
+        pass
+
+    def generate_action(
+        self,
+        question: str,
+        examples: str,
+        reflections: str,
+        prompt: str,
+        additional_keys: Dict[str, str],
+        **kwargs: Any,
+    ) -> Tuple[str, str]:
+        pass
+
+    def generate_observation(
+        self, action_type: str, query: str, key: str
+    ) -> Tuple[bool, str]:
+        pass
+
+    def create_output_dict(
+        self,
+        thought: str,
+        action_type: str,
+        obs: str,
+        is_correct: bool,
+        reflections: List[str],
+    ) -> Dict[str, Any]:
+        pass
+
+    def halting_condition(self, idx: int, key: str, **kwargs: Any) -> bool:
+        pass
+
+    def reset(self, **kwargs: Any) -> None:
+        pass
+
+    def reflect(
+        self,
+        reflect_strategy: str,
+        question: str,
+        examples: str,
+        prompt: str,
+        additional_keys: Dict[str, str],
+    ) -> Tuple[List[str], str]:
+        pass
+
+    def reflect_condition(
+        self, idx: int, reflect_strategy: Optional[str], key: str
+    ) -> bool:
+        pass
+
+        
 class ReflexionReActCodeStrategy(ReflexionReActBaseStrategy):
     pass
 
