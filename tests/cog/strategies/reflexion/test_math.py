16439e29f14ea99cb87e4100bf79a31032524186
alckasoc <tuvincent0106@gmail.com>
2024-06-29 01:16:31 -0700

    1 done

--- tests/cog/strategies/reflexion/test_math.py
+++ tests/cog/strategies/reflexion/test_math.py
@@ -1,1 +1,110 @@
 """Unit tests for Reflexion Math strategies."""
+
+from langchain_community.chat_models.fake import FakeListChatModel
+from langchain_core.language_models.chat_models import BaseChatModel
+
+from agential.cog.strategies.reflexion.math import (
+    ReflexionCoTGSM8KStrategy,
+    ReflexionCoTSVAMPStrategy,
+    ReflexionCoTTabMWPStrategy,
+    ReflexionReActGSM8KStrategy,
+    ReflexionReActSVAMPStrategy,
+    ReflexionReActTabMWPStrategy,
+)
+
+
+def test_reflexion_cot_init() -> None:
+    """Tests ReflexionCoTQAStrategy init."""
+
+
+def test_reflexion_cot_generate() -> None:
+    """Tests ReflexionCoTQAStrategy generate."""
+
+
+def test_reflexion_cot_generate_action() -> None:
+    """Tests ReflexionCoTQAStrategy generate_action."""
+
+
+def test_reflexion_cot_generate_observation() -> None:
+    """Tests ReflexionCoTQAStrategy generate_observation."""
+
+
+def test_reflexion_cot_create_output_dict() -> None:
+    """Tests ReflexionCoTQAStrategy create_output_dict."""
+
+
+def test_reflexion_cot_halting_condition() -> None:
+    """Tests ReflexionCoTQAStrategy halting_condition."""
+
+
+def test_reflexion_cot_reset() -> None:
+    """Tests ReflexionCoTQAStrategy reset."""
+
+
+def test_reflexion_cot_reflect() -> None:
+    """Tests ReflexionCoTQAStrategy reflect."""
+
+
+def test_reflexion_cot_reflect_condition() -> None:
+    """Tests ReflexionCoTQAStrategy reflect_condition."""
+
+
+def test_reflexion_cot_instantiate_strategies() -> None:
+    """Tests ReflexionCoTQAStrategy instantiate strategies."""
+    llm = FakeListChatModel(responses=[])
+    gsm8k_strategy = ReflexionCoTGSM8KStrategy(llm=llm)
+    svamp_strategy = ReflexionCoTSVAMPStrategy(llm=llm)
+    tabmwp_strategy = ReflexionCoTTabMWPStrategy(llm=llm)
+
+    assert isinstance(gsm8k_strategy, ReflexionCoTGSM8KStrategy)
+    assert isinstance(svamp_strategy, ReflexionCoTSVAMPStrategy)
+    assert isinstance(tabmwp_strategy, ReflexionCoTTabMWPStrategy)
+
+
+def test_reflexion_react_init() -> None:
+    """Tests ReflexionReActQAStrategy init."""
+
+
+def test_reflexion_react_generate() -> None:
+    """Tests ReflexionReActQAStrategy generate."""
+
+
+def test_reflexion_react_generate_action() -> None:
+    """Tests ReflexionReActQAStrategy generate_action."""
+
+
+def test_reflexion_react_generate_observation() -> None:
+    """Tests ReflexionReActQAStrategy generate_observation."""
+
+
+def test_reflexion_react_create_output_dict() -> None:
+    """Tests ReflexionReActQAStrategy create_output_dict."""
+
+
+def test_reflexion_react_react_create_output_dict() -> None:
+    """Tests ReflexionReActQAStrategy react_create_output_dict."""
+
+
+def test_reflexion_react_halting_condition() -> None:
+    """Tests ReflexionReActQAStrategy halting_condition."""
+
+
+def test_reflexion_react_react_halting_condition() -> None:
+    """Tests ReflexionReActQAStrategy react_halting_condition."""
+
+
+def test_reflexion_react_reset() -> None:
+    """Tests ReflexionReActQAStrategy reset."""
+
+
+def test_reflexion_react_reflect() -> None:
+    """Tests ReflexionReActQAStrategy reflect."""
+
+
+def test_reflexion_react_reflect_condition() -> None:
+    """Tests ReflexionReActQAStrategy reflect_condition."""
+
+
+def test_reflexion_react_instantiate_strategies() -> None:
+    """Tests ReflexionReActQAStrategy instantiate strategies."""
+
