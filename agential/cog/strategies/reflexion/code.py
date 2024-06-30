2eaa0f9a8f377e5ffe76c48d630d3fb6ac286db7
alckasoc <tuvincent0106@gmail.com>
2024-06-30 15:48:23 -0700

    parse code action

--- agential/cog/strategies/reflexion/code.py
+++ agential/cog/strategies/reflexion/code.py
@@ -56,6 +56,34 @@ def parse_code_action_cot(action: str) -> Tuple[str, str]:
     return action_type, query
 
 
+def parse_code_action_react(action: str) -> Tuple[str, str]:
+    """Parses an action string to extract the action type and code content.
+
+    Identifies action types (`Finish`, `Implement`, `Test`) and extracts the
+    corresponding code content enclosed within Markdown-style code blocks.
+    The action type is case-insensitive and the code content is trimmed of
+    leading and trailing whitespace.
+
+    Args:
+        action (str): The action string containing the action type and code content.
+
+    Returns:
+        Tuple[str, str]: A tuple containing the extracted action type (capitalized)
+        and the extracted code content.
+    """
+    action_split = action.split("```python", maxsplit=1)
+    match = re.search(r"\b(Finish|Test|Implement)\b", action_split[0], re.IGNORECASE)
+
+    action_type = match.group(0).lower().capitalize() if match else ""
+    try:
+        query = action_split[1].split("```")[0].strip() if action_type else ""
+    except:
+        action_type = ""
+        query = ""
+
+    return action_type, query
+
+
 class ReflexionCoTCodeStrategy(ReflexionCoTBaseStrategy):
     def __init__(
         self,
