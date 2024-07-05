99618a457a3b25eb63688e72f91f6522e4ab8158
alckasoc <tuvincent0106@gmail.com>
2024-07-05 01:59:41 -0700

    rename

--- agential/cog/strategies/reflexion/qa.py
+++ agential/cog/strategies/reflexion/qa.py
@@ -10,7 +10,7 @@ from langchain_community.docstore.wikipedia import Wikipedia
 from langchain_core.language_models.chat_models import BaseChatModel
 from tiktoken import Encoding
 
-from agential.cog.eval.reflexion import EM
+from agential.cog.eval.em import EM
 from agential.cog.functional.reflexion import (
     _is_halted,
     _prompt_cot_agent,
