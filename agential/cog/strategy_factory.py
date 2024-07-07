cd57c72b3750df1227ea97bcd96614e9390dfaad
alckasoc <tuvincent0106@gmail.com>
2024-07-07 15:52:01 -0700

    .

--- agential/cog/strategy_factory.py
+++ agential/cog/strategy_factory.py
@@ -61,77 +61,16 @@ from agential.cog.reflexion.strategies.qa import (
 )
 
 
-class CriticStrategyFactory:
-    """A factory class for creating instances of different CRITIC strategies based on the specified mode and benchmark.
-
-    Methods:
-        get_strategy(mode: Dict[str, str], **strategy_kwargs) -> CriticBaseStrategy:
-            Returns an instance of the appropriate Critic strategy based on the provided mode and benchmark.
-    """
-
-    @staticmethod
-    def get_strategy(
-        mode: Dict[str, str], **strategy_kwargs: Any
-    ) -> CriticBaseStrategy:
-        """Returns an instance of the appropriate Critic strategy based on the provided mode and benchmark.
-
-        Available modes:
-            - qa: "hotpotqa", "triviaqa", "ambignq", "fever"
-            - math: "gsm8k", "svamp", "tabmwp"
-            - code: "mbpp", "humaneval"
-
-        Args:
-            mode (Dict[str, str]): A dictionary specifying the mode and benchmark.
-                Example: {"qa": "hotpotqa"}, {"math": "gsm8k"}, {"code": "mbpp"}.
-            **strategy_kwargs (Dict[str, Any]): Additional keyword arguments to pass to the strategy's constructor.
-
-        Returns:
-            CriticBaseStrategy: An instance of the appropriate Critic strategy.
-
-        Raises:
-            ValueError: If the mode or benchmark is unsupported.
-        """
-        if "qa" in mode:
-            if mode["qa"] == "hotpotqa":
-                return CritHotQAStrategy(**strategy_kwargs)
-            elif mode["qa"] == "triviaqa":
-                return CritTriviaQAStrategy(**strategy_kwargs)
-            elif mode["qa"] == "ambignq":
-                return CritAmbigNQStrategy(**strategy_kwargs)
-            elif mode["qa"] == "fever":
-                return CritFEVERStrategy(**strategy_kwargs)
-            else:
-                raise ValueError(f"Unsupported QA benchmark: {mode['qa']}")
-        elif "math" in mode:
-            if mode["math"] == "gsm8k":
-                return CritGSM8KStrategy(**strategy_kwargs)
-            elif mode["math"] == "svamp":
-                return CritSVAMPStrategy(**strategy_kwargs)
-            elif mode["math"] == "tabmwp":
-                return CritTabMWPStrategy(**strategy_kwargs)
-            else:
-                raise ValueError(f"Unsupported Math benchmark: {mode['math']}")
-        elif "code" in mode:
-            if mode["code"] == "humaneval":
-                return CritHEvalCodeStrategy(**strategy_kwargs)
-            elif mode["code"] == "mbpp":
-                return CritMBPPCodeStrategy(**strategy_kwargs)
-            else:
-                raise ValueError(f"Unsupported Code benchmark: {mode['code']}")
-        else:
-            raise ValueError(f"Unsupported mode: {mode}")
-
-
 class ReActStrategyFactory:
     """A factory class for creating instances of different ReAct strategies based on the specified mode and benchmark.
 
     Methods:
-        get_strategy(mode: Dict[str, str], **strategy_kwargs) -> ReActBaseStrategy:
+        get_strategy(benchmark: str, **strategy_kwargs) -> ReActBaseStrategy:
             Returns an instance of the appropriate ReAct strategy based on the provided mode and benchmark.
     """
 
     @staticmethod
-    def get_strategy(mode: Dict[str, str], **strategy_kwargs: Any) -> ReActBaseStrategy:
+    def get_strategy(mode: str, **strategy_kwargs: Any) -> ReActBaseStrategy:
         """Returns an instance of the appropriate ReAct strategy based on the provided mode and benchmark.
 
         Available modes:
@@ -140,8 +79,7 @@ class ReActStrategyFactory:
             - code: "mbpp", "humaneval"
 
         Args:
-            mode (Dict[str, str]): A dictionary specifying the mode and benchmark.
-                Example: {"qa": "hotpotqa"}, {"math": "gsm8k"}, {"code": "mbpp"}.
+            mode (str): A string specifying the benchmark.
             **strategy_kwargs (Dict[str, Any]): Additional keyword arguments to pass to the strategy's constructor.
 
         Returns:
@@ -150,35 +88,26 @@ class ReActStrategyFactory:
         Raises:
             ValueError: If the mode or benchmark is unsupported.
         """
-        if "qa" in mode:
-            if mode["qa"] == "hotpotqa":
-                return ReActHotQAStrategy(**strategy_kwargs)
-            elif mode["qa"] == "triviaqa":
-                return ReActTriviaQAStrategy(**strategy_kwargs)
-            elif mode["qa"] == "ambignq":
-                return ReActAmbigNQStrategy(**strategy_kwargs)
-            elif mode["qa"] == "fever":
-                return ReActFEVERStrategy(**strategy_kwargs)
-            else:
-                raise ValueError(f"Unsupported QA benchmark: {mode['qa']}")
-        elif "math" in mode:
-            if mode["math"] == "gsm8k":
-                return ReActGSM8KStrategy(**strategy_kwargs)
-            elif mode["math"] == "svamp":
-                return ReActSVAMPStrategy(**strategy_kwargs)
-            elif mode["math"] == "tabmwp":
-                return ReActTabMWPStrategy(**strategy_kwargs)
-            else:
-                raise ValueError(f"Unsupported Math benchmark: {mode['math']}")
-        elif "code" in mode:
-            if mode["code"] == "humaneval":
-                return ReActHEvalStrategy(**strategy_kwargs)
-            elif mode["code"] == "mbpp":
-                return ReActMBPPStrategy(**strategy_kwargs)
-            else:
-                raise ValueError(f"Unsupported Code benchmark: {mode['code']}")
+        if mode["qa"] == "hotpotqa":
+            return ReActHotQAStrategy(**strategy_kwargs)
+        elif mode["qa"] == "triviaqa":
+            return ReActTriviaQAStrategy(**strategy_kwargs)
+        elif mode["qa"] == "ambignq":
+            return ReActAmbigNQStrategy(**strategy_kwargs)
+        elif mode["qa"] == "fever":
+            return ReActFEVERStrategy(**strategy_kwargs)
+        elif mode["math"] == "gsm8k":
+            return ReActGSM8KStrategy(**strategy_kwargs)
+        elif mode["math"] == "svamp":
+            return ReActSVAMPStrategy(**strategy_kwargs)
+        elif mode["math"] == "tabmwp":
+            return ReActTabMWPStrategy(**strategy_kwargs)
+        elif mode["code"] == "humaneval":
+            return ReActHEvalStrategy(**strategy_kwargs)
+        elif mode["code"] == "mbpp":
+            return ReActMBPPStrategy(**strategy_kwargs)
         else:
-            raise ValueError(f"Unsupported mode: {mode}")
+            raise ValueError(f"Unsupported benchmark: {mode}")
 
 
 class ReflexionCoTStrategyFactory:
@@ -297,6 +226,67 @@ class ReflexionReActStrategyFactory:
                 return ReflexionReActHEvalStrategy(**strategy_kwargs)
             elif mode["code"] == "mbpp":
                 return ReflexionReActMBPPStrategy(**strategy_kwargs)
+            else:
+                raise ValueError(f"Unsupported Code benchmark: {mode['code']}")
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+
+
+class CriticStrategyFactory:
+    """A factory class for creating instances of different CRITIC strategies based on the specified mode and benchmark.
+
+    Methods:
+        get_strategy(mode: Dict[str, str], **strategy_kwargs) -> CriticBaseStrategy:
+            Returns an instance of the appropriate Critic strategy based on the provided mode and benchmark.
+    """
+
+    @staticmethod
+    def get_strategy(
+        mode: Dict[str, str], **strategy_kwargs: Any
+    ) -> CriticBaseStrategy:
+        """Returns an instance of the appropriate Critic strategy based on the provided mode and benchmark.
+
+        Available modes:
+            - qa: "hotpotqa", "triviaqa", "ambignq", "fever"
+            - math: "gsm8k", "svamp", "tabmwp"
+            - code: "mbpp", "humaneval"
+
+        Args:
+            mode (Dict[str, str]): A dictionary specifying the mode and benchmark.
+                Example: {"qa": "hotpotqa"}, {"math": "gsm8k"}, {"code": "mbpp"}.
+            **strategy_kwargs (Dict[str, Any]): Additional keyword arguments to pass to the strategy's constructor.
+
+        Returns:
+            CriticBaseStrategy: An instance of the appropriate Critic strategy.
+
+        Raises:
+            ValueError: If the mode or benchmark is unsupported.
+        """
+        if "qa" in mode:
+            if mode["qa"] == "hotpotqa":
+                return CritHotQAStrategy(**strategy_kwargs)
+            elif mode["qa"] == "triviaqa":
+                return CritTriviaQAStrategy(**strategy_kwargs)
+            elif mode["qa"] == "ambignq":
+                return CritAmbigNQStrategy(**strategy_kwargs)
+            elif mode["qa"] == "fever":
+                return CritFEVERStrategy(**strategy_kwargs)
+            else:
+                raise ValueError(f"Unsupported QA benchmark: {mode['qa']}")
+        elif "math" in mode:
+            if mode["math"] == "gsm8k":
+                return CritGSM8KStrategy(**strategy_kwargs)
+            elif mode["math"] == "svamp":
+                return CritSVAMPStrategy(**strategy_kwargs)
+            elif mode["math"] == "tabmwp":
+                return CritTabMWPStrategy(**strategy_kwargs)
+            else:
+                raise ValueError(f"Unsupported Math benchmark: {mode['math']}")
+        elif "code" in mode:
+            if mode["code"] == "humaneval":
+                return CritHEvalCodeStrategy(**strategy_kwargs)
+            elif mode["code"] == "mbpp":
+                return CritMBPPCodeStrategy(**strategy_kwargs)
             else:
                 raise ValueError(f"Unsupported Code benchmark: {mode['code']}")
         else:
