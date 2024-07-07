6f7229070a815dbb3f1719677b28b8c76004c57d
alckasoc <tuvincent0106@gmail.com>
2024-07-07 01:03:32 -0700

    .

--- agential/cog/react/selector.py
+++ agential/cog/react/selector.py
@@ -1,5 +1,6 @@
 """ReAct prompts and fewshot examples selector."""
 
+from typing import Dict
 from agential.base.selector import BaseSelector
 from agential.cog.react.prompts import (
     REACT_INSTRUCTION_AMBIGNQ,
@@ -16,25 +17,43 @@ from agential.fewshots.manager import Benchmarks
 
 
 REACT_PROMPTS = {
-    Benchmarks.HOTPOTQA: REACT_INSTRUCTION_HOTPOTQA,
-    Benchmarks.FEVER: REACT_INSTRUCTION_FEVER,
-    Benchmarks.TRIVIAQA: REACT_INSTRUCTION_TRIVIAQA,
-    Benchmarks.AMBIGNQ: REACT_INSTRUCTION_AMBIGNQ,
-    Benchmarks.GSM8K: REACT_INSTRUCTION_GSM8K,
-    Benchmarks.SVAMP: REACT_INSTRUCTION_SVAMP,
-    Benchmarks.TABMWP: REACT_INSTRUCTION_TABMWP,
-    Benchmarks.HUMANEVAL: REACT_INSTRUCTION_HUMANEVAL,
-    Benchmarks.MBPP: REACT_INSTRUCTION_MBPP,
+    Benchmarks.HOTPOTQA: {
+        "prompt": REACT_INSTRUCTION_HOTPOTQA,
+    },
+    Benchmarks.FEVER: {
+        "prompt": REACT_INSTRUCTION_FEVER,
+    },
+    Benchmarks.TRIVIAQA: {
+        "prompt": REACT_INSTRUCTION_TRIVIAQA,
+    },
+    Benchmarks.AMBIGNQ: {
+        "prompt": REACT_INSTRUCTION_AMBIGNQ,
+    },
+    Benchmarks.GSM8K: {
+        "prompt": REACT_INSTRUCTION_GSM8K,
+    },
+    Benchmarks.SVAMP: {
+        "prompt": REACT_INSTRUCTION_SVAMP,
+    },
+    Benchmarks.TABMWP: {
+        "prompt": REACT_INSTRUCTION_TABMWP,
+    },
+    Benchmarks.HUMANEVAL: {
+        "prompt": REACT_INSTRUCTION_HUMANEVAL,
+    },
+    Benchmarks.MBPP: {
+        "prompt": REACT_INSTRUCTION_MBPP,
+    }
 }
 
 
 class ReActSelector(BaseSelector):
     @staticmethod
-    def get_fewshots(self) -> str:
-        return ""
+    def get_fewshots(self) -> Dict[str, str]:
+        return {}
 
     @staticmethod
-    def get_prompt(self, benchmark: str) -> str:
+    def get_prompt(self, benchmark: str) -> Dict[str, str]:
         if benchmark not in REACT_PROMPTS:
             raise ValueError(f"Benchmark '{benchmark}' not supported for ReAct.")
 
