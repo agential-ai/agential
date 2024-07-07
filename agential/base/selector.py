7a3279d8188186add01a829b35f21b8c7ae8c2d9
alckasoc <tuvincent0106@gmail.com>
2024-07-07 00:23:11 -0700

    .

---
+++ agential/base/selector.py
@@ -0,0 +1,25 @@
+"""Generic base Selector class."""
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+
+class BaseSelector(ABC):
+    """An abstract base class for auto-selecting prompts and few-shot examples."""
+
+    def __init__(self) -> None:
+        """Initialization."""
+
+    @abstractmethod
+    def get_fewshots(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> str:
+        """Gets the few-shot examples."""
+        pass
+
+    @abstractmethod
+    def get_prompt(self, *args: Any, **kwargs: Any) -> str:
+        """Gets the prompt instruction."""
+        pass
