0f597a86122a5a25558229815e75da233a751ef2
alckasoc <tuvincent0106@gmail.com>
2024-07-07 02:51:10 -0700

    .

--- tests/fewshots/test_manager.py
+++ tests/fewshots/test_manager.py
@@ -11,31 +11,25 @@ from agential.fewshots.manager import FewShotType, get_fewshot_examples
 def test_get_fewshot_examples() -> None:
     """Test get_fewshot_examples."""
     # Test valid input.
-    mode = {"qa": "hotpotqa"}
+    benchmark = "hotpotqa"
     fewshot_type = FewShotType.COT
-    result = get_fewshot_examples(mode, fewshot_type)
+    result = get_fewshot_examples(benchmark, fewshot_type)
     assert result == HOTPOTQA_FEWSHOT_EXAMPLES_COT
 
-    # Test invalid benchmark type.
-    mode = {"invalid_type": "hotpotqa"}
+    # Test invalid benchmark.
+    benchmark = "invalid_benchmark"
     fewshot_type = FewShotType.COT
     with pytest.raises(ValueError):
-        result = get_fewshot_examples(mode, fewshot_type)
-
-    # Test invalid benchmark name.
-    mode = {"qa": "invalid_benchmark"}
-    fewshot_type = FewShotType.COT
-    with pytest.raises(ValueError):
-        result = get_fewshot_examples(mode, fewshot_type)
+        result = get_fewshot_examples(benchmark, fewshot_type)
 
     # Test invalid few-shot type.
-    mode = {"qa": "hotpotqa"}
+    benchmark = "hotpotqa"
     fewshot_type = "invalid_fewshot"
     with pytest.raises(ValueError):
-        result = get_fewshot_examples(mode, fewshot_type)
+        result = get_fewshot_examples(benchmark, fewshot_type)
 
     # Test invalid few-shot type for the given benchmark.
-    mode = {"qa": "hotpotqa"}
+    benchmark = "hotpotqa"
     fewshot_type = FewShotType.POT
     with pytest.raises(ValueError):
-        result = get_fewshot_examples(mode, fewshot_type)
+        result = get_fewshot_examples(benchmark, fewshot_type)
