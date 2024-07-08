af34ebe0a5d80f885ddc901c8ad90ea025bee9bd
alckasoc <tuvincent0106@gmail.com>
2024-07-07 20:42:29 -0700

    .

---
+++ agential/manager/fewshot_mapping.py
@@ -0,0 +1,96 @@
+"""Few-shot mapping."""
+from agential.manager.constants import Benchmarks, FewShotType
+from agential.fewshots.ambignq import (
+    AMBIGNQ_FEWSHOT_EXAMPLES_COT,
+    AMBIGNQ_FEWSHOT_EXAMPLES_DIRECT,
+    AMBIGNQ_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.fever import (
+    FEVER_FEWSHOT_EXAMPLES_COT,
+    FEVER_FEWSHOT_EXAMPLES_DIRECT,
+    FEVER_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.gsm8k import (
+    GSM8K_FEWSHOT_EXAMPLES_COT,
+    GSM8K_FEWSHOT_EXAMPLES_POT,
+    GSM8K_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.hotpotqa import (
+    HOTPOTQA_FEWSHOT_EXAMPLES_COT,
+    HOTPOTQA_FEWSHOT_EXAMPLES_DIRECT,
+    HOTPOTQA_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.humaneval import (
+    HUMANEVAL_FEWSHOT_EXAMPLES_COT,
+    HUMANEVAL_FEWSHOT_EXAMPLES_POT,
+    HUMANEVAL_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.mbpp import (
+    MBPP_FEWSHOT_EXAMPLES_COT,
+    MBPP_FEWSHOT_EXAMPLES_POT,
+    MBPP_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.svamp import (
+    SVAMP_FEWSHOT_EXAMPLES_COT,
+    SVAMP_FEWSHOT_EXAMPLES_POT,
+    SVAMP_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.tabmwp import (
+    TABMWP_FEWSHOT_EXAMPLES_COT,
+    TABMWP_FEWSHOT_EXAMPLES_POT,
+    TABMWP_FEWSHOT_EXAMPLES_REACT,
+)
+from agential.fewshots.triviaqa import (
+    TRIVIAQA_FEWSHOT_EXAMPLES_COT,
+    TRIVIAQA_FEWSHOT_EXAMPLES_DIRECT,
+    TRIVIAQA_FEWSHOT_EXAMPLES_REACT,
+)
+
+
+BENCHMARK_FEWSHOTS = {
+    Benchmarks.HOTPOTQA: {
+        FewShotType.COT: HOTPOTQA_FEWSHOT_EXAMPLES_COT,
+        FewShotType.DIRECT: HOTPOTQA_FEWSHOT_EXAMPLES_DIRECT,
+        FewShotType.REACT: HOTPOTQA_FEWSHOT_EXAMPLES_REACT,
+    },
+    Benchmarks.FEVER: {
+        FewShotType.COT: FEVER_FEWSHOT_EXAMPLES_COT,
+        FewShotType.DIRECT: FEVER_FEWSHOT_EXAMPLES_DIRECT,
+        FewShotType.REACT: FEVER_FEWSHOT_EXAMPLES_REACT,
+    },
+    Benchmarks.TRIVIAQA: {
+        FewShotType.COT: TRIVIAQA_FEWSHOT_EXAMPLES_COT,
+        FewShotType.DIRECT: TRIVIAQA_FEWSHOT_EXAMPLES_DIRECT,
+        FewShotType.REACT: TRIVIAQA_FEWSHOT_EXAMPLES_REACT,
+    },
+    Benchmarks.AMBIGNQ: {
+        FewShotType.COT: AMBIGNQ_FEWSHOT_EXAMPLES_COT,
+        FewShotType.DIRECT: AMBIGNQ_FEWSHOT_EXAMPLES_DIRECT,
+        FewShotType.REACT: AMBIGNQ_FEWSHOT_EXAMPLES_REACT,
+    },
+    Benchmarks.GSM8K: {
+        FewShotType.POT: GSM8K_FEWSHOT_EXAMPLES_POT,
+        FewShotType.REACT: GSM8K_FEWSHOT_EXAMPLES_REACT,
+        FewShotType.COT: GSM8K_FEWSHOT_EXAMPLES_COT,
+    },
+    Benchmarks.SVAMP: {
+        FewShotType.POT: SVAMP_FEWSHOT_EXAMPLES_POT,
+        FewShotType.REACT: SVAMP_FEWSHOT_EXAMPLES_REACT,
+        FewShotType.COT: SVAMP_FEWSHOT_EXAMPLES_COT,
+    },
+    Benchmarks.TABMWP: {
+        FewShotType.POT: TABMWP_FEWSHOT_EXAMPLES_POT,
+        FewShotType.REACT: TABMWP_FEWSHOT_EXAMPLES_REACT,
+        FewShotType.COT: TABMWP_FEWSHOT_EXAMPLES_COT,
+    },
+    Benchmarks.HUMANEVAL: {
+        FewShotType.POT: HUMANEVAL_FEWSHOT_EXAMPLES_POT,
+        FewShotType.REACT: HUMANEVAL_FEWSHOT_EXAMPLES_REACT,
+        FewShotType.COT: HUMANEVAL_FEWSHOT_EXAMPLES_COT,
+    },
+    Benchmarks.MBPP: {
+        FewShotType.POT: MBPP_FEWSHOT_EXAMPLES_POT,
+        FewShotType.REACT: MBPP_FEWSHOT_EXAMPLES_REACT,
+        FewShotType.COT: MBPP_FEWSHOT_EXAMPLES_COT,
+    },
+}
