d1898f3e795c8546fe047e7cd60a5fe6668cc439
alckasoc <tuvincent0106@gmail.com>
2024-07-08 02:29:27 -0700

    .

--- tests/cog/critic/test_selector.py
+++ tests/cog/critic/test_selector.py
@@ -4,7 +4,7 @@ import pytest
 
 from langchain_community.chat_models.fake import FakeListChatModel
 
-from agential.cog.critic.selector import CriticSelector, CriticStrategyFactory
+from agential.cog.critic.selector import CriticFactory
 from agential.cog.critic.strategies.code import (
     CritHEvalCodeStrategy,
     CritMBPPCodeStrategy,
@@ -21,51 +21,58 @@ from agential.cog.critic.strategies.qa import (
     CritTriviaQAStrategy,
 )
 from agential.manager.constants import Benchmarks
+from agential.cog.critic.prompts import (
+    GSM8K_FEWSHOT_EXAMPLES_CRITIC,
+    GSM8K_FEWSHOT_EXAMPLES_CRITIC_NO_TOOL,
+    CRITIC_POT_INSTRUCTION_GSM8K,
+    CRITIC_CRITIQUE_INSTRUCTION_GSM8K,
+    CRITIC_CRITIQUE_NO_TOOL_INSTRUCTION_GSM8K
+)
 
 
 def test_critic_strategy_factory_get_strategy() -> None:
-    """Tests CriticStrategyFactory get_strategy method."""
+    """Tests CriticFactory get_strategy method."""
     llm = FakeListChatModel(responses=[])
 
     # QA benchmarks.
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.HOTPOTQA, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.HOTPOTQA, llm=llm),
         CritHotQAStrategy,
     )
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.TRIVIAQA, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.TRIVIAQA, llm=llm),
         CritTriviaQAStrategy,
     )
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.AMBIGNQ, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.AMBIGNQ, llm=llm),
         CritAmbigNQStrategy,
     )
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.FEVER, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.FEVER, llm=llm),
         CritFEVERStrategy,
     )
 
     # Math benchmarks.
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.GSM8K, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.GSM8K, llm=llm),
         CritGSM8KStrategy,
     )
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.SVAMP, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.SVAMP, llm=llm),
         CritSVAMPStrategy,
     )
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.TABMWP, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.TABMWP, llm=llm),
         CritTabMWPStrategy,
     )
 
     # Code benchmarks.
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.HUMANEVAL, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.HUMANEVAL, llm=llm),
         CritHEvalCodeStrategy,
     )
     assert isinstance(
-        CriticStrategyFactory.get_strategy(Benchmarks.MBPP, llm=llm),
+        CriticFactory.get_strategy(Benchmarks.MBPP, llm=llm),
         CritMBPPCodeStrategy,
     )
 
@@ -73,4 +80,64 @@ def test_critic_strategy_factory_get_strategy() -> None:
     with pytest.raises(
         ValueError, match="Unsupported benchmark: unknown for agent Critic"
     ):
-        CriticStrategyFactory.get_strategy("unknown", llm=llm)
+        CriticFactory.get_strategy("unknown", llm=llm)
+
+
+def test_critic_factory_get_fewshots() -> None:
+    """Tests CriticFactory get_fewshots method."""
+    # Valid benchmark with tool usage.
+    benchmark = Benchmarks.GSM8K
+    fewshots = CriticFactory.get_fewshots(benchmark, use_tool=True)
+    assert "critique_examples" in fewshots
+    assert fewshots == {
+        "critique_examples": GSM8K_FEWSHOT_EXAMPLES_CRITIC
+    }
+
+    # Valid benchmark without tool usage.
+    fewshots = CriticFactory.get_fewshots(benchmark, use_tool=False)
+    assert "critique_examples" in fewshots
+    assert fewshots == {
+        "critique_examples": GSM8K_FEWSHOT_EXAMPLES_CRITIC_NO_TOOL,
+    }
+
+    # Invalid benchmark.
+    with pytest.raises(
+        ValueError, match="Benchmark 'unknown' few-shots not found for Critic."
+    ):
+        CriticFactory.get_fewshots("unknown", use_tool=True)
+
+    # Missing use_tool argument.
+    with pytest.raises(ValueError, match="`use_tool` not specified."):
+        CriticFactory.get_fewshots(benchmark)
+
+
+def test_critic_factory_get_prompt() -> None:
+    """Tests CriticFactory get_prompt method."""
+    # Valid benchmark with tool usage.
+    benchmark = Benchmarks.GSM8K
+    prompts = CriticFactory.get_prompt(benchmark, use_tool=True)
+    assert "prompt" in prompts
+    assert "critique_prompt" in prompts
+    assert prompts == {
+        "prompt": CRITIC_POT_INSTRUCTION_GSM8K,
+        "critique_prompt": CRITIC_CRITIQUE_INSTRUCTION_GSM8K,
+    }
+
+    # Valid benchmark without tool usage.
+    prompts = CriticFactory.get_prompt(benchmark, use_tool=False)
+    assert "prompt" in prompts
+    assert "critique_prompt" in prompts
+    assert prompts == {
+        "prompt": CRITIC_POT_INSTRUCTION_GSM8K,
+        "critique_prompt": CRITIC_CRITIQUE_NO_TOOL_INSTRUCTION_GSM8K,
+    }
+
+    # Invalid benchmark.
+    with pytest.raises(
+        ValueError, match="Benchmark 'unknown' prompt not found for Critic."
+    ):
+        CriticFactory.get_prompt("unknown", use_tool=True)
+
+    # Missing use_tool argument.
+    with pytest.raises(ValueError, match="`use_tool` not specified."):
+        CriticFactory.get_prompt(benchmark)
