7cd295809475e11106d5c68d7658151a5461ba5c
alckasoc <tuvincent0106@gmail.com>
2024-07-11 01:26:02 -0700

    .

--- agential/cog/self_refine/factory.py
+++ agential/cog/self_refine/factory.py
@@ -4,3 +4,22 @@ from typing import Any, Dict
 
 from agential.base.factory import BaseFactory
 from agential.cog.constants import BENCHMARK_FEWSHOTS, Benchmarks, FewShotType
+from agential.cog.self_refine.prompts import (
+    SELF_REFINE_INSTRUCTION_GSM8K,
+    GSM8K_CRITIQUE_FEWSHOT_EXAMPLES,
+    SELF_REFINE_CRITIQUE_INSTRUCTION_GSM8K,
+    GSM8K_REFINE_FEWSHOT_EXAMPLES,
+    SELF_REFINE_REFINE_INSTRUCTION_GSM8K,
+)
+
+SELF_REFINE_BENCHMARK_FEWSHOTS = {
+    Benchmarks.HOTPOTQA: [FewShotType.REACT],
+    Benchmarks.FEVER: [FewShotType.REACT],
+    Benchmarks.TRIVIAQA: [FewShotType.REACT],
+    Benchmarks.AMBIGNQ: [FewShotType.REACT],
+    Benchmarks.GSM8K: [FewShotType.REACT],
+    Benchmarks.SVAMP: [FewShotType.REACT],
+    Benchmarks.TABMWP: [FewShotType.REACT],
+    Benchmarks.HUMANEVAL: [FewShotType.REACT],
+    Benchmarks.MBPP: [FewShotType.REACT],
+}
