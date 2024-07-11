18df378f5bf98e19faffdef6b4e3a87a97ae5b42
alckasoc <tuvincent0106@gmail.com>
2024-07-11 01:16:13 -0700

    .

---
+++ agential/cog/self_refine/factory.py
@@ -0,0 +1,6 @@
+"""ReAct prompts and fewshot examples selector."""
+
+from typing import Any, Dict
+
+from agential.base.factory import BaseFactory
+from agential.cog.constants import BENCHMARK_FEWSHOTS, Benchmarks, FewShotType
