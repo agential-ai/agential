f4055d70d49d1245c8be980be9d2448805d98761
alckasoc <tuvincent0106@gmail.com>
2024-07-14 00:28:54 -0700

    .

--- agential/cog/expel/strategies/qa.py
+++ agential/cog/expel/strategies/qa.py
@@ -1,6 +1,6 @@
 """ExpeL Agent strategies for QA."""
 
-from typing import Optional
+from typing import Optional, Dict, Any
 
 from langchain_core.language_models.chat_models import BaseChatModel
 
@@ -44,11 +44,15 @@ class ExpeLQAStrategy(ExpeLBaseStrategy):
     def get_dynamic_examples(
         self,
         question: str,
+        examples: str,
         k_docs: int,
         num_fewshots: int,
         max_fewshot_tokens: int,
         reranker_strategy: str,
+        additional_keys: Dict[str, Any]
     ):
+        additional_keys = additional_keys.copy()
+
         # Dynamically load in relevant past successful trajectories as fewshot examples.
         dynamic_examples = self.experience_memory.load_memories(
             query=question,
@@ -57,16 +61,18 @@ class ExpeLQAStrategy(ExpeLBaseStrategy):
             max_fewshot_tokens=max_fewshot_tokens,
             reranker_strategy=reranker_strategy,
         )["fewshots"]
-        examples = dynamic_examples if dynamic_examples else [examples]  # type: ignore
-        examples = "\n\n".join(examples + [END_OF_EXAMPLES_DELIMITER]) + "\n"  # type: ignore
+        examples = "\n\n---\n\n".join(
+            dynamic_examples if dynamic_examples else [examples]  # type: ignore
+        )
 
         # Dynamically load in all insights.
-        examples += RULE_PREFIX
         insights = self.insight_memory.load_memories()["insights"]
         insights = "".join(
             [f"{i}. {insight['insight']}\n" for i, insight in enumerate(insights)]
         )
-        examples += insights
+        additional_keys.update({"insights": insights})
+
+        return examples, additional_keys
 
     def gather_experience(self):
         pass
