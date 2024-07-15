b9f4dadb385e78ea933b04d91b6fe8598739622e
alckasoc <tuvincent0106@gmail.com>
2024-07-14 17:21:43 -0700

    .

--- agential/cog/expel/strategies/qa.py
+++ agential/cog/expel/strategies/qa.py
@@ -212,3 +212,23 @@ class ExpeLQAStrategy(ExpeLBaseStrategy):
             self.reflexion_react_agent.reset()
         self.experience_memory.clear()
         self.insight_memory.clear()
+
+
+class ExpeLHotQAStrategy(ExpeLQAStrategy):
+    """A strategy class for the HotpotQA benchmark using the ExpeL agent."""
+
+    pass
+
+class ExpeLTriviaQAStrategy(ExpeLQAStrategy):
+    """A strategy class for the TriviaQA benchmark using the ExpeL agent."""
+
+    pass
+
+class ExpeLAmbigNQStrategy(ExpeLQAStrategy):
+    """A strategy class for the AmbigNQ benchmark using the ExpeL agent."""
+    
+    pass
+
+class ExpeLFEVERStrategy(ExpeLQAStrategy):
+    """A strategy class for the FEVER benchmark using the ExpeL agent."""
+    pass
