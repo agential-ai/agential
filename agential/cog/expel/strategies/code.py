b45c00657d3de98d74484dc01f35c3c856e9f4ba
alckasoc <tuvincent0106@gmail.com>
2024-07-17 00:40:15 -0700

    .

--- agential/cog/expel/strategies/code.py
+++ agential/cog/expel/strategies/code.py
@@ -2,14 +2,20 @@
 
 from agential.cog.expel.strategies.general import ExpeLStrategy
 
+
 class ExpeLCodeStrategy(ExpeLStrategy):
     """A strategy class for Code benchmarks using the ExpeL agent."""
+
     pass
+
 
 class ExpeLHEvalStrategy(ExpeLCodeStrategy):
     """A strategy class for the HumanEval benchmark using the ExpeL agent."""
+
     pass
+
 
 class ExpeLMBPPStrategy(ExpeLCodeStrategy):
     """A strategy class for the MBPP benchmark using the ExpeL agent."""
+
     pass
--- agential/cog/expel/strategies/math.py
+++ agential/cog/expel/strategies/math.py
@@ -2,18 +2,26 @@
 
 from agential.cog.expel.strategies.general import ExpeLStrategy
 
+
 class ExpeLMathStrategy(ExpeLStrategy):
     """A strategy class for Math benchmarks using the ExpeL agent."""
+
     pass
+
 
 class ExpeLSVAMPStrategy(ExpeLMathStrategy):
     """A strategy class for the SVAMP benchmark using the ExpeL agent."""
+
     pass
+
 
 class ExpeLTabMWPStrategy(ExpeLMathStrategy):
     """A strategy class for the TabMWP benchmark using the ExpeL agent."""
+
     pass
+
 
 class ExpeLGSM8KStrategy(ExpeLMathStrategy):
     """A strategy class for the GSM8K benchmark using the ExpeL agent."""
+
     pass
