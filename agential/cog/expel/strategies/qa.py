202570fa1cf5c0c50797a969690f9895f9cfdffa
alckasoc <tuvincent0106@gmail.com>
2024-07-17 00:40:15 -0700

    .

--- agential/cog/expel/strategies/qa.py
+++ agential/cog/expel/strategies/qa.py
@@ -2,22 +2,32 @@
 
 from agential.cog.expel.strategies.general import ExpeLStrategy
 
+
 class ExpeLQAStrategy(ExpeLStrategy):
     """A strategy class for QA benchmarks using the ExpeL agent."""
+
     pass
+
 
 class ExpeLHotQAStrategy(ExpeLQAStrategy):
     """A strategy class for the HotpotQA benchmark using the ExpeL agent."""
+
     pass
+
 
 class ExpeLTriviaQAStrategy(ExpeLQAStrategy):
     """A strategy class for the TriviaQA benchmark using the ExpeL agent."""
+
     pass
+
 
 class ExpeLAmbigNQStrategy(ExpeLQAStrategy):
     """A strategy class for the AmbigNQ benchmark using the ExpeL agent."""
+
     pass
+
 
 class ExpeLFEVERStrategy(ExpeLQAStrategy):
     """A strategy class for the FEVER benchmark using the ExpeL agent."""
+
     pass
