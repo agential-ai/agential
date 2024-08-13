edfd0d969bf45b3385b98d0e71d60c6899d53ceb
alckasoc <tuvincent0106@gmail.com>
2024-08-13 02:11:23 -0700

    del test factory

--- tests/cog/react/test_agent.py
+++ tests/cog/react/test_agent.py
@@ -13,7 +13,22 @@ from agential.cog.react.prompts import (
 )
 from agential.cog.react.strategies.base import ReActBaseStrategy
 from agential.llm.llm import BaseLLM, MockLLM
-
+from agential.cog.constants import Benchmarks
+from agential.cog.react.strategies.code import (
+    ReActHEvalStrategy,
+    ReActMBPPStrategy,
+)
+from agential.cog.react.strategies.math import (
+    ReActGSM8KStrategy,
+    ReActSVAMPStrategy,
+    ReActTabMWPStrategy,
+)
+from agential.cog.react.strategies.qa import (
+    ReActAmbigNQStrategy,
+    ReActFEVERStrategy,
+    ReActHotQAStrategy,
+    ReActTriviaQAStrategy,
+)
 
 def test_init() -> None:
     """Test initialization."""
@@ -24,6 +39,92 @@ def test_init() -> None:
     assert agent.benchmark == "hotpotqa"
     assert isinstance(agent.strategy, ReActBaseStrategy)
 
+def test_react_factory_get_strategy() -> None:
+    """Tests ReActAgent get_strategy method."""
+    llm = MockLLM("gpt-3.5-turbo", responses=[])
+
+    # QA benchmarks.
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.HOTPOTQA, llm=llm),
+        ReActHotQAStrategy,
+    )
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.TRIVIAQA, llm=llm),
+        ReActTriviaQAStrategy,
+    )
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.AMBIGNQ, llm=llm),
+        ReActAmbigNQStrategy,
+    )
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.FEVER, llm=llm),
+        ReActFEVERStrategy,
+    )
+
+    # Math benchmarks.
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.GSM8K, llm=llm),
+        ReActGSM8KStrategy,
+    )
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.SVAMP, llm=llm),
+        ReActSVAMPStrategy,
+    )
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.TABMWP, llm=llm),
+        ReActTabMWPStrategy,
+    )
+
+    # Code benchmarks.
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.HUMANEVAL, llm=llm),
+        ReActHEvalStrategy,
+    )
+    assert isinstance(
+        ReActAgent.get_strategy(Benchmarks.MBPP, llm=llm),
+        ReActMBPPStrategy,
+    )
+
+    # Unsupported benchmark.
+    with pytest.raises(
+        ValueError, match="Unsupported benchmark: unknown for agent ReAct"
+    ):
+        ReActAgent.get_strategy("unknown", llm=llm)
+
+
+def test_react_factory_get_fewshots() -> None:
+    """Tests ReActAgent get_fewshots method."""
+    # Test valid input.
+    benchmark = Benchmarks.HOTPOTQA
+    result = ReActAgent.get_fewshots(benchmark, fewshot_type="react")
+    assert isinstance(result, dict)
+    assert result == {"examples": HOTPOTQA_FEWSHOT_EXAMPLES_REACT}
+
+    # Test unsupported benchmark.
+    with pytest.raises(
+        ValueError, match="Benchmark 'unknown' few-shots not found for ReAct."
+    ):
+        ReActAgent.get_fewshots("unknown", fewshot_type="react")
+
+    # Test unsupported fewshot_type.
+    with pytest.raises(
+        ValueError, match="Benchmark 'hotpotqa' few-shot type not supported for ReAct."
+    ):
+        ReActAgent.get_fewshots("hotpotqa", fewshot_type="pot")
+
+
+def test_react_factory_get_prompts() -> None:
+    """Tests ReActAgent get_prompts method."""
+    # Test valid input.
+    benchmark = Benchmarks.HOTPOTQA
+    result = ReActAgent.get_prompts(benchmark)
+    assert result == {"prompt": REACT_INSTRUCTION_HOTPOTQA}
+
+    # Test unsupported benchmark.
+    with pytest.raises(
+        ValueError, match="Benchmark 'unknown' prompt not found for ReAct."
+    ):
+        ReActAgent.get_prompts("unknown")
 
 def test_generate() -> None:
     """Test generate."""
