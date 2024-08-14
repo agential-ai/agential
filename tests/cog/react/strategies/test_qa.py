46c8d2746904be4066ea2d76950a12c8390ea3b4
emmalin <emailtoemma14@gmail.com>
2024-08-13 18:33:38 -0700

    testqa

--- tests/cog/react/strategies/test_qa.py
+++ tests/cog/react/strategies/test_qa.py
@@ -26,102 +26,57 @@ def test_init() -> None:
     assert strategy.max_tokens == 5000
     assert isinstance(strategy.docstore, DocstoreExplorer)
     assert isinstance(strategy.enc, Encoding)
-    assert strategy._scratchpad == ""
-    assert strategy._finished == False
-
-    assert strategy._prompt_metrics == {"thought": None, "action": None}
-
-
-def test_generate() -> None:
-    """Tests ReActQAStrategy generate."""
-    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
-
-    gt_result = "I need to search for the best kickboxer in the world who has been involved in controversies and crimes."
-    gt_scratchpad = "\nThought: I need to search for the best kickboxer in the world who has been involved in controversies and crimes."
-    responses = [
-        "I need to search for the best kickboxer in the world who has been involved in controversies and crimes.\nAction 1: Search[best kickboxer in the world controversies crimes]\nObservation 1: Could not find exact match. Similar: ['List of kickboxers', 'Kickboxing', 'List of controversies involving Kickboxing']\nThought 2: I should try searching for the best kickboxer in the world and then look for any controversies or crimes related to him.\nAction 2: Search[best kickboxer in the world]\nObservation 2: Could not find exact match. Similar: ['List of best kickboxers in the world', 'List of kickboxing organizations', 'Kickboxing', 'Best Fighters in the World']\nThought 3: I can try searching for top kickboxers and then look for controversies and crimes.\nAction 3: Search[top kickboxers]\nObservation 3: Could not find exact match. Similar: ['Top 10 kickboxers', 'Top 5 kickboxers', 'Top 15 kickboxers']\nThought 4: I need to refine my search terms to find the information I need.\nAction 4: Search[most famous kickboxer controversies crimes]\nObservation 4: Could not find exact match. Similar: ['Famous kickboxers', 'Kickboxing controversies', 'Famous kickboxers in the world']\nThought 5: I should try searching for famous kickboxers involved in controversies and crimes.\nAction 5: Search[famous kickboxers controversies crimes]\nObservation 5: Could not find exact match. Similar: ['Famous kickboxers', 'Kickboxing controversies', 'Famous kickboxers in the world']\nThought 6: I am unable to find the specific information I need within the given steps. \nAction 6: Finish[unable to find answer]"
-    ]
-    llm = MockLLM("gpt-3.5-turbo", responses=responses)
-    strategy = ReActQAStrategy(llm=llm)
-    result = strategy.generate(
-        question=question,
-        examples=HOTPOTQA_FEWSHOT_EXAMPLES_REACT,
-        prompt=REACT_INSTRUCTION_HOTPOTQA,
-        additional_keys={},
-    )
-    assert result == gt_result
-    assert not strategy._finished
-    assert strategy._scratchpad == gt_scratchpad
-
-    assert strategy._prompt_metrics == {
-        "thought": {
-            "prompt_tokens": 10,
-            "completion_tokens": 20,
-            "total_tokens": 30,
-            "prompt_tokens_cost": 1.5e-05,
-            "completion_tokens_cost": 3.9999999999999996e-05,
-            "total_tokens_cost": 5.4999999999999995e-05,
-            "time_sec": 0.5,
-        },
-        "action": None,
-    }
 
 
 def test_generate_action() -> None:
     """Tests ReActQAStrategy generate_action."""
     question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
-
+    gt_scratchpad = (
+        "\nAction 0: Search[best kick boxer in the world controversies crimes]"
+    )
     gt_action_type = "Search"
     gt_query = "best kick boxer in the world controversies crimes"
-    init_scratchpad = "\nThought: I need to search for the best kickboxer in the world who has been involved in controversies and crimes."
+    gt_out = "Search[best kick boxer in the world controversies crimes]"
     responses = ["Search[best kick boxer in the world controversies crimes]"]
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
-    action_type, query = strategy.generate_action(
+
+    scratchpad, action_type, query, out = strategy.generate_action(
+        idx=0,
+        scratchpad="",
         question=question,
         examples=HOTPOTQA_FEWSHOT_EXAMPLES_REACT,
         prompt=REACT_INSTRUCTION_HOTPOTQA,
         additional_keys={},
     )
+
+    assert scratchpad == gt_scratchpad
     assert action_type == gt_action_type
     assert query == gt_query
-
-    assert strategy._prompt_metrics == {
-        "thought": None,
-        "action": {
-            "prompt_tokens": 10,
-            "completion_tokens": 20,
-            "total_tokens": 30,
-            "prompt_tokens_cost": 1.5e-05,
-            "completion_tokens_cost": 3.9999999999999996e-05,
-            "total_tokens_cost": 5.4999999999999995e-05,
-            "time_sec": 0.5,
-        },
-    }
+    assert out.choices[0].message.content == gt_out
 
 
 def test_generate_observation() -> None:
     """Tests ReActQAStrategy generate_observation."""
     action_type = "Search"
+    gt_answer = ""
+    gt_scratchpad = "\nObservation 1: Buakaw Banchamek has faced several controversies and legal issues."
+    gt_obs = "Buakaw Banchamek has faced several controversies and legal issues."
     query = "best kick boxer in the world controversies crimes"
-    init_scratchpad = "\nThought: I need to search for the best kickboxer in the world who has been involved in controversies and crimes.\nAction: Search[best kick boxer in the world controversies crimes]"
     responses = []
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
 
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
+
     strategy.docstore.search = (
         lambda x: "Buakaw Banchamek has faced several controversies and legal issues."
     )
-    obs, external_tool_info = strategy.generate_observation(
-        idx=1, action_type=action_type, query=query
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=1, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
-    assert strategy._finished == False
-    assert strategy._scratchpad != init_scratchpad
     assert "search_result" in external_tool_info
     assert "lookup_result" in external_tool_info
     assert (
@@ -129,45 +84,55 @@ def test_generate_observation() -> None:
         == "Buakaw Banchamek has faced several controversies and legal issues."
     )
 
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == False
+
     # Test finish.
     action_type = "Finish"
+    gt_answer = "The best kickboxer is Buakaw Banchamek."
+    gt_scratchpad = "\nObservation 2: The best kickboxer is Buakaw Banchamek."
+    gt_obs = "The best kickboxer is Buakaw Banchamek."
     query = "The best kickboxer is Buakaw Banchamek."
-    init_scratchpad = "\nThought: I need to provide the final answer.\nAction: Finish[The best kickboxer is Buakaw Banchamek.]"
     responses = []
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
-    obs, external_tool_info = strategy.generate_observation(
-        idx=2, action_type=action_type, query=query
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=2, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
     assert obs == "The best kickboxer is Buakaw Banchamek."
-    assert strategy._finished == True
-    assert strategy._scratchpad != init_scratchpad
     assert "search_result" in external_tool_info
     assert "lookup_result" in external_tool_info
     assert external_tool_info == {"search_result": "", "lookup_result": ""}
 
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == True
+
     # Test search success.
     action_type = "Search"
+    gt_answer = ""
+    gt_scratchpad = "\nObservation 3: Buakaw Banchamek has faced several controversies and legal issues."
+    gt_obs = "Buakaw Banchamek has faced several controversies and legal issues."
     query = "best kick boxer in the world controversies crimes"
-    init_scratchpad = "\nThought: I need to search for the best kickboxer in the world who has been involved in controversies and crimes.\nAction: Search[best kick boxer in the world controversies crimes]"
     responses = ["Buakaw Banchamek has faced several controversies and legal issues."]
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
     strategy.docstore.search = (
         lambda x: "Buakaw Banchamek has faced several controversies and legal issues."
     )
-    obs, external_tool_info = strategy.generate_observation(
-        idx=3, action_type=action_type, query=query
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=3, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
     assert obs == "Buakaw Banchamek has faced several controversies and legal issues."
-    assert strategy._finished == False
-    assert strategy._scratchpad != init_scratchpad
     assert "search_result" in external_tool_info
     assert "lookup_result" in external_tool_info
     assert (
@@ -175,89 +140,114 @@ def test_generate_observation() -> None:
         == "Buakaw Banchamek has faced several controversies and legal issues."
     )
 
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == False
+
     # Test search failure.
     action_type = "Search"
+    gt_answer = ""
+    gt_scratchpad = "\nObservation 4: Could not find that page, please try again."
+    gt_obs = "Could not find that page, please try again."
     query = "best kick boxer in the world controversies crimes"
-    init_scratchpad = "\nThought: I need to search for the best kickboxer in the world who has been involved in controversies and crimes.\nAction: Search[best kick boxer in the world controversies crimes]"
     responses = []
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
     strategy.docstore.search = lambda x: (_ for _ in ()).throw(
         Exception("Search failed")
     )
-    obs, external_tool_info = strategy.generate_observation(
-        idx=4, action_type=action_type, query=query
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=4, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
     assert obs == "Could not find that page, please try again."
-    assert strategy._finished == False
-    assert strategy._scratchpad != init_scratchpad
     assert "search_result" in external_tool_info
     assert "lookup_result" in external_tool_info
     assert external_tool_info["search_result"] == ""
 
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == False
+
     # Test lookup success.
     action_type = "Lookup"
+    gt_answer = ""
+    gt_scratchpad = "\nObservation 5: Several controversies and legal issues related to Buakaw Banchamek."
+    gt_obs = "Several controversies and legal issues related to Buakaw Banchamek."
+
     query = "controversies"
-    init_scratchpad = "\nThought: I need to lookup controversies related to the best kickboxer in the world.\nAction: Lookup[controversies]"
     responses = ["Buakaw Banchamek has faced several controversies and legal issues."]
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
     strategy.docstore.lookup = (
         lambda x: "Several controversies and legal issues related to Buakaw Banchamek."
     )
-    obs, external_tool_info = strategy.generate_observation(
-        idx=5, action_type=action_type, query=query
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=5, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
     assert obs == "Several controversies and legal issues related to Buakaw Banchamek."
-    assert strategy._finished == False
-    assert strategy._scratchpad != init_scratchpad
     assert "search_result" in external_tool_info
     assert "lookup_result" in external_tool_info
     assert external_tool_info["lookup_result"] != ""
 
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == False
+
     # Test lookup failure.
     action_type = "Lookup"
+    gt_answer = ""
+    gt_scratchpad = "\nObservation 6: The last page Searched was not found, so you cannot Lookup a keyword in it. Please try one of the similar pages given."
+    gt_obs = "The last page Searched was not found, so you cannot Lookup a keyword in it. Please try one of the similar pages given."
     query = "controversies"
-    init_scratchpad = "\nThought: I need to lookup controversies related to the best kickboxer in the world.\nAction: Lookup[controversies]"
     responses = []
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
     strategy.docstore.lookup = lambda x: (_ for _ in ()).throw(
         ValueError("Lookup failed")
     )
-    obs, external_tool_info = strategy.generate_observation(
-        idx=6, action_type=action_type, query=query
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=6, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
     assert (
         obs
         == "The last page Searched was not found, so you cannot Lookup a keyword in it. Please try one of the similar pages given."
     )
-    assert strategy._finished == False
-    assert strategy._scratchpad != init_scratchpad
+
     assert "search_result" in external_tool_info
     assert "lookup_result" in external_tool_info
     assert external_tool_info["lookup_result"] == ""
 
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == False
+
     # Test invalid action.
     action_type = "Invalid"
+    gt_answer = ""
+    gt_scratchpad = "\nObservation 7: Invalid Action. Valid Actions are Lookup[<topic>] Search[<topic>] and Finish[<answer>]."
+    gt_obs = "Invalid Action. Valid Actions are Lookup[<topic>] Search[<topic>] and Finish[<answer>]."
     query = "invalid action"
-    init_scratchpad = "\nThought: I need to perform an invalid action.\nAction: Invalid[invalid action]"
     responses = []
     llm = MockLLM("gpt-3.5-turbo", responses=responses)
     strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = init_scratchpad
-    strategy._finished = False
-    obs, external_tool_info = strategy.generate_observation(
-        idx=7, action_type=action_type, query=query
+
+    scratchpad, answer, obs, finished, external_tool_info = (
+        strategy.generate_observation(
+            idx=7, scratchpad="", action_type=action_type, query=query
+        )
     )
     assert isinstance(obs, str)
     assert (
@@ -269,60 +259,10 @@ def test_generate_observation() -> None:
     assert external_tool_info["search_result"] == ""
     assert external_tool_info["lookup_result"] == ""
 
-
-def test_create_output_dict() -> None:
-    """Tests ReActQAStrategy create_output_dict."""
-    llm = MockLLM("gpt-3.5-turbo", responses=[])
-    strategy = ReActQAStrategy(llm=llm)
-    thought = "This is a thought."
-    action_type = "search"
-    query = "query"
-    obs = "observation"
-    external_tool_info = {"search_result": "", "lookup_result": ""}
-
-    expected_output = {
-        "thought": thought,
-        "action_type": action_type,
-        "query": query,
-        "observation": obs,
-        "answer": "",
-        "external_tool_info": {"search_result": "", "lookup_result": ""},
-        "prompt_metrics": {"thought": None, "action": None},
-    }
-
-    assert (
-        strategy.create_output_dict(
-            thought, action_type, query, obs, external_tool_info
-        )
-        == expected_output
-    )
-
-
-def test_halting_condition() -> None:
-    """Tests ReActQAStrategy halting_condition."""
-    llm = MockLLM("gpt-3.5-turbo", responses=[])
-    strategy = ReActQAStrategy(llm=llm)
-    idx = 0
-    question = "What is the capital of France?"
-    examples = ""
-    prompt = "Answer the question."
-
-    assert not strategy.halting_condition(idx, question, examples, prompt, {})
-
-
-def test_reset() -> None:
-    """Tests ReActQAStrategy reset."""
-    llm = MockLLM("gpt-3.5-turbo", responses=[])
-    strategy = ReActQAStrategy(llm=llm)
-    strategy._scratchpad = "Some previous state"
-    strategy._finished = True
-
-    strategy.reset()
-
-    assert strategy._scratchpad == ""
-    assert not strategy._finished
-
-    assert strategy._prompt_metrics == {"thought": None, "action": None}
+    assert answer == gt_answer
+    assert obs == gt_obs
+    assert scratchpad == gt_scratchpad
+    assert finished == False
 
 
 def test_instantiate_strategies() -> None:
