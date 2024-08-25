fe3bf9549baba0518e551c5a2f8ef2593a138fb9
alckasoc <tuvincent0106@gmail.com>
2024-08-25 04:51:03 -0700

    OOOOOOO

--- agential/cog/critic/strategies/math.py
+++ agential/cog/critic/strategies/math.py
@@ -6,7 +6,6 @@ from agential.cog.critic.functional import _prompt_agent, _prompt_critique
 from agential.cog.critic.strategies.base import CriticBaseStrategy
 from agential.llm.llm import BaseLLM
 from agential.utils.general import safe_execute
-from agential.utils.metrics import get_prompt_info
 from agential.utils.validation import validate_overlapping_keys
 
 
