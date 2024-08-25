8753e593ad167ac4ce94fbe3c840a115640301d5
alckasoc <tuvincent0106@gmail.com>
2024-08-24 22:58:20 -0700

    init test

--- tests/cog/critic/strategies/test_general.py
+++ tests/cog/critic/strategies/test_general.py
@@ -2,10 +2,10 @@
 
 
 from agential.cog.critic.strategies.general import CriticGeneralStrategy
-from agential.llm.llm import MockLLM
+from agential.llm.llm import BaseLLM, MockLLM
 
 
 def test_init() -> None:
     """Test initialization of the CRITIC general strategy."""
     strategy = CriticGeneralStrategy(llm=MockLLM("gpt-3.5-turbo", responses=[]))
-    assert strategy.llm == MockLLM("gpt-3.5-turbo", responses=[])
+    assert isinstance(strategy.llm, BaseLLM)
