2661e98e17e6536e16226e640bdf93ca2ed70736
alckasoc <tuvincent0106@gmail.com>
2024-08-26 13:49:15 -0700

    .

---
+++ tests/cog/self_refine/strategies/test_general.py
@@ -0,0 +1,21 @@
+"""Unit tests for the Self-Refine general strategy."""
+
+
+def test_init() -> None:
+    """Tests init."""
+    pass
+
+def test_generate_answer() -> None:
+    pass
+
+def test_generate_critique() -> None:
+    pass
+
+def test_update_answer_based_on_critique() -> None:
+    pass
+
+def test_halting_condition() -> None:
+    pass
+
+def test_reset() -> None:
+    pass
