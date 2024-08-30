3ea678f455d1995d6c821325710b56c531ff6fdf
alckasoc <tuvincent0106@gmail.com>
2024-08-29 19:45:25 -0700

    test

--- tests/cog/cot/strategies/test_general.py
+++ tests/cog/cot/strategies/test_general.py
@@ -1,7 +1,9 @@
 """Unit tests for CoT general strategy."""
 
-from agential.cog.cot.output import CoTOutput
+from agential.cog.cot.output import CoTOutput, CoTStepOutput
+from agential.cog.cot.prompts import COT_INSTRUCTION_HOTPOTQA
 from agential.cog.cot.strategies.general import CoTGeneralStrategy
+from agential.cog.fewshots.hotpotqa import HOTPOTQA_FEWSHOT_EXAMPLES_COT
 from agential.llm.llm import BaseLLM, MockLLM, Response
 
 
@@ -13,38 +15,23 @@ def test_init() -> None:
 
 def test_generate() -> None:
     """Tests the generate method."""
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = CoTOutput(answer='Badr Hari', total_prompt_tokens=20, total_completion_tokens=40, total_tokens=60, total_prompt_cost=3e-05, total_completion_cost=7.999999999999999e-05, total_cost=0.00010999999999999999, total_prompt_time=1.0, total_time=0.5, additional_info=CoTStepOutput(thought="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.", answer='Badr Hari', thought_response=Response(input_text='', output_text="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]", prompt_tokens=10, completion_tokens=20, total_tokens=30, prompt_cost=1.5e-05, completion_cost=3.9999999999999996e-05, total_cost=5.4999999999999995e-05, prompt_time=0.5), answer_response=Response(input_text='', output_text='Finish[Badr Hari]', prompt_tokens=10, completion_tokens=20, total_tokens=30, prompt_cost=1.5e-05, completion_cost=3.9999999999999996e-05, total_cost=5.4999999999999995e-05, prompt_time=0.5)))
+    responses = [
+        "Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+        'Finish[Badr Hari]',
+    ]
     strategy = CoTGeneralStrategy(
-        llm=MockLLM("gpt-3.5-turbo", responses=["1"]), testing=True
+        llm=MockLLM("gpt-3.5-turbo", responses=responses), testing=True
     )
     out = strategy.generate(
-        question="What is the capital of France?",
-        examples="",
-        prompt="",
+        question=question,
+        examples=HOTPOTQA_FEWSHOT_EXAMPLES_COT, 
+        prompt=COT_INSTRUCTION_HOTPOTQA,
         additional_keys={},
     )
-    assert out == CoTOutput(
-        answer="1",
-        total_prompt_tokens=10,
-        total_completion_tokens=20,
-        total_tokens=30,
-        total_prompt_cost=1.5e-05,
-        total_completion_cost=3.9999999999999996e-05,
-        total_cost=5.4999999999999995e-05,
-        total_prompt_time=0.5,
-        total_time=0.5,
-        additional_info=Response(
-            input_text="",
-            output_text="1",
-            prompt_tokens=10,
-            completion_tokens=20,
-            total_tokens=30,
-            prompt_cost=1.5e-05,
-            completion_cost=3.9999999999999996e-05,
-            total_cost=5.4999999999999995e-05,
-            prompt_time=0.5,
-        ),
-    )
-
+    assert out == gt_out
 
 def test_reset() -> None:
     """Tests the reset method."""
