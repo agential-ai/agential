7786267ab2ae8c918f41b0a3f05d3e503b5b6682
alckasoc <tuvincent0106@gmail.com>
2024-08-31 16:38:16 -0700

    update cot code

--- agential/prompting/cot/strategies/code.py
+++ agential/prompting/cot/strategies/code.py
@@ -2,9 +2,9 @@
 
 import time
 
-from typing import Dict
+from typing import Dict, List
 
-from agential.prompting.cot.functional import _prompt_llm
+from agential.prompting.cot.functional import _prompt_llm, accumulate_metrics
 from agential.prompting.cot.output import CoTOutput, CoTStepOutput
 from agential.prompting.cot.strategies.general import CoTGeneralStrategy
 
@@ -24,6 +24,7 @@ class CoTHEvalStrategy(CoTGeneralStrategy):
         examples: str,
         prompt: str,
         additional_keys: Dict[str, str],
+        num_retries: int,
     ) -> CoTOutput:
         """Generates an answer and critique for the given question using the provided examples and prompts.
 
@@ -32,54 +33,55 @@ class CoTHEvalStrategy(CoTGeneralStrategy):
             examples (str): Few-shot examples to guide the language model in generating the answer.
             prompt (str): The instruction template used to prompt the language model for the answer.
             additional_keys (Dict[str, str]): Additional keys to format the answer prompt.
-
+            num_retries (int): Number of retries.
+            
         Returns:
             CoTOutput: The output of the CoT strategy.
         """
         start = time.time()
 
-        thought_response = _prompt_llm(
-            llm=self.llm,
-            question=question,
-            examples=examples,
-            prompt=prompt,
-            additional_keys=additional_keys,
-        )
-        thought = thought_response.output_text.split("\n")[0].strip()
+        steps: List[CoTStepOutput] = []
+        for _ in range(max(num_retries, 1)):
+            thought_response = _prompt_llm(
+                llm=self.llm,
+                question=question,
+                examples=examples,
+                prompt=prompt,
+                additional_keys=additional_keys,
+            )
+            thought = thought_response.output_text.split("\n")[0].strip()
 
-        answer_response = _prompt_llm(
-            llm=self.llm,
-            question=question,
-            examples=examples,
-            prompt=f"{prompt}{thought}\nAction: ",
-            additional_keys=additional_keys,
-        )
-        answer = answer_response.output_text.split("```python")[-1].split("```")[0]
-        answer = f"```python\n{answer}\n```"
+            answer_response = _prompt_llm(
+                llm=self.llm,
+                question=question,
+                examples=examples,
+                prompt=f"{prompt}{thought}\nAction: ",
+                additional_keys=additional_keys,
+            )
+            answer = answer_response.output_text.split("```python")[-1].split("```")[0]
+            answer = f"```python\n{answer}\n```"
 
-        step = CoTStepOutput(
-            thought=thought,
-            answer=answer,
-            thought_response=thought_response,
-            answer_response=answer_response,
-        )
+            step = CoTStepOutput(
+                thought=thought,
+                answer=answer,
+                thought_response=thought_response,
+                answer_response=answer_response,
+            )
+            steps.append(step)
+
         total_time = time.time() - start
+        total_metrics = accumulate_metrics(steps)
         out = CoTOutput(
-            answer=answer,
-            total_prompt_tokens=thought_response.prompt_tokens
-            + answer_response.prompt_tokens,
-            total_completion_tokens=thought_response.completion_tokens
-            + answer_response.completion_tokens,
-            total_tokens=thought_response.total_tokens + answer_response.total_tokens,
-            total_prompt_cost=thought_response.prompt_cost
-            + answer_response.prompt_cost,
-            total_completion_cost=thought_response.completion_cost
-            + answer_response.completion_cost,
-            total_cost=thought_response.total_cost + answer_response.total_cost,
-            total_prompt_time=thought_response.prompt_time
-            + answer_response.prompt_time,
+            answer=[step.answer for step in steps],
+            total_prompt_tokens=total_metrics["total_prompt_tokens"],
+            total_completion_tokens=total_metrics["total_completion_tokens"],
+            total_tokens=total_metrics["total_tokens"],
+            total_prompt_cost=total_metrics["total_prompt_cost"],
+            total_completion_cost=total_metrics["total_completion_cost"],
+            total_cost=total_metrics["total_cost"],
+            total_prompt_time=total_metrics["total_prompt_time"],
             total_time=total_time if not self.testing else 0.5,
-            additional_info=step,
+            additional_info=steps,
         )
 
         return out
