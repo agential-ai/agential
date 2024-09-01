623773a0279dad29f124a709e42823736e5e772c
alckasoc <tuvincent0106@gmail.com>
2024-09-01 03:56:40 -0700

    done with testing for prompting

--- tests/prompting/cot/test_prompting.py
+++ tests/prompting/cot/test_prompting.py
@@ -322,3 +322,177 @@ def test_generate() -> None:
         additional_keys={},
     )
     assert out == gt_out
+
+    # Test auto-select prompts and few-shots.
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = CoTOutput(
+        answer=[["Badr Hari"]],
+        total_prompt_tokens=20,
+        total_completion_tokens=40,
+        total_tokens=60,
+        total_prompt_cost=3e-05,
+        total_completion_cost=7.999999999999999e-05,
+        total_cost=0.00010999999999999999,
+        total_prompt_time=1.0,
+        total_time=0.5,
+        additional_info=[
+            [
+                CoTStepOutput(
+                    thought="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.",
+                    answer="Badr Hari",
+                    thought_response=Response(
+                        input_text="",
+                        output_text="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                    answer_response=Response(
+                        input_text="",
+                        output_text="Finish[Badr Hari]",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                )
+            ]
+        ],
+    )
+    responses = [
+        "Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+        "Finish[Badr Hari]",
+    ]
+    method = CoT(
+        llm=MockLLM("gpt-3.5-turbo", responses=responses),
+        benchmark="hotpotqa",
+        testing=True,
+    )
+    out = method.generate(
+        question=question,
+    )
+    assert out == gt_out
+
+    # Test auto-select prompts and few-shots.
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = CoTOutput(
+        answer=[["Badr Hari"]],
+        total_prompt_tokens=20,
+        total_completion_tokens=40,
+        total_tokens=60,
+        total_prompt_cost=3e-05,
+        total_completion_cost=7.999999999999999e-05,
+        total_cost=0.00010999999999999999,
+        total_prompt_time=1.0,
+        total_time=0.5,
+        additional_info=[
+            [
+                CoTStepOutput(
+                    thought="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.",
+                    answer="Badr Hari",
+                    thought_response=Response(
+                        input_text="",
+                        output_text="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                    answer_response=Response(
+                        input_text="",
+                        output_text="Finish[Badr Hari]",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                )
+            ]
+        ],
+    )
+    responses = [
+        "Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+        "Finish[Badr Hari]",
+    ]
+    method = CoT(
+        llm=MockLLM("gpt-3.5-turbo", responses=responses),
+        benchmark="hotpotqa",
+        testing=True,
+    )
+    out = method.generate(question=question, fewshot_type="cot")
+    assert out == gt_out
+
+    # Test auto-select prompts and few-shots.
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = CoTOutput(
+        answer=[["Badr Hari"]],
+        total_prompt_tokens=20,
+        total_completion_tokens=40,
+        total_tokens=60,
+        total_prompt_cost=3e-05,
+        total_completion_cost=7.999999999999999e-05,
+        total_cost=0.00010999999999999999,
+        total_prompt_time=1.0,
+        total_time=0.5,
+        additional_info=[
+            [
+                CoTStepOutput(
+                    thought="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.",
+                    answer="Badr Hari",
+                    thought_response=Response(
+                        input_text="",
+                        output_text="Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                    answer_response=Response(
+                        input_text="",
+                        output_text="Finish[Badr Hari]",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                )
+            ]
+        ],
+    )
+    responses = [
+        "Let's think step by step. Given the information provided, the person described is likely to be Badr Hari, a Moroccan-Dutch kickboxer known for his skills in the ring as well as his controversial behavior both inside and outside of the sport.\nAction: Finish[Badr Hari]",
+        "Finish[Badr Hari]",
+    ]
+    method = CoT(
+        llm=MockLLM("gpt-3.5-turbo", responses=responses),
+        benchmark="hotpotqa",
+        testing=True,
+    )
+    with pytest.raises(
+        ValueError,
+        match="Benchmark 'hotpotqa' few-shot type not supported for CoT.",
+    ):
+        out = method.generate(question=question, fewshot_type="react")
+    assert out == gt_out
--- tests/prompting/standard/test_prompting.py
+++ tests/prompting/standard/test_prompting.py
@@ -279,3 +279,126 @@ def test_generate() -> None:
         warming=[None],
     )
     assert out == gt_out
+
+    # Test auto-select prompts and few-shots.
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = StandardOutput(
+        answer=[["Badr Hari"]],
+        total_prompt_tokens=10,
+        total_completion_tokens=20,
+        total_tokens=30,
+        total_prompt_cost=1.5e-05,
+        total_completion_cost=3.9999999999999996e-05,
+        total_cost=5.4999999999999995e-05,
+        total_prompt_time=0.5,
+        total_time=0.5,
+        additional_info=[
+            [
+                StandardStepOutput(
+                    answer="Badr Hari",
+                    answer_response=Response(
+                        input_text="",
+                        output_text="Badr Hari",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                )
+            ]
+        ],
+    )
+    responses = ["Badr Hari"]
+    llm = MockLLM("gpt-3.5-turbo", responses=responses)
+    method = Standard(llm=llm, benchmark="hotpotqa", testing=True)
+
+    out = method.generate(
+        question=question,
+    )
+    assert out == gt_out
+
+    # Test auto-select prompts and few-shots.
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = StandardOutput(
+        answer=[["Badr Hari"]],
+        total_prompt_tokens=10,
+        total_completion_tokens=20,
+        total_tokens=30,
+        total_prompt_cost=1.5e-05,
+        total_completion_cost=3.9999999999999996e-05,
+        total_cost=5.4999999999999995e-05,
+        total_prompt_time=0.5,
+        total_time=0.5,
+        additional_info=[
+            [
+                StandardStepOutput(
+                    answer="Badr Hari",
+                    answer_response=Response(
+                        input_text="",
+                        output_text="Badr Hari",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                )
+            ]
+        ],
+    )
+    responses = ["Badr Hari"]
+    llm = MockLLM("gpt-3.5-turbo", responses=responses)
+    method = Standard(llm=llm, benchmark="hotpotqa", testing=True)
+
+    out = method.generate(question=question, fewshot_type="direct")
+    assert out == gt_out
+
+    # Test auto-select prompts and few-shots.
+    question = 'Who was once considered the best kick boxer in the world, however he has been involved in a number of controversies relating to his "unsportsmanlike conducts" in the sport and crimes of violence outside of the ring'
+
+    gt_out = StandardOutput(
+        answer=[["Badr Hari"]],
+        total_prompt_tokens=10,
+        total_completion_tokens=20,
+        total_tokens=30,
+        total_prompt_cost=1.5e-05,
+        total_completion_cost=3.9999999999999996e-05,
+        total_cost=5.4999999999999995e-05,
+        total_prompt_time=0.5,
+        total_time=0.5,
+        additional_info=[
+            [
+                StandardStepOutput(
+                    answer="Badr Hari",
+                    answer_response=Response(
+                        input_text="",
+                        output_text="Badr Hari",
+                        prompt_tokens=10,
+                        completion_tokens=20,
+                        total_tokens=30,
+                        prompt_cost=1.5e-05,
+                        completion_cost=3.9999999999999996e-05,
+                        total_cost=5.4999999999999995e-05,
+                        prompt_time=0.5,
+                    ),
+                )
+            ]
+        ],
+    )
+    responses = ["Badr Hari"]
+    llm = MockLLM("gpt-3.5-turbo", responses=responses)
+    method = Standard(llm=llm, benchmark="hotpotqa", testing=True)
+
+    with pytest.raises(
+        ValueError,
+        match="Benchmark 'hotpotqa' few-shot type not supported for Standard.",
+    ):
+        out = method.generate(question=question, fewshot_type="react")
+    assert out == gt_out
