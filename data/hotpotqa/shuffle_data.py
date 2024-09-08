c9a444a5010e4829f1a00eda8bf51e124493e92a
alckasoc <tuvincent0106@gmail.com>
2024-09-07 18:28:18 -0700

    eedit shuffle data

--- data/hotpotqa/shuffle_data.py
+++ data/hotpotqa/shuffle_data.py
@@ -7,7 +7,8 @@ import pickle
 
 parser = argparse.ArgumentParser(description="Shuffle HotpotQA dataset indices.")
 parser.add_argument('--file_name', type=str, default='hotpot_dev_v1_simplified.json', help='HotpotQA dataset file json.')
-parser.add_argument('--save_file_name', type=str, default='hotpot_dev_v1_simplified_s42_indices.pkl', help='Save file name.')
+parser.add_argument('--save_file_name', type=str, default='hotpot_dev_v1_simplified', help='Save file name.')
+parser.add_argument('--sample_size', type=int, default=500, help='Sample size (default: 500).')
 parser.add_argument('--seed', type=int, default=42, help='Random seed for shuffling the dataset indices (default: 42).')
 args = parser.parse_args()
 
@@ -17,8 +18,13 @@ if __name__ == '__main__':
     with open(args.file_name, 'r') as file:
         data = json.load(file)
 
-    numbers = list(range(len(data)))
-    random.shuffle(numbers)
+    indices = list(range(len(data)))
+    random.shuffle(indices)
 
-    with open(args.save_file_name, 'wb') as f:
-        pickle.dump(numbers, f)
+    with open(f"{args.save_file_name}_s{args.seed}_indices.pkl", 'wb') as f:
+        pickle.dump(indices, f)
+
+    data = [data[i] for i in indices[:args.sample_size]]
+
+    with open(f"{args.save_file_name}_s{args.seed}_sample{args.sample_size}.json", 'w') as json_file:
+        json.dump(data, json_file, indent=4)
