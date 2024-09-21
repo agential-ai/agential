2ebf1075927b78dca48595c7e81fbcaa7e0d097b
alckasoc <tuvincent0106@gmail.com>
2024-09-21 00:11:31 -0700

    SR qa sh

---
+++ experiments/self_refine/run.sh
@@ -0,0 +1,27 @@
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --patience 1 \
+    --fewshot_type "cot" \
+    --max_interactions 3
+
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --patience 1 \
+    --fewshot_type "cot" \
+    --max_interactions 3
+
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --patience 1 \
+    --fewshot_type "cot" \
+    --max_interactions 3
+
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --patience 1 \
+    --fewshot_type "cot" \
+    --max_interactions 3
