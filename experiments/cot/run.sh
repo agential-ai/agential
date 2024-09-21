af4363e86f3b24ac4ae983a870d26f61d9d3ed8c
alckasoc <tuvincent0106@gmail.com>
2024-09-21 05:27:14 -0700

    sh

--- experiments/cot/run.sh
+++ experiments/cot/run.sh
@@ -1,23 +1,60 @@
-python hotpotqa.py \
-    --model "gpt-3.5-turbo" \
-    --seed 42 \
-    --num_retries 1 \
-    --warming 0.0
+# Base runs
+# python hotpotqa.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --num_retries 1 \
+#     --warming 0.0
 
-python fever.py \
-    --model "gpt-3.5-turbo" \
-    --seed 42 \
-    --num_retries 1 \
-    --warming 0.0
+# python fever.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --num_retries 1 \
+#     --warming 0.0
 
-python ambignq.py \
-    --model "gpt-3.5-turbo" \
-    --seed 42 \
-    --num_retries 1 \
-    --warming 0.0
+# python ambignq.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --num_retries 1 \
+#     --warming 0.0
 
-python triviaqa.py \
-    --model "gpt-3.5-turbo" \
-    --seed 42 \
-    --num_retries 1 \
-    --warming 0.0
+# python triviaqa.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --num_retries 1 \
+#     --warming 0.0
+
+# num_retries=1, warming=[0.0, 0.5]
+python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.5
+python fever.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.5
+python ambignq.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.5
+python triviaqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.5
+
+# num_retries=1, warming=[0.0, 0.3, 0.7, 1.0]
+python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.3 0.7 1.0
+python fever.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.3 0.7 1.0
+python ambignq.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.3 0.7 1.0
+python triviaqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.3 0.7 1.0
+
+# num_retries=2, warming=[0.0, 0.5]
+python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.5
+python fever.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.5
+python ambignq.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.5
+python triviaqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.5
+
+# num_retries=2, warming=[0.0, 0.3, 0.7, 1.0]
+python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.3 0.7 1.0
+python fever.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.3 0.7 1.0
+python ambignq.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.3 0.7 1.0
+python triviaqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 2 --warming 0.0 0.3 0.7 1.0
+
+# num_retries=3, warming=[0.0, 0.5]
+python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.5
+python fever.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.5
+python ambignq.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.5
+python triviaqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.5
+
+# num_retries=3, warming=[0.0, 0.3, 0.7, 1.0]
+python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.3 0.7 1.0
+python fever.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.3 0.7 1.0
+python ambignq.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.3 0.7 1.0
+python triviaqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 3 --warming 0.0 0.3 0.7 1.0
--- experiments/react/run.sh
+++ experiments/react/run.sh
@@ -1,23 +1,124 @@
+# Base runs
+# python hotpotqa.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --max_steps 6 \
+#     --max_tokens 5000
+
+# python fever.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --max_steps 6 \
+#     --max_tokens 5000
+
+# python ambignq.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --max_steps 6 \
+#     --max_tokens 5000
+
+# python triviaqa.py \
+#     --model "gpt-3.5-turbo" \
+#     --seed 42 \
+#     --max_steps 6 \
+#     --max_tokens 5000
+
+# max_steps=4
 python hotpotqa.py \
     --model "gpt-3.5-turbo" \
     --seed 42 \
-    --max_steps 6 \
+    --max_steps 4 \
     --max_tokens 5000
 
 python fever.py \
     --model "gpt-3.5-turbo" \
     --seed 42 \
-    --max_steps 6 \
+    --max_steps 4 \
     --max_tokens 5000
 
 python ambignq.py \
     --model "gpt-3.5-turbo" \
     --seed 42 \
-    --max_steps 6 \
+    --max_steps 4 \
     --max_tokens 5000
 
 python triviaqa.py \
     --model "gpt-3.5-turbo" \
     --seed 42 \
-    --max_steps 6 \
+    --max_steps 4 \
+    --max_tokens 5000
+
+# max_steps=5
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 5 \
+    --max_tokens 5000
+
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 5 \
+    --max_tokens 5000
+
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 5 \
+    --max_tokens 5000
+
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 5 \
+    --max_tokens 5000
+
+# max_steps=8
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 8 \
+    --max_tokens 5000
+
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 8 \
+    --max_tokens 5000
+
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 8 \
+    --max_tokens 5000
+
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 8 \
+    --max_tokens 5000
+
+# max_steps=10
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 10 \
+    --max_tokens 5000
+
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 10 \
+    --max_tokens 5000
+
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 10 \
+    --max_tokens 5000
+
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 10 \
     --max_tokens 5000
