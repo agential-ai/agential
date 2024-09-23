cca2cbc4ef598b239d090539fcbe06e3bfe6a0e4
alckasoc <tuvincent0106@gmail.com>
2024-09-22 17:43:13 -0700

    analysis for react

--- experiments/cot/run.sh
+++ experiments/cot/run.sh
@@ -1,27 +1,27 @@
 # Base runs
-# python hotpotqa.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --num_retries 1 \
-#     --warming 0.0
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --num_retries 1 \
+    --warming 0.0
 
-# python fever.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --num_retries 1 \
-#     --warming 0.0
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --num_retries 1 \
+    --warming 0.0
 
-# python ambignq.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --num_retries 1 \
-#     --warming 0.0
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --num_retries 1 \
+    --warming 0.0
 
-# python triviaqa.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --num_retries 1 \
-#     --warming 0.0
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --num_retries 1 \
+    --warming 0.0
 
 # num_retries=1, warming=[0.0, 0.5]
 python hotpotqa.py --model "gpt-3.5-turbo" --seed 42 --num_retries 1 --warming 0.0 0.5
--- experiments/react/run.sh
+++ experiments/react/run.sh
@@ -1,27 +1,27 @@
 # Base runs
-# python hotpotqa.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --max_steps 6 \
-#     --max_tokens 5000
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 6 \
+    --max_tokens 5000
 
-# python fever.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --max_steps 6 \
-#     --max_tokens 5000
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 6 \
+    --max_tokens 5000
 
-# python ambignq.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --max_steps 6 \
-#     --max_tokens 5000
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 6 \
+    --max_tokens 5000
 
-# python triviaqa.py \
-#     --model "gpt-3.5-turbo" \
-#     --seed 42 \
-#     --max_steps 6 \
-#     --max_tokens 5000
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --seed 42 \
+    --max_steps 6 \
+    --max_tokens 5000
 
 # max_steps=4
 python hotpotqa.py \
