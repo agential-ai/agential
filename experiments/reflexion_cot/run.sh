2433253cf8bc64c309a67a359bbbccab2edc4a98
alckasoc <tuvincent0106@gmail.com>
2024-09-26 14:27:45 -0700

    edit scripts

--- experiments/reflexion_cot/run.sh
+++ experiments/reflexion_cot/run.sh
@@ -34,3 +34,20 @@ python triviaqa.py \
     --max_trials 3 \
     --patience 3 \
     --reflect_strategy "reflexion"
+
+
+# reflect_strategy [last_attempt, last_attempt_and_reflexion] 
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
+
+# max_trials [5, 7]
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 5 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 7 --patience 3 --reflect_strategy "reflexion"
+
+# max_reflections [5, 7]
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 3 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 7 --max_trials 3 --patience 3 --reflect_strategy "reflexion"
+
+# patience [2, 4]
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 2 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 4 --reflect_strategy "reflexion"
