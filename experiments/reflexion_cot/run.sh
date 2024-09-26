5c7e9415253de3c7306b01573fdce2f2fc18e119
alckasoc <tuvincent0106@gmail.com>
2024-09-25 22:35:06 -0700

    reflexion cot

---
+++ experiments/reflexion_cot/run.sh
@@ -0,0 +1,9 @@
+# Base runs
+python hotpotqa.py \
+    --model "gpt-3.5-turbo" \
+    --eval_model "gpt-4o" \
+    --seed 42 \
+    --max_reflections 3 \
+    --max_trials 3 \
+    --patience 1 \
+    --reflect_strategy "reflexion"
