f12fa13c5ad66061088842fc11ec91375a0972b0
tedasdf <teedsingyau@gmail.com>
2024-09-26 15:48:28 +1000

    experiments/reflexion_cot

--- experiments/reflexion_cot/run.sh
+++ experiments/reflexion_cot/run.sh
@@ -7,3 +7,30 @@ python hotpotqa.py \
     --max_trials 3 \
     --patience 1 \
     --reflect_strategy "reflexion"
+
+python fever.py \
+    --model "gpt-3.5-turbo" \
+    --eval_model "gpt-4o" \
+    --seed 42 \
+    --max_reflections 3 \
+    --max_trials 3 \
+    --patience 1 \
+    --reflect_strategy "reflexion"
+
+python ambignq.py \
+    --model "gpt-3.5-turbo" \
+    --eval_model "gpt-4o" \
+    --seed 42 \
+    --max_reflections 3 \
+    --max_trials 3 \
+    --patience 1 \
+    --reflect_strategy "reflexion"
+
+python triviaqa.py \
+    --model "gpt-3.5-turbo" \
+    --eval_model "gpt-4o" \
+    --seed 42 \
+    --max_reflections 3 \
+    --max_trials 3 \
+    --patience 1 \
+    --reflect_strategy "reflexion"
