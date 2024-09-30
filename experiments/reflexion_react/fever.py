87c0c288dccd3baca2e05df8ed6e43780d175b9f
alckasoc <tuvincent0106@gmail.com>
2024-09-29 19:04:48 -0700

    fever

--- experiments/reflexion_react/fever.py
+++ experiments/reflexion_react/fever.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 import tiktoken
 from agential.agents.reflexion.agent import ReflexionReAct
-from agential.eval.metrics.classification import EM, f1, precision, recall
+from agential.eval.metrics.classification import EM, f1, fuzzy_EM, llm_as_judge_eval, precision, recall
 import os
 import pickle
 
@@ -114,6 +114,8 @@ if __name__ == '__main__':
     eval_table_data = []
     perf_table_data = []
     em_scores = []
+    fuzzy_em_scores = []
+    llm_judge_eval_scores = []
     precision_scores = []
     recall_scores = []
     f1_scores = []
@@ -132,19 +134,23 @@ if __name__ == '__main__':
         )
 
         # Calculate metrics.
-        is_correct = int(EM(out.answer, answer, llm_as_judge=True, llm=eval_llm))
+        is_correct = int(EM(out.answer, answer))
+        is_correct_fuzzy = int(fuzzy_EM(out.answer, answer))
+        llm_judge_eval_score = int(llm_as_judge_eval(llm=eval_llm, question=question, answer=out.answer, key=answer))
         precision_score = precision(out.answer, answer)
         recall_score = recall(out.answer, answer)
         f1_score = f1(out.answer, answer)
 
         # Update scores.
         em_scores.append(is_correct)
+        fuzzy_em_scores.append(is_correct_fuzzy)
+        llm_judge_eval_scores.append(llm_judge_eval_score)
         precision_scores.append(precision_score)
         recall_scores.append(recall_score)
         f1_scores.append(f1_score)
 
         # Update tables.
-        eval_table_data.append([question, answer, out.answer, is_correct, precision_score, recall_score, f1_score])
+        eval_table_data.append([question, answer, out.answer, is_correct, is_correct_fuzzy, llm_judge_eval_score, precision_score, recall_score, f1_score])
         perf_table_data.append([
             out.total_prompt_tokens, 
             out.total_completion_tokens, 
@@ -162,18 +168,22 @@ if __name__ == '__main__':
         # Log metrics.
         run.log({
             "em": is_correct,
+            "fuzzy_em": is_correct_fuzzy,
+            "llm_judge_eval": llm_judge_eval_score,
             "precision": precision_score,
             "recall": recall_score,
             "f1": f1_score,
         })
 
     total_em = sum(em_scores) / len(em_scores)
+    total_em_fuzzy = sum(fuzzy_em_scores) / len(fuzzy_em_scores)
+    total_llm_judge_eval = sum(llm_judge_eval_scores) / len(llm_judge_eval_scores)
     total_precision = sum(precision_scores) / len(precision_scores)
     total_recall = sum(recall_scores) / len(recall_scores)
     total_f1 = sum(f1_scores) / len(f1_scores)
 
-    eval_table = wandb.Table(data=eval_table_data, columns=["question", "answer", "predicted_answer", "EM", "precision", "recall", "f1"])
-    perf_columns = ["total_prompt_tokens", "total_completion_tokens", "total_tokens", "total_prompt_cost", "total_completion_cost", "total_cost", "total_prompt_time", "total_time"]
+    eval_table = wandb.Table(data=eval_table_data, columns=["question", "answer", "predicted_answer", "EM", "fuzzy_EM", "llm_judge_eval", "precision", "recall", "f1"])
+    perf_columns = ["total_prompt_tokens", "total_completion_tokens", "total_tokens", "total_prompt_cost (USD)", "total_completion_cost (USD)", "total_cost (USD)", "total_prompt_time (s)", "total_time (s)"]
     perf_table = wandb.Table(data=perf_table_data, columns=perf_columns)
 
     outputs_save_path = os.path.join(output_path, f"{run.name}.pkl")
@@ -190,12 +200,16 @@ if __name__ == '__main__':
     })
 
     column_averages = np.mean(np.array(perf_table_data, dtype=float), axis=0).tolist()
+    column_sums = np.sum(np.array(perf_table_data, dtype=float), axis=0).tolist()
     run.log({
         "total_em": total_em,
+        "total_em_fuzzy": total_em_fuzzy,
+        "total_llm_judge_eval": total_llm_judge_eval,
         "total_precision": total_precision,
         "total_recall": total_recall,
         "total_f1": total_f1,
-        **dict(zip(perf_columns, column_averages))
+        **dict(zip([f"avg_{col}" for col in perf_columns], column_averages)),
+        **dict(zip([f"sum_{col}" for col in perf_columns], column_sums)),
     })
 
     run.finish()
