1cfb43fde291572578eed2cc4224dc610405812e
alckasoc <tuvincent0106@gmail.com>
2024-09-30 23:02:52 -0700

    scripts

--- experiments/reflexion_cot/run.sh
+++ experiments/reflexion_cot/run.sh
@@ -8,18 +8,41 @@ python triviaqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max
 # Hyperparameter Sweep
 ###############################################################
 
-# reflect_strategy [last_attempt, last_attempt_and_reflexion] 
+# max_reflections=2
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 1 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 1 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 1 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
+
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 3 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 3 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 3 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
+
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 5 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 5 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 2 --max_trials 5 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
+
+# max_reflections=3
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 1 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 1 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 1 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
+
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 3 --reflect_strategy "reflexion"
 python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 3 --reflect_strategy "last_attempt"
 python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
 
-# max_trials [5, 7]
 python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 5 --patience 3 --reflect_strategy "reflexion"
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 7 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 5 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 5 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
 
-# max_reflections [5, 7]
+# max_reflections=5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 1 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 1 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 1 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
+
 python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 3 --patience 3 --reflect_strategy "reflexion"
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 7 --max_trials 3 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 3 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 3 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
 
-# patience [2, 4]
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 2 --reflect_strategy "reflexion"
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 3 --max_trials 3 --patience 4 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 5 --patience 3 --reflect_strategy "reflexion"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 5 --patience 3 --reflect_strategy "last_attempt"
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --max_reflections 5 --max_trials 5 --patience 3 --reflect_strategy "last_attempt_and_reflexion"
--- experiments/self_refine/run.sh
+++ experiments/self_refine/run.sh
@@ -8,44 +8,35 @@ python triviaqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --pat
 # Hyperparameter Sweep
 ###############################################################
 
-# fewshot_type="direct", max_interactions=3
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "direct" --max_interactions 3
+# fewshot_type=cot
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 3 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 3 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 3 --num_results 10
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 5 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 5 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 5 --num_results 10
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 7 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 7 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "cot" --max_interactions 7 --num_results 10
 
-# fewshot_type="react", max_interactions=3
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "react" --max_interactions 3
+# fewshot_type=direct
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 3 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 3 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 3 --num_results 10
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 5 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 5 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 5 --num_results 10
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 7 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 7 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "direct" --max_interactions 7 --num_results 10
 
-# fewshot_type="cot", max_interactions=1
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "cot" --max_interactions 1
-
-# fewshot_type="direct", max_interactions=1
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "direct" --max_interactions 1
-
-# fewshot_type="react", max_interactions=1
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "react" --max_interactions 1
-
-# fewshot_type="cot", max_interactions=2
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "cot" --max_interactions 2
-
-# fewshot_type="direct", max_interactions=2
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "direct" --max_interactions 2
-
-# fewshot_type="react", max_interactions=2
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "react" --max_interactions 2
-
-# fewshot_type="cot", max_interactions=5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "cot" --max_interactions 5
-
-# fewshot_type="direct", max_interactions=5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "direct" --max_interactions 5
-
-# fewshot_type="react", max_interactions=5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "react" --max_interactions 5
-
-# fewshot_type="cot", max_interactions=7
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "cot" --max_interactions 7
-
-# fewshot_type="direct", max_interactions=7
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "direct" --max_interactions 7
-
-# fewshot_type="react", max_interactions=7
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --patience 1 --fewshot_type "react" --max_interactions 7
+# fewshot_type=react
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 3 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 3 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 3 --num_results 10
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 5 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 5 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 5 --num_results 10
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 7 --num_results 5
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 7 --num_results 8
+python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o" --seed 42 --fewshot_type "react" --max_interactions 7 --num_results 10
