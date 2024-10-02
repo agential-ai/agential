addd41cf8dc0f7c1da2bd25c7846b63a5d2123f4
alckasoc <tuvincent0106@gmail.com>
2024-10-01 17:46:41 -0700

    gsm8k for standard is lgtm

--- experiments/standard/gsm8k.py
+++ experiments/standard/gsm8k.py
@@ -1,11 +1,12 @@
 """Run Standard on GSM8K."""
+
 import numpy as np
-from agential.eval.metrics.classification import EM, f1, fuzzy_EM, llm_as_judge_eval, precision, recall
+from agential.eval.metrics.classification import EM
 import os
 import pickle
-import re
 import warnings
 
+from agential.utils.general import safe_execute
 from agential.prompting.standard.prompting import Standard
 warnings.filterwarnings('ignore')
 
@@ -36,7 +37,7 @@ method_name = "standard"
 benchmark = "gsm8k"
 
 if __name__ == '__main__':
-    data = load_dataset("openai/gsm8k", "main")['train']
+    data = load_dataset("openai/gsm8k", "main")['test']
 
     model = args.model
     eval_model = args.eval_model
@@ -90,70 +91,35 @@ if __name__ == '__main__':
     eval_table_data = []
     perf_table_data = []
     em_scores = []
-    fuzzy_em_scores = []
-    llm_judge_eval_scores = []
-    precision_scores = []
-    recall_scores = []
-    f1_scores = []
     outputs = []
 
     for instance in data:
-        
         question = instance["question"]
-        answer = instance["answer"]
+        answer: str = instance["answer"]
+        answer = str(float(answer.split("#### ")[-1].strip().replace(",", "")))
 
-        # Ensure the answer is a string and extract the numeric value
-        if isinstance(answer, str):
-            match = re.search(r"#### (\d+)", answer)
-            num_answer = match.group(1) if match else None
-        else:
-            num_answer = None
-        
-        # Inference
+        # Inference.
         out = method.generate(
             question=question,
-            key=str(answer),
+            key=answer,
             num_retries=num_retries,
             warming=warming
         )
 
-        # Clean up the generated code string
-        code_str_clean = out.answer.replace("```python", "").replace("```", "").strip()
+        code_str = out.answer.replace("```python", "").replace("```", "").strip()
+        pred_answers, _ = safe_execute(code_string=code_str)
+        try:
+            pred_answer = str(float(pred_answers[0]))
+        except:
+            pred_answer = "NaN"
         
-        # Get the last variable name from the code
-        lines = code_str_clean.strip().splitlines()
-        last_line = lines[-1].strip()
-        variable_name = last_line.split('=')[0].strip()
+        is_correct = int(EM(pred_answer, answer, is_numeric=True))
 
-        # Execute the code and store results in exec_vars
-        exec_vars = {}
-        exec(code_str_clean, {}, exec_vars)
-
-        # Access the output from executed code
-        answer_out = exec_vars.get(variable_name)
-
-        # Convert outputs to string format
-        answer_out = str(float(answer_out))
-        num_answer = str(float(num_answer))
-
-        # Calculate evaluation metrics
-        is_correct = int(EM(answer_out, num_answer))
-        is_correct_fuzzy = int(fuzzy_EM(answer_out, num_answer))
-        llm_judge_eval_score = int(llm_as_judge_eval(llm=eval_llm, question=question, answer=answer_out, key=num_answer))
-        precision_score = precision(answer_out, num_answer)
-        recall_score = recall(answer_out, num_answer)
-        f1_score = f1(answer_out, num_answer)
-
-        # Update scores
+        # Update scores.
         em_scores.append(is_correct)
-        fuzzy_em_scores.append(is_correct_fuzzy)
-        llm_judge_eval_scores.append(llm_judge_eval_score)
-        precision_scores.append(precision_score)
-        recall_scores.append(recall_score)
-        f1_scores.append(f1_score)
 
         # Update tables.
-        eval_table_data.append([question, answer, out.answer, is_correct, is_correct_fuzzy, llm_judge_eval_score, precision_score, recall_score, f1_score])
+        eval_table_data.append([question, answer, pred_answer, out.answer, is_correct])
         perf_table_data.append([
             out.total_prompt_tokens, 
             out.total_completion_tokens, 
@@ -171,23 +137,13 @@ if __name__ == '__main__':
         # Log metrics per instance.
         run.log({
             "em": is_correct,
-            "fuzzy_em": is_correct_fuzzy,
-            "llm_judge_eval": llm_judge_eval_score,
-            "precision": precision_score,
-            "recall": recall_score,
-            "f1": f1_score,
         })
 
     # Calculate total scores.
     total_em = sum(em_scores) / len(em_scores)
-    total_em_fuzzy = sum(fuzzy_em_scores) / len(fuzzy_em_scores)
-    total_llm_judge_eval = sum(llm_judge_eval_scores) / len(llm_judge_eval_scores)
-    total_precision = sum(precision_scores) / len(precision_scores)
-    total_recall = sum(recall_scores) / len(recall_scores)
-    total_f1 = sum(f1_scores) / len(f1_scores)
 
     # Create tables.
-    eval_table = wandb.Table(data=eval_table_data, columns=["question", "answer", "predicted_answer", "EM", "fuzzy_EM", "llm_judge_eval", "precision", "recall", "f1"])
+    eval_table = wandb.Table(data=eval_table_data, columns=["question", "answer", "code_answer", "predicted_answer", "EM"])
     perf_columns = ["total_prompt_tokens", "total_completion_tokens", "total_tokens", "total_prompt_cost (USD)", "total_completion_cost (USD)", "total_cost (USD)", "total_prompt_time (s)", "total_time (s)"]
     perf_table = wandb.Table(data=perf_table_data, columns=perf_columns)
 
@@ -212,11 +168,6 @@ if __name__ == '__main__':
     column_sums = np.sum(np.array(perf_table_data, dtype=float), axis=0).tolist()
     run.log({
         "total_em": total_em,
-        "total_em_fuzzy": total_em_fuzzy,
-        "total_llm_judge_eval": total_llm_judge_eval,
-        "total_precision": total_precision,
-        "total_recall": total_recall,
-        "total_f1": total_f1,
         **dict(zip([f"avg_{col}" for col in perf_columns], column_averages)),
         **dict(zip([f"sum_{col}" for col in perf_columns], column_sums)),
     })
