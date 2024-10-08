8e20431ed1f16c673a484ac4d303b69b55c51deb
alckasoc <tuvincent0106@gmail.com>
2024-10-07 18:26:35 -0700

    edit

--- data/mbpp/README.md
+++ data/mbpp/README.md
@@ -1,3 +1,3 @@
 # Data Source
 
-The MBPP dataset is located here: https://huggingface.co/datasets/google-research-datasets/mbpp
+The MBPP dataset is located here: https://huggingface.co/datasets/google-research-datasets/mbpp. We use the sanitized test set for evaluation.
---
+++ experiments/standard/mbpp.py
@@ -0,0 +1,176 @@
+"""Run Standard on MBPP."""
+
+import numpy as np
+from agential.eval.metrics.classification import EM
+import os
+import pickle
+import warnings
+
+from agential.utils.general import safe_execute
+from agential.prompting.standard.prompting import Standard
+warnings.filterwarnings('ignore')
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from agential.core.llm import LLM
+
+from experiments.utils import set_seed
+
+import wandb
+wandb.login()
+from datasets import load_dataset
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Run Standard experiments.")
+parser.add_argument("--model", type=str, default="gpt-3.5-turbo", help="The model")
+parser.add_argument("--eval_model", type=str, default="gpt-4o", help="The evaluator model")
+parser.add_argument("--seed", type=int, default=42, help="Random seed")
+parser.add_argument("--num_retries", type=int, default=1, help="Number of retries")
+parser.add_argument("--warming", type=float, nargs='+', default=[0.0], help="Warming values")
+args = parser.parse_args()
+
+set_seed(args.seed)
+root_dir = "output"
+method_name = "standard"
+benchmark = "mbpp"
+
+if __name__ == '__main__':
+    data = load_dataset("google-research-datasets/mbpp", "sanitized")['test']
+
+    model = args.model
+    eval_model = args.eval_model
+    seed = args.seed
+    num_retries = args.num_retries
+    warming = args.warming
+
+    output_path = os.path.join(root_dir, benchmark)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    llm = LLM(
+        model, 
+        organization=os.getenv("OPENAI_ORGANIZATION"), 
+        temperature=0,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        seed=seed
+    )
+
+    eval_llm = LLM(
+        eval_model,
+        organization=os.getenv("OPENAI_ORGANIZATION"),
+        temperature=0,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        seed=seed
+    )
+
+    method = Standard(
+        llm=llm,
+        benchmark=benchmark,
+    )
+
+    run = wandb.init(
+        project=benchmark, 
+        entity="agential",
+        config={
+            "model": model,
+            "eval_model": eval_model,
+            "seed": seed,
+            "num_retries": num_retries,
+            "warming": warming,
+        },
+        group=method_name,
+        tags=[f"method={method_name}", f"model={model}", f"eval_model={eval_model}", f"seed={seed}", f"num_retries={num_retries}", f"warming={warming}"],
+    )
+
+    eval_table_data = []
+    perf_table_data = []
+    em_scores = []
+    outputs = []
+
+    i = 0
+    for instance in data:
+        if i == 5: break
+        i+= 1
+        question = instance["prompt"]
+        answer: str = "\n".join(instance['test_imports'] + [''] + instance['test_list']).strip()
+
+        # Inference.
+        out = method.generate(
+            question=question,
+            key=answer,
+            additional_keys={"tests": answer},
+            num_retries=num_retries,
+            warming=warming
+        )
+
+        code_str = out.answer.replace("```python", "").replace("```", "").strip()
+        _, execution_status = safe_execute(
+            f"from typing import *\n\n{code_str}\n{answer}"
+        )
+        
+        is_correct = int(EM(execution_status, "Done", normalize=False))
+
+        # Update scores.
+        em_scores.append(is_correct)
+
+        # Update tables.
+        eval_table_data.append([question, answer, execution_status, out.answer, is_correct])
+        perf_table_data.append([
+            out.total_prompt_tokens, 
+            out.total_completion_tokens, 
+            out.total_tokens, 
+            out.total_prompt_cost,
+            out.total_completion_cost,
+            out.total_cost,
+            out.total_prompt_time,
+            out.total_time
+        ])
+
+        # Update outputs.
+        outputs.append(out)
+
+        # Log metrics per instance.
+        run.log({
+            "pass@k=1": is_correct,
+        })
+
+    # Calculate total scores.
+    total_em = sum(em_scores) / len(em_scores)
+
+    # Create tables.
+    eval_table = wandb.Table(data=eval_table_data, columns=["question", "answer", "code_answer", "predicted_answer", "pass@k=1"])
+    perf_columns = ["total_prompt_tokens", "total_completion_tokens", "total_tokens", "total_prompt_cost (USD)", "total_completion_cost (USD)", "total_cost (USD)", "total_prompt_time (s)", "total_time (s)"]
+    perf_table = wandb.Table(data=perf_table_data, columns=perf_columns)
+
+    # Save outputs as pkl.
+    outputs_save_path = os.path.join(output_path, f"{run.name}.pkl")
+    with open(outputs_save_path, 'wb') as f:
+        pickle.dump(outputs, f)
+
+    # Save outputs as artifact.
+    artifact = wandb.Artifact(name=run.name, type="output")
+    artifact.add_file(local_path=outputs_save_path, name="outputs.pkl")
+    artifact.save()
+
+    # Log tables.
+    run.log({
+        f"{run.name}_eval": eval_table,
+        f"{run.name}_perf": perf_table
+    })
+
+    # Log all metrics.
+    column_averages = np.mean(np.array(perf_table_data, dtype=float), axis=0).tolist()
+    column_sums = np.sum(np.array(perf_table_data, dtype=float), axis=0).tolist()
+    run.log({
+        "pass@k=1": total_em,
+        **dict(zip([f"avg_{col}" for col in perf_columns], column_averages)),
+        **dict(zip([f"sum_{col}" for col in perf_columns], column_sums)),
+    })
+    
+    run.finish()
