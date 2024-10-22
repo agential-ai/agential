5d0c11da493129d9c59e30b02f698506bce8006c
alckasoc <tuvincent0106@gmail.com>
2024-10-21 17:19:03 -0700

    finish clin memory

--- agential/agents/clin/memory.py
+++ agential/agents/clin/memory.py
@@ -6,14 +6,20 @@ from agential.agents.base.modules.memory import BaseMemory
 class CLINMemory(BaseMemory):
     """CLIN Memory implementation."""
 
-    def __init__(self):
+    def __init__(
+        self, 
+        k: int = 10
+    ) -> None:
+        """Initialize."""
+        super().__init__()
+        self.k = k
         self.previous_trials = []
 
     def clear(self) -> None:
         """Clear all memories."""
         self.previous_trials = []
 
-    def add_memories(self, question: str, summary: str, meta_summary: str, eval_report: str) -> None:
+    def add_memories(self, question: str, summary: str, meta_summary: str, eval_report: str, is_correct: bool) -> None:
         """Add summaries to the CLIN Memory.
 
         Args:
@@ -21,6 +27,7 @@ class CLINMemory(BaseMemory):
             summary (str): The summary of the question.
             meta_summary (str): The meta summary of the question.
             eval_report (str): The evaluation report of the question.
+            is_correct (bool): Whether the question was answered correctly.
 
         Returns:
             None
@@ -31,6 +38,7 @@ class CLINMemory(BaseMemory):
                 "summary": summary,
                 "meta_summary": meta_summary,
                 "eval_report": eval_report,
+                "is_correct": is_correct,
             }
         )
 
@@ -40,7 +48,9 @@ class CLINMemory(BaseMemory):
         Returns:
             Dict[str, Any]: A dictionary containing all stored memories.
         """
-        return {"previous_trials": self.previous_trials}
+        previous_successful_trials = [trial for trial in self.previous_trials if trial["is_correct"]]
+        previous_successful_k_trials = previous_successful_trials[-self.k:]
+        return {"previous_successful_k_trials": previous_successful_k_trials}
 
     def show_memories(self) -> Dict[str, Any]:
         """Show all memories.
