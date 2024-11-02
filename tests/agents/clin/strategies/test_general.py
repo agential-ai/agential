2e6e7315c601c340a4b89dac5ee6661f015ffc0e
alckasoc <tuvincent0106@gmail.com>
2024-11-01 23:40:19 -0700

    skeleton

--- tests/agents/clin/strategies/test_general.py
+++ tests/agents/clin/strategies/test_general.py
@@ -1,1 +1,37 @@
 """Test the CLIN general strategy."""
+
+import tiktoken
+from agential.agents.clin.strategies.general import CLINGeneralStrategy
+
+def test_init() -> None:
+    """Test CLIN general strategy initialization."""
+    strategy = CLINGeneralStrategy(llm=None, memory=None)
+    assert strategy.max_trials == 3
+    assert strategy.max_steps == 6
+    assert strategy.max_tokens == 5000
+    assert strategy.enc == tiktoken.encoding_for_model("gpt-3.5-turbo")
+    assert strategy.testing is False
+
+
+def test_generate_thought() -> None:
+    """Test CLIN general strategy generate thought."""
+
+
+def test_generate_summary() -> None:
+    """Test CLIN general strategy generate summary."""
+    pass
+
+
+def test_generate_meta_summary() -> None:
+    """Test CLIN general strategy generate meta summary."""
+    pass
+
+
+def test_react_halting_condition() -> None:
+    """Test CLIN general strategy react halting condition."""
+    pass
+
+
+def test_reset() -> None:
+    """Test CLIN general strategy reset."""
+    pass
