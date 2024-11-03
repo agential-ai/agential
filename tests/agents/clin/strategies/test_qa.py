b0ecbba019199b77dd1d59aacdcdb0be62c20164
alckasoc <tuvincent0106@gmail.com>
2024-11-02 18:36:50 -0700

    test init

--- tests/agents/clin/strategies/test_qa.py
+++ tests/agents/clin/strategies/test_qa.py
@@ -1,1 +1,36 @@
 """Test the CLIN QA strategy."""
+
+import tiktoken
+
+from agential.agents.clin.strategies.qa import CLINQAStrategy
+from agential.utils.docstore import DocstoreExplorer
+
+
+def test_init() -> None:
+    """Test CLIN QA strategy initialization."""
+    strategy = CLINQAStrategy(llm=None, memory=None)
+    assert strategy.max_trials == 3
+    assert strategy.max_steps == 6
+    assert strategy.max_tokens == 5000
+    assert strategy.enc == tiktoken.encoding_for_model("gpt-3.5-turbo")
+    assert strategy.testing is False
+    assert isinstance(strategy.docstore, DocstoreExplorer)
+
+
+def test_generate() -> None:
+    """Test CLIN QA strategy generate."""
+
+
+def test_generate_react() -> None:
+    """Test CLIN QA strategy generate react."""
+
+def test_generate_action() -> None:
+    """Test CLIN QA strategy generate action."""
+
+
+def test_generate_observation() -> None:
+    """Test CLIN QA strategy generate observation."""
+
+
+def test_halting_condition() -> None:
+    """Test CLIN QA strategy halting condition."""
