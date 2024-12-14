9e628c837f844a6190f9626648f863968ff3f8a4
tedasdf <teedsingyau@gmail.com>
2024-12-14 13:02:44 +1100

    svamp

--- experiments/react/svamp.py
+++ experiments/react/svamp.py
@@ -48,7 +48,7 @@ method_name = "react"
 benchmark = "svamp"
 
 if __name__ == "__main__":
-    data = load_dataset("tongyx361/svamp")["test"]
+    data = load_dataset("Sing0402/svamp_200")['train']
 
     n_eval_samples = args.n_eval_samples
     model = args.model
