470bb02285b32aa1cbf611bdca91b547d50c9d66
tedasdf <teedsingyau@gmail.com>
2024-12-14 12:40:32 +1100

    gsm8k

--- experiments/reflexion_cot/gsm8k.py
+++ experiments/reflexion_cot/gsm8k.py
@@ -49,7 +49,7 @@ method_name = "reflexion_cot"
 benchmark = "gsm8k"
 
 if __name__ == "__main__":
-    data = load_dataset("openai/gsm8k", "main")["test"]
+    data = load_dataset("Sing0402/gsm8k_200")['train']
 
     n_eval_samples = args.n_eval_samples
     model = args.model
