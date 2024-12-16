ebc22fbd235156b1182abb433e3599a4934f93d2
alckasoc <tuvincent0106@gmail.com>
2024-12-16 00:47:00 -0800

    finished sr sh

--- experiments/self_refine/run.sh
+++ experiments/self_refine/run.sh
@@ -5,38 +5,75 @@ python ambignq.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 -
 python triviaqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --patience 1 --fewshot_type "cot" --max_interactions 3
 
 ###############################################################
-# Hyperparameter Sweep
+# [Optional] Hyperparameter Sweep
 ###############################################################
 
+hparam_qa_benchmark = "hotpotqa"
+
 # fewshot_type=cot
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 3 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 3 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 3 --num_results 10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 5 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 5 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 5 --num_results 10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 7 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 7 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 7 --num_results 10
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 7
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 7
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "cot" --max_interactions 7
 
 # fewshot_type=direct
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 3 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 3 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 3 --num_results 10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 5 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 5 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 5 --num_results 10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 7 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 7 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 7 --num_results 10
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 7
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 7
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "direct" --max_interactions 7
 
 # fewshot_type=react
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 3 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 3 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 3 --num_results 10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 5 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 5 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 5 --num_results 10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 7 --num_results 5
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 7 --num_results 8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 7 --num_results 10
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 3
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 5
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 7
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 7
+python ${hparam_qa_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --fewshot_type "react" --max_interactions 7
+
+hparam_math_code_benchmark = "gsm8k"
+
+# fewshot_type=cot
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7
+
+# fewshot_type=direct
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7
+
+# fewshot_type=react
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 3 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 5 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7 
+python ${hparam_math_code_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_interactions 7 
