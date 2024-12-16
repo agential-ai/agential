b1568e7a9179293480cb5c014a29f0692c3e4302
alckasoc <tuvincent0106@gmail.com>
2024-12-16 00:37:00 -0800

    finished react sh

--- experiments/react/run.sh
+++ experiments/react/run.sh
@@ -3,26 +3,33 @@ python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 
 python fever.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
 python ambignq.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
 python triviaqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
+python gsm8k.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
+python svamp.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
+python tabmwp.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
+python humaneval.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
+python mbpp.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 5000
 
 ###############################################################
 # Hyperparameter Sweep
 ###############################################################
 
+hparam_benchmark = "hotpotqa"
+
 # max_steps=6
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 7500
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 10000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 7500
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 6 --max_tokens 10000
 
 # max_steps=4
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 4 --max_tokens 5000
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 4 --max_tokens 7500
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 4 --max_tokens 10000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 4 --max_tokens 5000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 4 --max_tokens 7500
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 4 --max_tokens 10000
 
 # max_steps=8
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 8 --max_tokens 5000
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 8 --max_tokens 7500
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 8 --max_tokens 10000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 8 --max_tokens 5000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 8 --max_tokens 7500
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 8 --max_tokens 10000
 
 # max_steps=10
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 10 --max_tokens 5000
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 10 --max_tokens 7500
-python hotpotqa.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 10 --max_tokens 10000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 10 --max_tokens 5000
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 10 --max_tokens 7500
+python ${hparam_benchmark}.py --model "gpt-3.5-turbo" --eval_model "gpt-4o-mini" --seed 42 --max_steps 10 --max_tokens 10000
