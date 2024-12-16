77fcd2370a140dfd09ee7c140008bc336b25d3ce
alckasoc <tuvincent0106@gmail.com>
2024-12-15 18:58:24 -0800

    fixed typo bug

--- experiments/critic/hotpotqa.py
+++ experiments/critic/hotpotqa.py
@@ -62,7 +62,7 @@ method_name = "critic"
 benchmark = "hotpotqa"
 
 if __name__ == "__main__":
-    data = load_dataset("Sing0402/fever_200")["train"]
+    data = load_dataset("Sing0402/hotpotqa_200")["train"]
 
     n_eval_samples = args.n_eval_samples
     model = args.model
