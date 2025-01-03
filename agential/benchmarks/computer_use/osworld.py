5a56c62a0b3ec5191ac68ae68d15ab9b7aeba57c
alckasoc <tuvincent0106@gmail.com>
2025-01-03 00:00:53 -0800

    todos

--- agential/benchmarks/computer_use/osworld.py
+++ agential/benchmarks/computer_use/osworld.py
@@ -10,8 +10,17 @@ env = DesktopEnv(action_space="pyautogui")
 obs = env.reset(task_config=example)
 obs, reward, done, info = env.step("pyautogui.rightClick()")
 
+# TODO: Write BaseBenchmark
+# TODO: Write BaseComputerUseBenchmark
+# TODO: Write OSWorld
+#           - it's mostly done but we need a standard interface
+# TODO: Documentation
+#           - this one definitely needs lots of documentation on setup (best to reference the OSWorld code base)
+#           - don't forget to credit the original code! 
+# TODO: Testing
+# TODO: Linting, code coverage
 
-class OSWorld(BaseBenchmark):
+class OSWorld(BaseComputerUseBenchmark):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.env = DesktopEnv(**kwargs)
