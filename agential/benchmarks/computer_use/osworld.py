c29b34881a01804d847eaac2a903676bcad4d77f
alckasoc <tuvincent0106@gmail.com>
2025-01-02 23:57:44 -0800

    starter code

--- agential/benchmarks/computer_use/osworld.py
+++ agential/benchmarks/computer_use/osworld.py
@@ -1,5 +1,6 @@
 """OSWorld Benchmark."""
 
+from typing import Any, Dict, Optional
 from desktop_env.desktop_env import DesktopEnv
 
 example = {}
@@ -10,6 +11,22 @@ obs = env.reset(task_config=example)
 obs, reward, done, info = env.step("pyautogui.rightClick()")
 
 
-class OSWorld(BaseBenchmark, DesktopEnv):
+class OSWorld(BaseBenchmark):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.env = DesktopEnv(**kwargs)
+
+    def close(self):
+        self.env.close()
+
+    def reset(self, task_config: Optional[Dict[str, Any]] = None, seed=None, options=None) -> Dict[str, Any]:
+        self.env.reset()
+
+    def step(self, action, pause=2):
+        self.env.step() 
+
+    def evaluate(self):
+        self.env.evaluate()
+
+    def render(self):
+        self.env.render()
