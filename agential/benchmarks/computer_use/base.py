04649e06e1664d3e5db0abf57862fb8b7aef5fc0
alckasoc <tuvincent0106@gmail.com>
2025-01-03 00:26:03 -0800

    LETS FUCKING GOOOOOOOO; INSTALLED DESKTOP ENV

--- agential/benchmarks/computer_use/base.py
+++ agential/benchmarks/computer_use/base.py
@@ -5,3 +5,4 @@ class BaseComputerUseBenchmark(BaseBenchmark):
     """Base benchmark class for computer-use benchmarks."""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
