05b9077e6557cdc0273696c26f1745fe47fb9463
alckasoc <tuvincent0106@gmail.com>
2025-01-13 01:35:34 -0800

    add fixture secrets (fake), lint, tests

--- tests/fixtures/osworld.py
+++ tests/fixtures/osworld.py
@@ -14,10 +14,22 @@ def data_dir(pytestconfig) -> str:
 @pytest.fixture
 def osworld_screenshot_path(data_dir: str) -> str:
     """Dir path to OSWorld screenshot."""
-    return os.path.join(data_dir, "osworld_baseline", "output_image.jpeg")
+    return os.path.join(data_dir, "osworld", "output_image.jpeg")
 
 
 @pytest.fixture
 def osworld_access_tree(data_dir: str) -> str:
     """Dir path to OSWorld screenshot."""
-    return os.path.join(data_dir, "osworld_baseline", "accessibility_tree.txt")
+    return os.path.join(data_dir, "osworld", "accessibility_tree.txt")
+
+
+@pytest.fixture
+def osworld_google_settings_json_path(data_dir: str) -> str:
+    """Dir path to OSWorld screenshot."""
+    return os.path.join(data_dir, "osworld", "settings", "google", "settings.json")
+
+
+@pytest.fixture
+def osworld_googledrive_settings_yml_path(data_dir: str) -> str:
+    """Dir path to OSWorld screenshot."""
+    return os.path.join(data_dir, "osworld", "settings", "googledrive", "settings.yml")
