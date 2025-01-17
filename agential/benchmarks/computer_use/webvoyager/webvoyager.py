9d12c3fef1078ea4082fe01fb102f31f5d46c680
alckasoc <tuvincent0106@gmail.com>
2025-01-17 13:17:09 -0800

    .

--- agential/benchmarks/computer_use/webvoyager/webvoyager.py
+++ agential/benchmarks/computer_use/webvoyager/webvoyager.py
@@ -4,30 +4,40 @@ from typing import Any
 from agential.benchmarks.computer_use.base import BaseComputerUseBenchmark
 from selenium import webdriver
 
-def driver_config(args):
+def driver_config(
+    download_dir: str,
+    headless: bool,
+    force_device_scale: bool,
+):
     options = webdriver.ChromeOptions()
 
-    if args.save_accessibility_tree:
-        args.force_device_scale = True
-
-    if args.force_device_scale:
+    if force_device_scale:
         options.add_argument("--force-device-scale-factor=1")
-    if args.headless:
+    if headless:
         options.add_argument("--headless")
         options.add_argument(
             "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
         )
     options.add_experimental_option(
         "prefs", {
-            "download.default_directory": args.download_dir,
+            "download.default_directory": download_dir,
             "plugins.always_open_pdf_externally": True
         }
     )
     return options
 
 class WebVoyager(BaseComputerUseBenchmark):
-    def __init__(self) -> None:
-        options = driver_config(args)
+    def __init__(
+        self,
+        download_dir: str,
+        headless: bool,
+        force_device_scale: bool,
+    ) -> None:
+        self.options = driver_config(
+            download_dir=download_dir,
+            headless=headless,
+            force_device_scale=force_device_scale,
+        )
 
     def close(self) -> None:
         pass
