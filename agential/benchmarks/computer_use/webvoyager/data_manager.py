21c677fce6e648696635c3608c9a21e7f8c6f379
alckasoc <tuvincent0106@gmail.com>
2025-01-16 21:38:47 -0800

    data manager init

--- agential/benchmarks/computer_use/webvoyager/data_manager.py
+++ agential/benchmarks/computer_use/webvoyager/data_manager.py
@@ -8,22 +8,45 @@ import os
 class WebVoyagerDataManager:
     """WebVoyager data manager to process and read JSONL files."""
 
-    def __init__(self, examples_dir: str = "") -> None:
+    def __init__(self, mode: str = "benchmark", examples_dir: str = "") -> None:
         """
         Initialize the data manager and load data from a JSONL file.
 
         Args:
+            mode (str): The mode to run the benchmark in. Can be either 'custom' or 'benchmark'. Defaults to "benchmark".
             examples_dir (str): Path to the JSONL file containing data. Defaults to "" (will use benchmark examples).
         """
-        self.data = []
+        self.mode = mode
+        self.examples_dir = examples_dir
+        self.data: List[Dict[str, Any]] = []
 
-        if not examples_dir:
+        # Only if in "benchmark" mode.
+        self.reference_answers: Dict[str, Any] = {}
+
+        if self.mode == "custom":
+            if self.examples_dir == "":
+                raise ValueError("examples_dir must be provided if mode is 'custom'.")
+            if not os.path.exists(self.examples_dir):
+                raise ValueError("examples_dir does not exist.")
+        
+        
+        elif self.mode == "benchmark":
             current_file_path = os.path.dirname(__file__)
             examples_dir = os.path.join(
                 current_file_path, "evaluation_examples", "WebVoyager_data.jsonl"
             )
-
-        self.examples_dir = examples_dir
+            examples_answers_dir = os.path.join(
+                current_file_path, "evaluation_examples", "reference_answer.json"
+            )
+            try:
+                with open(examples_answers_dir, "r") as file:
+                    self.reference_answers = json.load(file)
+            except FileNotFoundError:
+                raise FileNotFoundError(f"File not found: {examples_answers_dir}")
+            except json.JSONDecodeError as e:
+                raise ValueError(f"Invalid JSON in file: {e}")
+        else:
+            raise ValueError("Mode must be either 'custom' or 'benchmark'.")
 
         try:
             with open(self.examples_dir, "r") as file:
