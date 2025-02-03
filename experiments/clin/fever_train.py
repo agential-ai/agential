b9c15f7aa18e29cfeed5f8a0780c16c5d425fa15
alckasoc <tuvincent0106@gmail.com>
2025-02-02 19:28:48 -0800

    fix

--- experiments/clin/fever_train.py
+++ experiments/clin/fever_train.py
@@ -121,9 +121,6 @@ if __name__ == "__main__":
         benchmark=benchmark,
         memory=CLINMemory(
             k=k,
-            memories=memory['summaries'],
-            meta_summaries=memory['meta_summaries'],
-            history=memory['history'],
         ),
         # kwargs.
         max_trials=max_trials,
